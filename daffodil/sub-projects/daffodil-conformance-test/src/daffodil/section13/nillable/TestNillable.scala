272d515b7e061a8f1ef1f729708b6edc486e2ec1
Mike Beckerle <mbeckerle.dfdl@gmail.com>
2012-06-09 17:26:28 -0400

    Added daffodil-conformance-test sub-project.
    Starting point with nillability test created during f2f meeting.

---
+++ daffodil/sub-projects/daffodil-conformance-test/src/daffodil/section13/nillable/TestNillable.scala
@@ -0,0 +1,21 @@
+package daffodil.section13.nillable
+
+
+import junit.framework.Assert._
+import org.scalatest.junit.JUnit3Suite
+import scala.xml._
+import daffodil.xml.XMLUtils
+import daffodil.xml.XMLUtils._
+import daffodil.dsom.Compiler
+import daffodil.util._
+import daffodil.tdml.DFDLTestSuite
+import java.io.File
+
+class TestNillable extends JUnit3Suite {
+  val testDir = "src/daffodil/section13/nillable/"
+  val aa = testDir + "nillable.tdml"
+  val runnerAA = new DFDLTestSuite(new File(aa))
+
+  def test_litNil1() { runnerAA.runOneTest("litNil1") }
+
+}
