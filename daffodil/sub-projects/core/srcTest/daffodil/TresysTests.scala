0e12ad76032c2d8a0466d2b9a6779890ecee7f0a
Paul Chu <pchu@tresys.com>
2012-07-12 14:16:37 -0400

    Close DFDL-112 LengthKind Delimited

--- daffodil/sub-projects/core/srcTest/daffodil/TresysTests.scala
+++ daffodil/sub-projects/core/srcTest/daffodil/TresysTests.scala
@@ -19,32 +19,10 @@ class TresysTests extends JUnit3Suite {
 
   def test_AA000() { runnerAA.runOneTest("AA000") }
   
-  val ab = testDir + "AB.tdml"
-  val runnerAB = new DFDLTestSuite(new File(ab))
-    
-  def test_AB000() { runnerAB.runOneTest("AB000") }
-  def test_AB001() { runnerAB.runOneTest("AB001") }
-  def test_AB002() { runnerAB.runOneTest("AB002") }
-  def test_AB003() { runnerAB.runOneTest("AB003") }
-//  def test_AB004() { runnerAB.runOneTest("AB004") }
-//  def test_AB005() { runnerAB.runOneTest("AB005") }
-  
-  val an = testDir + "AN.tdml"
-  val runnerAN = new DFDLTestSuite(new File(an))
-  
-  def test_AN000() { runnerAN.runOneTest("AN000") }
-  def test_AN001() { runnerAN.runOneTest("AN001") }
-  
   val delimited = testDir + "dpaext1.tdml"
   val runnerDelimited = new DFDLTestSuite(new File(delimited))
   
-  def test_length_delimited_12_02() { runnerDelimited.runOneTest("length_delimited_12_02") }
   def test_length_delimited_12_03_controversial() { runnerDelimited.runOneTest("length_delimited_12_03_controversial") }
-  def test_length_delimited_12_03() { runnerDelimited.runOneTest("length_delimited_12_03") }
-//  def test_length_delimited_12_05() { runnerDelimited.runOneTest("length_delimited_12_05") }
-//  def test_length_delimited_12_06() { runnerDelimited.runOneTest("length_delimited_12_06") }
-  def test_multiple_delimiters() { runnerDelimited.runOneTest("multiple_delimiters") }
-  def test_multiple_delimiters2() { runnerDelimited.runOneTest("multiple_delimiters2") }
 
   val td = testDir + "multiple-diagnostics.tdml"
   val runnerMD = new DFDLTestSuite(new File(td))
---
+++ daffodil/sub-projects/daffodil-conformance-test/src/daffodil/section14/sequence_groups/TestSequenceGroups.scala
@@ -0,0 +1,20 @@
+package daffodil.section14.sequence_groups
+
+import junit.framework.Assert._
+import org.scalatest.junit.JUnit3Suite
+import scala.xml._
+import daffodil.xml.XMLUtils
+import daffodil.xml.XMLUtils._
+import daffodil.dsom.Compiler
+import daffodil.util._
+import daffodil.tdml.DFDLTestSuite
+import java.io.File
+
+class TestSequenceGroups extends JUnit3Suite {
+  val testDir = "src/daffodil/section14/sequence_groups/"
+  val aa = testDir + "SequenceGroupTests.tdml"
+  val runner = new DFDLTestSuite(new File(aa))
+  
+  def test_multiple_delimiters2() { runner.runOneTest("multiple_delimiters2") }
+  
+  }
