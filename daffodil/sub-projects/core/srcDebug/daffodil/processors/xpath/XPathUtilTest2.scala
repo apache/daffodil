cee4d0d020d2738a07b0de9b441acd4a20127c96
Mike Beckerle <mbeckerle.dfdl@gmail.com>
2012-07-27 17:24:39 -0400

    Unit test that illustrates issue with XPath and XML default namespaces. (For Jonathan)

---
+++ daffodil/sub-projects/core/srcDebug/daffodil/processors/xpath/XPathUtilTest2.scala
@@ -0,0 +1,71 @@
+package daffodil.processors.xpath
+
+import org.jdom.Document
+import org.jdom.Element
+import org.jdom.Text
+import daffodil.processors.VariableMap
+import daffodil.xml._
+import org.scalatest.junit.JUnit3Suite
+import junit.framework.Assert._
+
+
+class XPathUtilTest2 extends JUnit3Suite {
+
+ 
+  /**
+   * Test that uses default namespace, not no-namespace.
+   */
+   def testXPathWithDefaultNS1() { 
+    
+    // <root xmlns="urn:foobarNS><child1>19</child1></root>
+    val text = new Text("19")
+    val child1 = new Element("child1", "urn:foobarNS")
+    val root = new Element("root", "urn:foobarNS")
+    child1 addContent(text)
+    root addContent(child1)
+    val document = new Document(root)
+       
+    val ns = List(org.jdom.Namespace.getNamespace("", "urn:foobarNS"))
+    val result = XPathUtil evalExpressionFromString("/root/child1",
+        new VariableMap(),
+        root,
+        ns)
+    
+    result match {
+      case NodeResult(x) => assertEquals("19", x.getText())
+      case _ => {
+        println(result) 
+        fail
+      }
+    }
+  }
+   
+   /**
+    * Variation just gets the namespace by way of the root.getNamespace() call. 
+    */
+  def testXPathWithDefaultNS2() { 
+    
+    // <root xmlns="urn:foobarNS><child1>19</child1></root>
+    val text = new Text("19")
+    val child1 = new Element("child1", "urn:foobarNS")
+    val root = new Element("root", "urn:foobarNS")
+    child1 addContent(text)
+    root addContent(child1)
+    val document = new Document(root)
+       
+    val ns = List(root.getNamespace()) // different way to get namespace info.
+    val result = XPathUtil evalExpressionFromString("/root/child1",
+        new VariableMap(),
+        root,
+        ns)
+    
+    result match {
+      case NodeResult(x) => assertEquals("19", x.getText())
+      case _ => {
+        println(result) 
+        fail
+      }
+    }
+  }
+        
+}
