0c6898b052282f073b50cd85e0823a948e8d4670
Paul Chu <pchu@tresys.com>
2012-08-01 17:23:44 -0400

    Moved multiple_delimiters2 test to ibm-tests folder.

--- daffodil/sub-projects/daffodil-conformance-test/srcTest/daffodil/section14/sequence_groups/TestSequenceGroups.scala
+++ daffodil/sub-projects/daffodil-conformance-test/srcTest/daffodil/section14/sequence_groups/TestSequenceGroups.scala
@@ -11,8 +11,8 @@ import daffodil.tdml.DFDLTestSuite
 import java.io.File
 
 class TestSequenceGroups extends JUnit3Suite {
-  val testDir = "srcTest/daffodil/section14/sequence_groups/"
-  val aa = testDir + "SequenceGroupTests.tdml"
+  val testDir = "srcTest/daffodil/ibm-tests/"
+  val aa = testDir + "dpaext1.tdml"
   val runner = new DFDLTestSuite(new File(aa))
   
   def test_multiple_delimiters2() { runner.runOneTest("multiple_delimiters2") }
