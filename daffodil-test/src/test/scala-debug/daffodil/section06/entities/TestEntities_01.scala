89d6b61978f1355609c0961d6e504dd250df8a25
Paul Chu <pchu@tresys.com>
2012-08-14 11:45:29 -0400

    Tests for hexadecimal and decimal code points, namely, byte_entities_6_06 and byte_entities_6_07, for DFDL character entities fail.

--- daffodil-test/src/test/scala-debug/daffodil/section06/entities/TestEntities_01.scala
+++ daffodil-test/src/test/scala-debug/daffodil/section06/entities/TestEntities_01.scala
@@ -17,5 +17,7 @@ class TestEntities_01 extends JUnit3Suite {
   val runner_01 = new DFDLTestSuite(Misc.getRequiredResource(aa_01))
   
   def test_text_entities_6_02() { runner_01.runOneTest("text_entities_6_02") }
+  def test_byte_entities_6_06() { runner_01.runOneTest("byte_entities_6_06") }
+  def test_byte_entities_6_07() { runner_01.runOneTest("byte_entities_6_07") }
   
   }
