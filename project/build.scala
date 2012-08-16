61abc4ad7ac78c86b8d9daaff1c2e305c9cc1acf
Steve Lawrence <slawrence@tresys.com>
2012-08-16 13:34:51 -0400

    Add auto building of properties
    GeneratedCode.scala is no longer tracked in git. It must be built. To
    build this file, run the following:
    
    $ sbt
    > project daffodil-lib
    > gen-props
    
    Also updates IDE settings to include the new autogenerated source

--- project/build.scala
+++ project/build.scala
@@ -11,10 +11,14 @@ object DaffodilBuild extends Build {
 	// daffodil projects
 	lazy val root    = Project(id = "daffodil", base = file("."), settings = s ++ nopub)
 	                           .configs(DebugTest)
-	                           .aggregate(lib, core, test)
+	                           .aggregate(propgen, lib, core, test)
 
-	lazy val lib     = Project(id = "daffodil-lib", base = file("daffodil-lib"), settings = s)
+	lazy val propgen = Project(id = "daffodil-propgen", base = file("daffodil-propgen"), settings = s)
 	                           .configs(DebugTest)
+
+	lazy val lib     = Project(id = "daffodil-lib", base = file("daffodil-lib"), settings = s ++ propgenSettings)
+	                           .configs(DebugTest)
+	                           .dependsOn(propgen)
 
 	lazy val core    = Project(id = "daffodil-core", base = file("daffodil-core"), settings = s)
 	                           .configs(DebugTest)
@@ -24,7 +28,31 @@ object DaffodilBuild extends Build {
 	                           .configs(DebugTest)
 	                           .dependsOn(core)
 
-	lazy val propgen = Project(id = "daffodil-propgen", base = file("daffodil-propgen"), settings = s)
+	val propertyGenerator = TaskKey[Seq[File]]("gen-props", "Generate properties scala source")
+	lazy val propgenSettings = Seq(
+		sourceGenerators in Compile <+= (propertyGenerator in Compile),
+		propertyGenerator in Compile <<=
+			(sourceManaged in Compile, dependencyClasspath in Runtime in propgen) map {
+				(outdir, cp) => runPropertyGenerator(outdir, cp.files)
+			}
+	)
+
+	def runPropertyGenerator(outdir : File, cp: Seq[File]): Seq[File] = {
+		val mainClass = "daffodil.propGen.PropertyGenerator"
+		val out = new java.io.ByteArrayOutputStream()
+		val ret = new Fork.ForkScala(mainClass).fork(None, Nil, cp, Seq(outdir.toString), None, false, CustomOutput(out)).exitValue()
+		if (ret != 0) {
+			sys.error("Failed to generate code")
+		}
+		val in = new java.io.InputStreamReader(new java.io.ByteArrayInputStream(out.toByteArray))
+		val bin = new java.io.BufferedReader(in)
+		val iterator = Iterator.continually(bin.readLine()).takeWhile(_ != null)
+		val files = iterator.map(f => new File(f)).toList
+		files	
+	}
+
+	// modify the managed source directories so that any generated code can be more easily included in IDE's
+	s ++= Seq(sourceManaged <<= baseDirectory(_ / "src_managed"))
 
 	// creates 'sbt debug:*' tasks, using src/test/scala-debug as the source directory
 	lazy val DebugTest = config("debug") extend(Test)
