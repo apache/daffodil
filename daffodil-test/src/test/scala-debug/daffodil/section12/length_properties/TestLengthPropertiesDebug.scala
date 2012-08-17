b2a6027be11e346b289e6f09a68c460e240f2038
Paul Chu <pchu@tresys.com>
2012-08-17 10:30:47 -0400

    Test, LengthProp_01, causes DFDL processor to hang.  It uses a lot of memory and I needed to go into Task Manager to end the javaw.exe process

---
+++ daffodil-test/src/test/scala-debug/daffodil/section12/length_properties/TestLengthPropertiesDebug.scala
@@ -0,0 +1,26 @@
+package daffodil.section12.length_properties
+
+import junit.framework.Assert._
+import org.scalatest.junit.JUnit3Suite
+import scala.xml._
+import daffodil.xml.XMLUtils
+import daffodil.xml.XMLUtils._
+import daffodil.compiler.Compiler
+import daffodil.util._
+import daffodil.tdml.DFDLTestSuite
+import java.io.File
+
+class TestLengthPropertiesDebug extends JUnit3Suite {
+  val testDir_01 = "/daffodil/ibm-tests/"
+  val tdml_01 = testDir_01 + "dpaext1.tdml"
+  val runner_01 = new DFDLTestSuite(Misc.getRequiredResource(tdml_01))
+  
+  //def test_length_explicit_12_01() { runner_01.runOneTest("length_explicit_12_01") }
+  //def test_length_explicit_12_02() { runner_01.runOneTest("length_explicit_12_02") }
+  
+  val testDir_02 = "/daffodil/section12/length_properties/"
+  val tdml_02 = testDir_02 + "LengthProperties.tdml"
+  val runner_02 = new DFDLTestSuite(Misc.getRequiredResource(tdml_02))
+  
+  def test_LengthProp_01() { runner_02.runOneTest("LengthProp_01") }
+  }
