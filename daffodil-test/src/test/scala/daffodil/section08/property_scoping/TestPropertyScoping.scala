433b2a97dc8ad799b53c785421721e37f0c3005e
Paul Chu <pchu@tresys.com>
2012-08-17 16:51:46 -0400

    Added NearestEnclosingSequenceElementRef test to close DFDL-275

--- daffodil-test/src/test/scala/daffodil/section08/property_scoping/TestPropertyScoping.scala
+++ daffodil-test/src/test/scala/daffodil/section08/property_scoping/TestPropertyScoping.scala
@@ -24,4 +24,5 @@ class TestPropertyScoping extends JUnit3Suite {
   //def test_property_scoping_03() { runner_01.runOneTest("property_scoping_03") }
   def test_property_scoping_04() { runner_01.runOneTest("property_scoping_04") }
   def test_property_scoping_05() { runner_01.runOneTest("property_scoping_05") }
+  def testNearestEnclosingSequenceElementRef() { runner_01.runOneTest("NearestEnclosingSequenceElementRef") }
 }
