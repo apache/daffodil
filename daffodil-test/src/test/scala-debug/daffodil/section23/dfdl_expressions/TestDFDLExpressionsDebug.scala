3ddebca28e5c3a5bd78bdf208c1047134ac015b5
Paul Chu <pchu@tresys.com>
2012-09-19 13:58:56 -0400

    length expression fails using utf-8 encoding

--- daffodil-test/src/test/scala-debug/daffodil/section23/dfdl_expressions/TestDFDLExpressionsDebug.scala
+++ daffodil-test/src/test/scala-debug/daffodil/section23/dfdl_expressions/TestDFDLExpressionsDebug.scala
@@ -17,6 +17,5 @@ class TestDFDLExpressionsDebug extends JUnit3Suite {
   
   // ArrayOptElem_02 should pass when binary unsignedInt is implemented.
   def test_ArrayOptElem_02() { runner.runOneTest("ArrayOptElem_02") }
-//  def test_expressions_lke2_rel() { runner.runOneTest("lke2_rel") }
   def test_expressions_lke3_rel() { runner.runOneTest("lke3_rel") }
   }
