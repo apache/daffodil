901145795477ad322be5504ca8b75c0546434f2f
Jessie Chab <jchab@tresys.com>
2012-09-21 12:34:32 -0700

    Adding 12 facets tests

--- daffodil-test/src/test/scala/daffodil/section05/facets/TestFacets.scala
+++ daffodil-test/src/test/scala/daffodil/section05/facets/TestFacets.scala
@@ -16,6 +16,17 @@ class TestFacets extends JUnitSuite {
   val aa = testDir + "Facets.tdml"
   lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa))
   
-  @Test def test_error_minOccurs() { runner.runOneTest("error_minOccurs") }
-  
+    def test_correctNumCells0() { runner.runOneTest("correctNumCells0") }
+    def test_extraCellsParsed() { runner.runOneTest("extraCellsParsed") }
+    def test_scalarElements() { runner.runOneTest("scalarElements") }
+    def test_lessThanMinBasic() { runner.runOneTest("lessThanMinBasic") }
+    def test_moreThanMaxBasic() { runner.runOneTest("moreThanMaxBasic") }
+    def test_arrayElements() { runner.runOneTest("arrayElements") }
+    def test_upToMaxParsed() { runner.runOneTest("upToMaxParsed") }
+    def test_lessThanMinCells() { runner.runOneTest("lessThanMinCells") }
+    def test_moreThanMinCells() { runner.runOneTest("moreThanMinCells") }
+    def test_lessThanMinCellsAfterLargeRow() { runner.runOneTest("lessThanMinCellsAfterLargeRow") }
+    def test_largeNumRows() { runner.runOneTest("largeNumRows") }
+    def test_minMaxDoNotMatch() { runner.runOneTest("minMaxDoNotMatch") }
+
   }
