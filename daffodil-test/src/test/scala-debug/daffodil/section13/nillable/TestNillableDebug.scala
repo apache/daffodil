68302570bc2dc61c3e0289a0ebb0381c36a5db97
Paul Chu <pchu@tresys.com>
2012-10-15 15:17:54 -0400

    nil test fails when dfdl:nilKind is literalValue, dfdl:nilValue is %ES;, the type is complex

---
+++ daffodil-test/src/test/scala-debug/daffodil/section13/nillable/TestNillableDebug.scala
@@ -0,0 +1,23 @@
+package daffodil.section13.nillable
+
+
+import junit.framework.Assert._
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
+import scala.xml._
+import daffodil.xml.XMLUtils
+import daffodil.xml.XMLUtils._
+import daffodil.compiler.Compiler
+import daffodil.util._
+import daffodil.tdml.DFDLTestSuite
+import java.io.File
+import daffodil.debugger.Debugger.withDebugger
+
+class TestNillableDebug extends JUnitSuite {
+  val testDir = "/daffodil/section13/nillable/"
+  val aa = testDir + "nillable.tdml"
+  lazy val runnerAA = new DFDLTestSuite(Misc.getRequiredResource(aa))
+
+  @Test def test_litNil4() { runnerAA.runOneTest("litNil4") }
+
+}
