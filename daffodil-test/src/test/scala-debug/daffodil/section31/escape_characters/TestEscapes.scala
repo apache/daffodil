c0e37e0b9cebb2821a7a4699e9e95d97997df9e8
Jessie Chab <jchab@tresys.com>
2012-10-24 09:49:00 -0700

    Creating tests for escape scheme use cases and moving failing tests to scala-debug - DFDL-301, DFDL-377

---
+++ daffodil-test/src/test/scala-debug/daffodil/section31/escape_characters/TestEscapes.scala
@@ -0,0 +1,23 @@
+package daffodil.section31.escape_characters
+
+import junit.framework.Assert._
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
+import scala.xml._
+import daffodil.xml.XMLUtils
+import daffodil.xml.XMLUtils._
+import daffodil.compiler.Compiler
+import daffodil.util._
+import daffodil.tdml.DFDLTestSuite
+import java.io.File
+
+class TestEscapes extends JUnitSuite {
+  val testDir = "/daffodil/section31/escape_characters/"
+  val tdml = testDir + "Escapes.tdml"
+  lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(tdml))
+  
+  @Test def test_escapes10() { runner.runOneTest("escape_entry10") }
+  @Test def test_escapes2_10() { runner.runOneTest("escape_entry2-10") }
+  @Test def test_escapes2_14() { runner.runOneTest("escape_entry2-14") }
+
+  }
