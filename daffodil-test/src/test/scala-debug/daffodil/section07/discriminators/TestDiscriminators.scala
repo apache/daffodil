9c2936b7bb05fd9c1237fdf8bc50c6bc10a8aa95
Jessie Chab <jchab@tresys.com>
2012-10-24 11:06:12 -0700

    Attempting to create tests to verify DFDL-371 - Discriminator evaluates even if element fails. Test are currently failing.

---
+++ daffodil-test/src/test/scala-debug/daffodil/section07/discriminators/TestDiscriminators.scala
@@ -0,0 +1,18 @@
+package daffodil.section07.discriminators
+
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
+import junit.framework.Assert._
+import daffodil.tdml.DFDLTestSuite
+import daffodil.util.Misc
+import daffodil.debugger.Debugger
+
+class TestDiscriminators extends JUnitSuite {
+  val testDir = "/daffodil/section07/discriminators/"
+  val tdml = testDir + "discriminator.tdml"
+  lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(tdml))
+
+  @Test def test_assertions_discriminatorGuidesChoice2() { runner.runOneTest("discriminatorGuidesChoice2") }
+  @Test def test_assertions_discriminatorGuidesChoice3() { runner.runOneTest("discriminatorGuidesChoice3") }
+
+}
