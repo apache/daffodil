ac910021ac4e69dadd945541819f1afa1fa88030
Paul Chu <pchu@tresys.com>
2012-11-06 13:13:32 -0500

    A test with discriminator on a choice branch

---
+++ daffodil-test/src/test/scala-debug/daffodil/section07/discriminators/TestDiscriminatorsDebug.scala
@@ -0,0 +1,17 @@
+package daffodil.section07.discriminators
+
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
+import junit.framework.Assert._
+import daffodil.tdml.DFDLTestSuite
+import daffodil.util.Misc
+import daffodil.debugger.Debugger
+
+class TestDiscriminatorsDebug extends JUnitSuite {
+  val testDir = "/daffodil/section07/discriminators/"
+  val tdml = testDir + "discriminator.tdml"
+  lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(tdml))
+
+  @Test def test_choiceBranchDiscrimFail() { runner.runOneTest("choiceBranchDiscrimFail") }
+
+}
