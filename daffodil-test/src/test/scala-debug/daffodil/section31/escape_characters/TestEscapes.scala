3b15c95216b7c53e3badc33db03f4a0152b94983
Mike Beckerle <mbeckerle.dfdl@gmail.com>
2012-11-13 13:34:34 -0500

    Can't have more than one TestEscapes class. Renamed debug one to TestEscapes2.

--- daffodil-test/src/test/scala-debug/daffodil/section31/escape_characters/TestEscapes.scala
+++ daffodil-test/src/test/scala-debug/daffodil/section31/escape_characters/TestEscapes.scala
@@ -11,13 +11,13 @@ import daffodil.util._
 import daffodil.tdml.DFDLTestSuite
 import java.io.File
 
-class TestEscapes extends JUnitSuite {
+class TestEscapes2 extends JUnitSuite {
   val testDir = "/daffodil/section31/escape_characters/"
   val tdml = testDir + "Escapes.tdml"
   lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(tdml))
-  
+
   @Test def test_escapes10() { runner.runOneTest("escape_entry10") }
   @Test def test_escapes2_10() { runner.runOneTest("escape_entry2-10") }
   @Test def test_escapes2_14() { runner.runOneTest("escape_entry2-14") }
 
-  }
+}
