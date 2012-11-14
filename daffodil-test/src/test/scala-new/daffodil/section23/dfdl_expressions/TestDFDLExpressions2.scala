1dcc035f415bfdff7624e54efe7d4f91617e227b
Mike Beckerle <mbeckerle.dfdl@gmail.com>
2012-11-14 10:22:40 -0500

    Added 3 tests for repeat-flag behavior. This behavior is used by many binary formats, but this test is a simple textual-data test which works by the same principles.
    Also, I left the use of the fn:trace(...) XPath function in place by way
    of illustration to developers/testers how to use it.

--- daffodil-test/src/test/scala-new/daffodil/section23/dfdl_expressions/TestDFDLExpressions2.scala
+++ daffodil-test/src/test/scala-new/daffodil/section23/dfdl_expressions/TestDFDLExpressions2.scala
@@ -10,6 +10,7 @@ import daffodil.compiler.Compiler
 import daffodil.util._
 import daffodil.tdml.DFDLTestSuite
 import java.io.File
+import daffodil.debugger.Debugger
 
 class TestDFDLExpressions2 extends JUnitSuite {
   val testDir = "/daffodil/section23/dfdl_expressions/"
@@ -21,7 +22,11 @@ class TestDFDLExpressions2 extends JUnitSuite {
   //  @Test def test_expressions_lke3_rel() { runner.runOneTest("lke3_rel") }
   //  @Test def test_ocke_rel() { runner.runOneTest("ocke_rel") }
   @Test def test_expresion_bad_path_to_variable() { runner.runOneTest("expresion_bad_path_to_variable") }
-    @Test def test_checkConstraints() { runner.runOneTest("dfdlCheckConstraints") }
-    @Test def test_maxOccursPass(){ runner.runOneTest("checkMaxOccurs_Pass")}
-    @Test def test_maxOccursUnboundedPass(){ runner.runOneTest("checkMaxOccursUnbounded_Pass") }
+  @Test def test_checkConstraints() { runner.runOneTest("dfdlCheckConstraints") }
+  @Test def test_maxOccursPass() { runner.runOneTest("checkMaxOccurs_Pass") }
+  @Test def test_maxOccursUnboundedPass() { runner.runOneTest("checkMaxOccursUnbounded_Pass") }
+
+  @Test def test_repeatFlags1() { runner.runOneTest("repeatFlags1") }
+  @Test def test_repeatFlags2() = { runner.runOneTest("repeatFlags2") }
+  @Test def test_repeatFlags3() { runner.runOneTest("repeatFlags3") }
 }
