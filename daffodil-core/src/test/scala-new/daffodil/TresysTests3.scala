a5f7d8d4a358fd754087490e1a069f4f48d5123b
Paul Chu <pchu@tresys.com>
2012-11-20 12:13:15 -0500

    Removed duplicate tests for DFDL expressions (DFDL-140)

--- daffodil-core/src/test/scala-new/daffodil/TresysTests3.scala
+++ daffodil-core/src/test/scala-new/daffodil/TresysTests3.scala
@@ -17,14 +17,6 @@ import daffodil.debugger.Debugger
 class TresysTests3 extends JUnitSuite {
   val testDir = "/test-suite/tresys-contributed/"
 
-  lazy val runnerex = new DFDLTestSuite(Misc.getRequiredResource(testDir + "expressions.tdml"))
-  @Test def test_nonFunctionIsDetected() = { runnerex.runOneTest("nonFunctionIsDetected") }
-  @Test def test_constantFunction1() = Debugger.withDebugger { runnerex.runOneTest("constantFunction1") }
-  @Test def test_dfdlPosition1() = { runnerex.runOneTest("dfdlPosition1") }
-  @Test def test_dfdlPosition2() = { runnerex.runOneTest("dfdlPosition2") }
-  @Test def test_dfdlPosition3() = { runnerex.runOneTest("dfdlPosition3") }
-  @Test def test_dfdlPosition4() = { runnerex.runOneTest("dfdlPosition4") }
-
   val nsd = testDir + "nested-separator-delimited.tdml"
   lazy val runnerNSD = new DFDLTestSuite(Misc.getRequiredResource(nsd))
 
--- daffodil-test/src/test/scala/daffodil/section23/dfdl_expressions/TestDFDLExpressions.scala
+++ daffodil-test/src/test/scala/daffodil/section23/dfdl_expressions/TestDFDLExpressions.scala
@@ -30,6 +30,8 @@ class TestDFDLExpressions extends JUnitSuite {
   //@Test def test_ocke_rel() { runner.runOneTest("ocke_rel") }
   //@Test def test_expresion_bad_path_to_variable() { runner.runOneTest("expresion_bad_path_to_variable") }
 
+  @Test def test_nonFunctionIsDetected() = { runner.runOneTest("nonFunctionIsDetected") }
+  @Test def test_constantFunction1() = Debugger.withDebugger { runner.runOneTest("constantFunction1") }
   @Test def test_dfdlPosition1() = Debugger.withDebugger{ runner.runOneTest("dfdlPosition1") }
   @Test def test_dfdlPosition2() { runner.runOneTest("dfdlPosition2") }
   @Test def test_dfdlPosition3() { runner.runOneTest("dfdlPosition3") }
