8ac14fb06a3c35aa891d7ef70220ac848f87573b
Jessie Chab <jchab@tresys.com>
2012-12-21 11:45:19 -0800

    Adding negative tests for escapeSchemes

--- daffodil-test/src/test/scala-debug/daffodil/section07/escapeScheme/TestEscapeSchemeDebug.scala
+++ daffodil-test/src/test/scala-debug/daffodil/section07/escapeScheme/TestEscapeSchemeDebug.scala
@@ -11,13 +11,11 @@ import daffodil.util._
 import daffodil.tdml.DFDLTestSuite
 import java.io.File
 
-class TestEscapeScheme extends JUnitSuite {
+class TestEscapeSchemeDebug extends JUnitSuite {
   val testDir = "/daffodil/section07/escapeScheme/"
   val tdml = testDir + "escapeScheme.tdml"
   lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(tdml))
 
-  @Test def test_escape_scheme() { runner.runOneTest("escapeSchemeSimple") }
-  @Test def test_escapeSchemeFail() { runner.runOneTest("escapeSchemeFail") }
-//  @Test def test_escapeSchemeFail2() { runner.runOneTest("escapeSchemeFail2") }
+  @Test def test_escapeSchemeFail2() { runner.runOneTest("escapeSchemeFail2") }
 
 }
