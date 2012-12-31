f1a746375b4d5e651a1f0317d081d52820841e15
Paul Chu <pchu@tresys.com>
2012-12-31 11:00:35 -0500

    Added nonNegativeInteger primitive tests.

--- daffodil-test/src/test/scala-debug/daffodil/section05/simple_types/TestSimpleTypes2.scala
+++ daffodil-test/src/test/scala-debug/daffodil/section05/simple_types/TestSimpleTypes2.scala
@@ -61,5 +61,7 @@ class TestSimpleTypes2 extends JUnitSuite {
   @Test def test_padding_nil() { runner.runOneTest("padding_nil") }
   @Test def test_padding_nil2() { runner.runOneTest("padding_nil2") }
   @Test def test_justification_1() { runner.runOneTest("justification_1") }
+  @Test def test_nonNegativeInteger_text() { runner.runOneTest("nonNegativeInteger_text") }
+  @Test def test_nonNegativeInteger_bin() { runner.runOneTest("nonNegativeInteger_bin") }
 
 }
