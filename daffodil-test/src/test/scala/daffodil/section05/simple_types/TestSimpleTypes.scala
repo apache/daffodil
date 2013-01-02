cb71a03bc619ac4fcd24a58a07a00efa757f07ce
Jessie Chab <jchab@tresys.com>
2013-01-02 11:59:44 -0800

    Modified test padding_escape so the escape and padding characters are not identical. This test demonstrates DFDL-7-089R: On parsing, the escape scheme is applied after padding characters are trimmed (DFDL-259)

--- daffodil-test/src/test/scala/daffodil/section05/simple_types/TestSimpleTypes.scala
+++ daffodil-test/src/test/scala/daffodil/section05/simple_types/TestSimpleTypes.scala
@@ -201,7 +201,7 @@ class TestSimpleTypes extends JUnitSuite {
   @Test def test_number_padding3() { runner.runOneTest("number_padding3") }
   @Test def test_number_padding4() { runner.runOneTest("number_padding4") }
   @Test def test_number_padding5() { runner.runOneTest("number_padding5") }
-//  @Test def test_padding_escape() { runner.runOneTest("padding_escape") }
+  @Test def test_padding_escape() { runner.runOneTest("padding_escape") }
   @Test def test_padding_nil() { runner.runOneTest("padding_nil") }
   @Test def test_padding_nil2() { runner.runOneTest("padding_nil2") }
 //  @Test def test_justification_1() { runner.runOneTest("justification_1") }
