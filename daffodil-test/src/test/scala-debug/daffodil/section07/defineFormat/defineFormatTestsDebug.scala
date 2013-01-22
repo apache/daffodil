5171b1fefb9815dc1771303991dbc7d1d39cca16
Jessie Chab <jchab@tresys.com>
2013-01-22 07:18:10 -0800

    Adding failing test for DFDL-7-033R & DFDL-7-034R (DFDL-478)

--- daffodil-test/src/test/scala-debug/daffodil/section07/defineFormat/defineFormatTestsDebug.scala
+++ daffodil-test/src/test/scala-debug/daffodil/section07/defineFormat/defineFormatTestsDebug.scala
@@ -11,14 +11,11 @@ import daffodil.util._
 import daffodil.tdml.DFDLTestSuite
 import java.io.File
 
-class defineFormatTests extends JUnitSuite {
+class defineFormatTestsDebug extends JUnitSuite {
   val testDir = "/daffodil/section07/defineFormat/"
   val tdml = testDir + "defineFormat.tdml"
   lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(tdml))
 
-  @Test def test_defineFormat_01() { runner.runOneTest("defineFormat_01") }
-  @Test def test_Lesson3_defineFormat() { runner.runOneTest("Lesson3_defineFormat") }
-  @Test def test_Lesson3_inherit_defineFormat() { runner.runOneTest("Lesson3_inherit_defineFormat") }
-//  @Test def test_nameCollision() { runner.runOneTest("nameCollision") }
+  @Test def test_nameCollision() { runner.runOneTest("nameCollision") }
 
 }
