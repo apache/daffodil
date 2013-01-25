33ad775d2e79684e06ab8acb32677c92ee9c4bd4
Steve Lawrence <slawrence@tresys.com>
2013-01-25 14:34:51 -0500

    Reformat sbt configuration to scala coding standards

--- build.sbt
+++ build.sbt
@@ -9,16 +9,16 @@ logBuffered in ThisBuild := false
 transitiveClassifiers := Seq("sources", "javadoc")
 
 libraryDependencies in ThisBuild := Seq(
-	"junit" % "junit" % "4.11",
-	"com.github.stefanbirkner" % "system-rules" % "1.2.0",
-	"org.jdom" % "jdom" % "1.1.3",
-	"net.sourceforge.saxon" % "saxon" % "9.1.0.8" classifier "" classifier "dom" classifier "jdom" classifier "s9api" classifier "xpath",
-	"com.ibm.icu" % "icu4j" % "50.1.1",// classifier "" classifier "charset" classifier "localespi",
-	"org.scalatest" % "scalatest_2.9.1" % "1.6.1",
-	"joda-time" % "joda-time" % "1.6",
-        "xerces" % "xercesImpl" % "2.10.0",
-        "xml-resolver" % "xml-resolver" % "1.2",
-	"org.rogach" %% "scallop" % "0.7.0"
+  "junit" % "junit" % "4.11",
+  "com.github.stefanbirkner" % "system-rules" % "1.2.0",
+  "org.jdom" % "jdom" % "1.1.3",
+  "net.sourceforge.saxon" % "saxon" % "9.1.0.8" classifier "" classifier "dom" classifier "jdom" classifier "s9api" classifier "xpath",
+  "com.ibm.icu" % "icu4j" % "50.1.1",// classifier "" classifier "charset" classifier "localespi",
+  "org.scalatest" % "scalatest_2.9.1" % "1.6.1",
+  "joda-time" % "joda-time" % "1.6",
+  "xerces" % "xercesImpl" % "2.10.0",
+  "xml-resolver" % "xml-resolver" % "1.2",
+  "org.rogach" %% "scallop" % "0.7.0"
 )
 
 retrieveManaged := true
--- project/build.scala
+++ project/build.scala
@@ -6,111 +6,111 @@ import com.typesafe.sbt.SbtStartScript
 
 object DaffodilBuild extends Build {
 
-	var s = Defaults.defaultSettings
-	lazy val nopub = Seq(publish := {}, publishLocal := {})
+  var s = Defaults.defaultSettings
+  lazy val nopub = Seq(publish := {}, publishLocal := {})
 
-	// daffodil projects
-	lazy val root    = Project(id = "daffodil", base = file("."), settings = s ++ nopub)
-	                           .configs(DebugTest)
-	                           .configs(NewTest)
-	                           .aggregate(propgen, lib, core, test)
+  // daffodil projects
+  lazy val root    = Project(id = "daffodil", base = file("."), settings = s ++ nopub)
+                             .configs(DebugTest)
+                             .configs(NewTest)
+                             .aggregate(propgen, lib, core, test)
 
-	lazy val propgen = Project(id = "daffodil-propgen", base = file("daffodil-propgen"), settings = s ++ nopub)
-	                           .configs(DebugTest)
-	                           .configs(NewTest)
+  lazy val propgen = Project(id = "daffodil-propgen", base = file("daffodil-propgen"), settings = s ++ nopub)
+                             .configs(DebugTest)
+                             .configs(NewTest)
 
-	lazy val lib     = Project(id = "daffodil-lib", base = file("daffodil-lib"), settings = s ++ propgenSettings)
-	                           .configs(DebugTest)
-	                           .configs(NewTest)
+  lazy val lib     = Project(id = "daffodil-lib", base = file("daffodil-lib"), settings = s ++ propgenSettings)
+                             .configs(DebugTest)
+                             .configs(NewTest)
 
-	lazy val core    = Project(id = "daffodil-core", base = file("daffodil-core"), settings = s ++ startScriptSettings)
-	                           .configs(DebugTest)
-	                           .configs(NewTest)
-	                           .dependsOn(lib)
+  lazy val core    = Project(id = "daffodil-core", base = file("daffodil-core"), settings = s ++ startScriptSettings)
+                             .configs(DebugTest)
+                             .configs(NewTest)
+                             .dependsOn(lib)
 
-	lazy val test    = Project(id = "daffodil-test", base = file("daffodil-test"), settings = s ++ nopub)
-	                           .configs(DebugTest)
-	                           .configs(NewTest)
-	                           .dependsOn(core)
+  lazy val test    = Project(id = "daffodil-test", base = file("daffodil-test"), settings = s ++ nopub)
+                             .configs(DebugTest)
+                             .configs(NewTest)
+                             .dependsOn(core)
 
-	val propertyGenerator = TaskKey[Seq[File]]("gen-props", "Generate properties scala source")
-	lazy val propgenSettings = Seq(
-		sourceGenerators in Compile <+= (propertyGenerator in Compile),
-		propertyGenerator in Compile <<=
-			(sourceManaged in Compile, dependencyClasspath in Runtime in propgen) map {
-				(outdir, cp) => runPropertyGenerator(outdir, cp.files)
-			}
-	)
+  val propertyGenerator = TaskKey[Seq[File]]("gen-props", "Generate properties scala source")
+  lazy val propgenSettings = Seq(
+    sourceGenerators in Compile <+= (propertyGenerator in Compile),
+    propertyGenerator in Compile <<=
+      (sourceManaged in Compile, dependencyClasspath in Runtime in propgen) map {
+        (outdir, cp) => runPropertyGenerator(outdir, cp.files)
+      }
+  )
 
-	def runPropertyGenerator(outdir : File, cp: Seq[File]): Seq[File] = {
-		val mainClass = "daffodil.propGen.PropertyGenerator"
-		val out = new java.io.ByteArrayOutputStream()
-		val ret = new Fork.ForkScala(mainClass).fork(None, Nil, cp, Seq(outdir.toString), None, false, CustomOutput(out)).exitValue()
-		if (ret != 0) {
-			sys.error("Failed to generate code")
-		}
-		val in = new java.io.InputStreamReader(new java.io.ByteArrayInputStream(out.toByteArray))
-		val bin = new java.io.BufferedReader(in)
-		val iterator = Iterator.continually(bin.readLine()).takeWhile(_ != null)
-		val files = iterator.map(f => new File(f)).toList
-		files	
-	}
+  def runPropertyGenerator(outdir: File, cp: Seq[File]): Seq[File] = {
+    val mainClass = "daffodil.propGen.PropertyGenerator"
+    val out = new java.io.ByteArrayOutputStream()
+    val ret = new Fork.ForkScala(mainClass).fork(None, Nil, cp, Seq(outdir.toString), None, false, CustomOutput(out)).exitValue()
+    if (ret != 0) {
+      sys.error("Failed to generate code")
+    }
+    val in = new java.io.InputStreamReader(new java.io.ByteArrayInputStream(out.toByteArray))
+    val bin = new java.io.BufferedReader(in)
+    val iterator = Iterator.continually(bin.readLine()).takeWhile(_ != null)
+    val files = iterator.map(f => new File(f)).toList
+    files
+  }
 
-	// modify the managed source directories so that any generated code can be more easily included in IDE's
-	s ++= Seq(sourceManaged <<= baseDirectory(_ / "src_managed"))
+  // modify the managed source directories so that any generated code can be more easily included in IDE's
+  s ++= Seq(sourceManaged <<= baseDirectory(_ / "src_managed"))
 
-	// creates 'sbt debug:*' tasks, using src/test/scala-debug as the source directory
-	lazy val DebugTest = config("debug") extend(Runtime)
-	lazy val debugSettings: Seq[Setting[_]] = inConfig(DebugTest)(Defaults.testSettings ++ Seq(
-		sourceDirectory <<= baseDirectory(_ / "src" / "test"),
-		scalaSource <<= sourceDirectory(_ / "scala-debug"),
-		exportJars := false
-	))
-	s ++= Seq(debugSettings : _*)
-	
-	// creates 'sbt debug' task, which is essentially an alias for 'sbt debug:test'
-	lazy val debugTask = TaskKey[Unit]("debug", "Executes all debug tests")
-	lazy val debugTaskSettings = debugTask <<= (executeTests in DebugTest, streams in DebugTest, resolvedScoped in DebugTest, state in DebugTest) map {
-		(results, s, scoped, state) => {
-			val display = Project.showContextKey(state)
-			Tests.showResults(s.log, results, "No tests to run for " + display(scoped))
-		}
-	}
-	s ++= Seq(debugTaskSettings)
+  // creates 'sbt debug:*' tasks, using src/test/scala-debug as the source directory
+  lazy val DebugTest = config("debug") extend(Runtime)
+  lazy val debugSettings: Seq[Setting[_]] = inConfig(DebugTest)(Defaults.testSettings ++ Seq(
+    sourceDirectory <<= baseDirectory(_ / "src" / "test"),
+    scalaSource <<= sourceDirectory(_ / "scala-debug"),
+    exportJars := false
+  ))
+  s ++= Seq(debugSettings : _*)
 
-	
-	// creates 'sbt new:*' tasks, using src/test/scala-new as the source directory
-	lazy val NewTest = config("new") extend(Runtime)
-	lazy val newSettings: Seq[Setting[_]] = inConfig(NewTest)(Defaults.testSettings ++ Seq(
-		sourceDirectory <<= baseDirectory(_ / "src" / "test"),
-		scalaSource <<= sourceDirectory(_ / "scala-new"),
-		exportJars := false
-	))
-	s ++= Seq(newSettings : _*)
-	
-	// creates 'sbt new' task, which is essentially an alias for 'sbt new:test'
-	lazy val newTask = TaskKey[Unit]("new", "Executes all new tests")
-	lazy val newTaskSettings = newTask <<= (executeTests in NewTest, streams in NewTest, resolvedScoped in NewTest, state in NewTest) map {
-		(results, s, scoped, state) => {
-			val display = Project.showContextKey(state)
-			Tests.showResults(s.log, results, "No tests to run for " + display(scoped))
-		}
-	}
-	s ++= Seq(newTaskSettings)
+  // creates 'sbt debug' task, which is essentially an alias for 'sbt debug:test'
+  lazy val debugTask = TaskKey[Unit]("debug", "Executes all debug tests")
+  lazy val debugTaskSettings = debugTask <<= (executeTests in DebugTest, streams in DebugTest, resolvedScoped in DebugTest, state in DebugTest) map {
+    (results, s, scoped, state) => {
+      val display = Project.showContextKey(state)
+      Tests.showResults(s.log, results, "No tests to run for " + display(scoped))
+    }
+  }
+  s ++= Seq(debugTaskSettings)
 
-	// add scala-new as a source test directory for the 'sbt test' commands
-	lazy val buildNewWithTestSettings = unmanagedSourceDirectories in Test <++= baseDirectory { base =>
-		Seq(base / "src/test/scala-new")
-	}
-	s ++= Seq(buildNewWithTestSettings)	
 
-	
-	// jacoco configuration
-	s ++= Seq(jacoco.settings : _*)
+  // creates 'sbt new:*' tasks, using src/test/scala-new as the source directory
+  lazy val NewTest = config("new") extend(Runtime)
+  lazy val newSettings: Seq[Setting[_]] = inConfig(NewTest)(Defaults.testSettings ++ Seq(
+    sourceDirectory <<= baseDirectory(_ / "src" / "test"),
+    scalaSource <<= sourceDirectory(_ / "scala-new"),
+    exportJars := false
+  ))
+  s ++= Seq(newSettings : _*)
 
-	// start-script configuration	
-	lazy val startScriptSettings = Seq(SbtStartScript.startScriptForJarSettings : _*) ++
-	                               Seq(mainClass in Compile := Some("daffodil.Main"))
+  // creates 'sbt new' task, which is essentially an alias for 'sbt new:test'
+  lazy val newTask = TaskKey[Unit]("new", "Executes all new tests")
+  lazy val newTaskSettings = newTask <<= (executeTests in NewTest, streams in NewTest, resolvedScoped in NewTest, state in NewTest) map {
+    (results, s, scoped, state) => {
+      val display = Project.showContextKey(state)
+      Tests.showResults(s.log, results, "No tests to run for " + display(scoped))
+    }
+  }
+  s ++= Seq(newTaskSettings)
+
+  // add scala-new as a source test directory for the 'sbt test' commands
+  lazy val buildNewWithTestSettings = unmanagedSourceDirectories in Test <++= baseDirectory { base =>
+    Seq(base / "src/test/scala-new")
+  }
+  s ++= Seq(buildNewWithTestSettings)
+
+
+  // jacoco configuration
+  s ++= Seq(jacoco.settings : _*)
+
+  // start-script configuration
+  lazy val startScriptSettings = Seq(SbtStartScript.startScriptForJarSettings : _*) ++
+                                 Seq(mainClass in Compile := Some("daffodil.Main"))
 
   // get the version from the latest tag
   s ++= Seq(version := {
