be38232299acb5566e258bdd5d4714646bee0031
Jessie Chab <jchab@tresys.com>
2013-02-04 08:04:07 -0800

    add arrayExpressions tests adding runtime-valued properties tests for when they've been implemented
    DFDL-545

--- daffodil-test/src/test/scala-debug/edu/illinois/ncsa/daffodil/section16/array_optional_elem/TestArrayOptionalElemDebug.scala
+++ daffodil-test/src/test/scala-debug/edu/illinois/ncsa/daffodil/section16/array_optional_elem/TestArrayOptionalElemDebug.scala
@@ -43,36 +43,12 @@ import edu.illinois.ncsa.daffodil.util._
 import edu.illinois.ncsa.daffodil.tdml.DFDLTestSuite
 import java.io.File
 
-class TestArrayOptionalElem {
+class TestArrayOptionalElemDebug {
   val testDir = "/edu/illinois/ncsa/daffodil/section16/array_optional_elem/"
   val aa = testDir + "ArrayOptionalElem.tdml"
 
   lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa))
   
-  @Test def test_arrayExpressions01() { runner.runOneTest("arrayExpressions01") }
-//  @Test def test_arrayExpressions02() { runner.runOneTest("arrayExpressions02") }
-//  @Test def test_arrayExpressions03() { runner.runOneTest("arrayExpressions03") }
-  @Test def test_arrayExpressions04() { runner.runOneTest("arrayExpressions04") }
-  
-  @Test def test_error01() { runner.runOneTest("error01") }
-  @Test def test_postfixNoErr() { runner.runOneTest("postfixNoErr") }
-
-  @Test def test_optionalElem() { runner.runOneTest("optionalElem") }
-  @Test def test_optionalWithSeparators() { runner.runOneTest("optionalWithSeparators") }
-  @Test def test_Lesson6_optional_element() { runner.runOneTest("Lesson6_optional_element") }
-  @Test def test_Lesson6_optional_element_01() { runner.runOneTest("Lesson6_optional_element_01") }
-  @Test def test_Lesson6_fixed_array() { runner.runOneTest("Lesson6_fixed_array") }
-  @Test def test_Lesson6_variable_array() { runner.runOneTest("Lesson6_variable_array") }
-  @Test def test_Lesson6_variable_array_01() { runner.runOneTest("Lesson6_variable_array_01") }
-  @Test def test_Lesson6_variable_array_02() { runner.runOneTest("Lesson6_variable_array_02") }
-
-  val testDir01 = "/edu/illinois/ncsa/daffodil/section05/facets/"
-  val ab = testDir01 + "Facets.tdml"
-  lazy val runner01 = new DFDLTestSuite(Misc.getRequiredResource(ab))
-  @Test def test_leftOverData_Neg() { runner01.runOneTest("leftOverData_Neg") }
-  
-  val testDir1 = "/edu/illinois/ncsa/daffodil/ibm-tests/"
-  val tdml1 = testDir1 + "dpaext2.tdml"
-  lazy val runner1 = new DFDLTestSuite(Misc.getRequiredResource(tdml1))
-  @Test def test_arrays_16_01() { runner1.runOneTest("arrays_16_01") }
+  @Test def test_arrayExpressions02() { runner.runOneTest("arrayExpressions02") }
+  @Test def test_arrayExpressions03() { runner.runOneTest("arrayExpressions03") }
 }
