1396d72a6fa803bb69dd9a54fac63e85a15645d6
Jessie Chab <jchab@tresys.com>
2013-02-06 13:07:31 -0800

    Adding facet validation tests, some of which currently fail (DFDL-387) Moving developer tests from scala-new to scala

--- daffodil-test/src/test/scala-debug/daffodil/section05/facets/TestFacetsDebug.scala
+++ daffodil-test/src/test/scala-debug/daffodil/section05/facets/TestFacetsDebug.scala
@@ -17,10 +17,46 @@ class TestFacetsDebug extends JUnitSuite {
   val aa = testDir + "Facets.tdml"
   lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa))
 
-  @Test def test_maxLength01() { runner.runOneTest("maxLength01") }
-  @Test def test_maxLength02() { runner.runOneTest("maxLength02") }
-  @Test def test_maxLength03() { runner.runOneTest("maxLength03") }
+  // Note, these won't pass until Decimal is implemented
+  //@Test def test_totalDigits_Pass_Decimal{ runner.runOneTest("checkTotalDigits_Pass_Decimal") }
+  //@Test def test_totalDigits_Fail_Decimal{ runner.runOneTest("checkTotalDigits_Fail_Decimal") }
+  //@Test def test_fractionDigits_Pass{ runner.runOneTest("checkFractionDigits_Pass") }
+  //@Test def test_fractionDigits_Fail{ runner.runOneTest("checkFractionDigits_Fail") }
+  //@Test def test_fractionDigits_Pass_LessDigits{ runner.runOneTest("checkFractionDigits_Pass_LessDigits")} 
+  //@Test def test_totalDigitsAndFractionDigits_Pass { runner.runOneTest("checkTotalDigitsFractionDigits_Pass")}  
+  //@Test def test_totalDigitsAndFractionDigits_Fail { runner.runOneTest("checkTotalDigitsFractionDigits_Fail")}  
 
-  @Test def test_maxOccursFail() { runner.runOneTest("checkMaxOccurs_Fail") }
+//  @Test def test_fractionDigitsPass() { runner.runOneTest("fractionDigitsPass") }
+//  @Test def test_fractionDigitsFail() { runner.runOneTest("fractionDigitsFail") }
+//  @Test def test_fractionDigitsFailNeg() { runner.runOneTest("fractionDigitsFailNeg") }
+//  @Test def test_fractionTotalDigitsFail() { runner.runOneTest("fractionTotalDigitsFail") }
+//  @Test def test_fractionTotalDigitsPass() { runner.runOneTest("fractionTotalDigitsPass") }
+//  @Test def test_fractionTotalDigitsFail2() { runner.runOneTest("fractionTotalDigitsFail2") }
+//  @Test def test_fractionTotalDigitsFail3() { runner.runOneTest("fractionTotalDigitsFail3") }
+//  @Test def test_fractionDigitsFailNotInt() { runner.runOneTest("fractionDigitsFailNotInt") }
+
+//  @Test def test_totalDigits01() { runner.runOneTest("totalDigits01") }
+//  @Test def test_totalDigits02() { runner.runOneTest("totalDigits02") }
+//  @Test def test_totalDigits03() { runner.runOneTest("totalDigits03") }
+//  @Test def test_totalDigits04() { runner.runOneTest("totalDigits04") }
+
+//  @Test def test_totalDigits05b() { runner.runOneTest("totalDigits05b") }
+
+//  @Test def test_facetEnum06() { runner.runOneTest("facetEnum06") }
+  
+//  @Test def test_minMaxInEx02() { runner.runOneTest("minMaxInEx02") }
+//  @Test def test_minMaxInEx04() { runner.runOneTest("minMaxInEx04") }
+//  @Test def test_minMaxInEx08() { runner.runOneTest("minMaxInEx08") }
+//  @Test def test_minMaxInEx10() { runner.runOneTest("minMaxInEx10") }
+//  @Test def test_minMaxInEx14() { runner.runOneTest("minMaxInEx14") }
+
+// Issues with date and time and how they are interpreted with min/max/in/exclusive facets
+
+//  @Test def test_minMaxInExdateTime01() { runner.runOneTest("minMaxInExdateTime01") }
+//  @Test def test_minMaxInExdateTime02() { runner.runOneTest("minMaxInExdateTime02") }
+//  @Test def test_minMaxInExdateTime03() { runner.runOneTest("minMaxInExdateTime03") }
+//  @Test def test_minMaxInExdateTime04() { runner.runOneTest("minMaxInExdateTime04") }
+//  @Test def test_minMaxInExdateTime05() { runner.runOneTest("minMaxInExdateTime05") }
+//  @Test def test_minMaxInExdateTime06() { runner.runOneTest("minMaxInExdateTime06") }
 
 }
--- daffodil-test/src/test/scala/daffodil/section05/facets/TestFacets.scala
+++ daffodil-test/src/test/scala/daffodil/section05/facets/TestFacets.scala
@@ -15,7 +15,115 @@ import daffodil.debugger.Debugger
 class TestFacets extends JUnitSuite {
   val testDir = "/daffodil/section05/facets/"
   val aa = testDir + "Facets.tdml"
-  lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa))
+  lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa), validateTDMLFile = false)
+
+  @Test def test_facet_pattern_01() { runner.runOneTest("facetPattern01") }
+  @Test def test_facet_pattern_02() { runner.runOneTest("facetPattern02") }
+  @Test def test_facet_pattern_03() { runner.runOneTest("facetPattern03") }
+  @Test def test_facet_pattern_04() { runner.runOneTest("facetPattern04") }
+  @Test def test_facet_pattern_05() { runner.runOneTest("facetPattern05") }
+  @Test def test_facet_pattern_06() { runner.runOneTest("facetPattern06") }
+  @Test def test_facet_pattern_07() { runner.runOneTest("facetPattern07") }
+  @Test def test_facet_pattern_08() { runner.runOneTest("facetPattern08") }
+  @Test def test_facet_pattern_09() { runner.runOneTest("facetPattern09") }
+
+  @Test def test_facetEnum01() { runner.runOneTest("facetEnum01") }
+  @Test def test_facetEnum02() { runner.runOneTest("facetEnum02") }
+  @Test def test_facetEnum03() { runner.runOneTest("facetEnum03") }
+  @Test def test_facetEnum04() { runner.runOneTest("facetEnum04") }
+  @Test def test_facetEnum05() { runner.runOneTest("facetEnum05") }
+//  @Test def test_facetEnum06() { runner.runOneTest("facetEnum06") }
+  @Test def test_facetEnum07() { runner.runOneTest("facetEnum07") }
+
+  @Test def test_maxLength01() { runner.runOneTest("maxLength01") }
+  @Test def test_maxLength02() { runner.runOneTest("maxLength02") }
+  @Test def test_maxLength03() { runner.runOneTest("maxLength03") }
+
+//  @Test def test_totalDigits01() { runner.runOneTest("totalDigits01") }
+//  @Test def test_totalDigits02() { runner.runOneTest("totalDigits02") }
+//  @Test def test_totalDigits03() { runner.runOneTest("totalDigits03") }
+//  @Test def test_totalDigits04() { runner.runOneTest("totalDigits04") }
+  @Test def test_totalDigits05() { runner.runOneTest("totalDigits05") }
+//  @Test def test_totalDigits05b() { runner.runOneTest("totalDigits05b") }
+  @Test def test_totalDigits06() { runner.runOneTest("totalDigits06") }
+ 
+//  @Test def test_fractionDigitsPass() { runner.runOneTest("fractionDigitsPass") }
+//  @Test def test_fractionDigitsFail() { runner.runOneTest("fractionDigitsFail") }
+//  @Test def test_fractionDigitsFailNeg() { runner.runOneTest("fractionDigitsFailNeg") }
+//  @Test def test_fractionTotalDigitsFail() { runner.runOneTest("fractionTotalDigitsFail") }
+//  @Test def test_fractionTotalDigitsPass() { runner.runOneTest("fractionTotalDigitsPass") }
+//  @Test def test_fractionTotalDigitsFail2() { runner.runOneTest("fractionTotalDigitsFail2") }
+//  @Test def test_fractionTotalDigitsFail3() { runner.runOneTest("fractionTotalDigitsFail3") }
+//  @Test def test_fractionDigitsFailNotInt() { runner.runOneTest("fractionDigitsFailNotInt") }
+ 
+  @Test def test_arraysMinOccursZero() { runner.runOneTest("arraysMinOccursZero") }
+  @Test def test_arraysOccurInRange_01() { runner.runOneTest("arraysOccursInRange_01") }
+  @Test def test_arraysOccurInRange_02() { runner.runOneTest("arraysOccursInRange_02") }
+  @Test def test_arraysOccurInRange_03() { runner.runOneTest("arraysOccursInRange_03") }
+  @Test def test_arraysOccurInRange_04() { runner.runOneTest("arraysOccursInRange_04") }
+  @Test def test_arraysOccurInRange_05() { runner.runOneTest("arraysOccursInRange_05") }
+  @Test def test_arraysOccursOutOfRange_01() { runner.runOneTest("arraysOccursOutOfRange_01") }
+  @Test def test_arraysOccursOutOfRange_02() { runner.runOneTest("arraysOccursOutOfRange_02") }
+
+  @Test def test_minInclusive_Pass { runner.runOneTest("checkMinInclusive_Pass")}
+  @Test def test_maxInclusive_Pass { runner.runOneTest("checkMaxInclusive_Pass")}
+  @Test def test_minInclusive_Fail { runner.runOneTest("checkMinInclusive_Fail")}
+  @Test def test_maxInclusive_Fail { runner.runOneTest("checkMaxInclusive_Fail")}
+  @Test def test_maxInclusive_Pass_MaxInt { runner.runOneTest("checkMaxInclusive_Pass_MaxInt")}  
+  @Test def test_maxInclusive_Fail_MaxInt { runner.runOneTest("checkMaxInclusive_Fail_MaxInt")}
+  @Test def test_minInclusive_Fail_MinInt { runner.runOneTest("checkMinInclusive_Fail_MinInt")}  
+  
+  @Test def test_minExclusive_Fail { runner.runOneTest("checkMinExclusive_Fail")}
+  @Test def test_maxExclusive_Fail { runner.runOneTest("checkMaxExclusive_Fail")}
+  @Test def test_minExclusive_Pass { runner.runOneTest("checkMinExclusive_Pass")}
+  @Test def test_maxExclusive_Pass { runner.runOneTest("checkMaxExclusive_Pass")}
+  @Test def test_combining_Pass { runner.runOneTest("checkCombining_Pass")}
+  @Test def test_combining_Fail { runner.runOneTest("checkCombining_Fail")}
+  @Test def test_combining_Fail_1 { runner.runOneTest("checkCombining_Fail_1")}
+  
+  @Test def test_enumeration_Pass { runner.runOneTest("checkEnumeration_Pass")}
+  @Test def test_enumeration_Fail { runner.runOneTest("checkEnumeration_Fail")}
+  @Test def test_enumeration_Pass_Subset { runner.runOneTest("checkEnumeration_Pass_Subset")}
+  @Test def test_enumeration_Fail_Subset { runner.runOneTest("checkEnumeration_Fail_Subset")}
+  
+  // Satisfied that Date and Time should also work if DateTime works.
+  @Test def test_maxInclusive_Pass_DateTime { runner.runOneTest("checkMaxInclusive_Pass_DateTime")}
+  @Test def test_maxInclusive_Fail_DateTime { runner.runOneTest("checkMaxInclusive_Fail_DateTime")}
+
+  @Test def test_minLength_Pass { runner.runOneTest("checkMinLength_Pass") }
+  @Test def test_minLength_Fail { runner.runOneTest("checkMinLength_Fail") }
+  @Test def test_minLength_Fail_NotString { runner.runOneTest("checkMinLength_Fail_NotString") }
+  @Test def test_minLength_Fail_Combining { runner.runOneTest("checkMinLength_Fail_Combining") }
+  @Test def test_minLength_Pass_Combining { runner.runOneTest("checkMinLength_Pass_Combining") }
+  @Test def test_maxLength_Pass { runner.runOneTest("checkMaxLength_Pass") }
+  @Test def test_maxLength_Fail { runner.runOneTest("checkMaxLength_Fail") }
+  @Test def test_maxLength_Fail_NotString { runner.runOneTest("checkMaxLength_Fail_NotString") }
+  @Test def test_maxLength_Fail_Combining { runner.runOneTest("checkMaxLength_Fail_Combining") }
+  @Test def test_maxLength_Pass_Combining { runner.runOneTest("checkMaxLength_Pass_Combining") }
+  @Test def test_totalDigits_Pass{ runner.runOneTest("checkTotalDigits_Pass") }
+  @Test def test_totalDigits_Fail{ runner.runOneTest("checkTotalDigits_Fail") }
+ 
+  @Test def test_minMaxInEx01() { runner.runOneTest("minMaxInEx01") }
+//  @Test def test_minMaxInEx02() { runner.runOneTest("minMaxInEx02") }
+  @Test def test_minMaxInEx03() { runner.runOneTest("minMaxInEx03") }
+//  @Test def test_minMaxInEx04() { runner.runOneTest("minMaxInEx04") }
+  @Test def test_minMaxInEx05() { runner.runOneTest("minMaxInEx05") }
+  @Test def test_minMaxInEx06() { runner.runOneTest("minMaxInEx06") }
+  @Test def test_minMaxInEx07() { runner.runOneTest("minMaxInEx07") }
+//  @Test def test_minMaxInEx08() { runner.runOneTest("minMaxInEx08") }
+  @Test def test_minMaxInEx09() { runner.runOneTest("minMaxInEx09") }
+//  @Test def test_minMaxInEx10() { runner.runOneTest("minMaxInEx10") }
+  @Test def test_minMaxInEx11() { runner.runOneTest("minMaxInEx11") }
+  @Test def test_minMaxInEx12() { runner.runOneTest("minMaxInEx12") }
+  @Test def test_minMaxInEx13() { runner.runOneTest("minMaxInEx13") }
+//  @Test def test_minMaxInEx14() { runner.runOneTest("minMaxInEx14") }
+
+//  @Test def test_minMaxInExdateTime01() { runner.runOneTest("minMaxInExdateTime01") }
+//  @Test def test_minMaxInExdateTime02() { runner.runOneTest("minMaxInExdateTime02") }
+//  @Test def test_minMaxInExdateTime03() { runner.runOneTest("minMaxInExdateTime03") }
+//  @Test def test_minMaxInExdateTime04() { runner.runOneTest("minMaxInExdateTime04") }
+//  @Test def test_minMaxInExdateTime05() { runner.runOneTest("minMaxInExdateTime05") }
+//  @Test def test_minMaxInExdateTime06() { runner.runOneTest("minMaxInExdateTime06") }
 
   @Test def test_correctNumCells0() { runner.runOneTest("correctNumCells0") }
   @Test def test_extraCellsParsed() { runner.runOneTest("extraCellsParsed") }
@@ -32,24 +140,9 @@ class TestFacets extends JUnitSuite {
   @Test def test_lessThanMinCellsAfterLargeRow_Neg() { runner.runOneTest("lessThanMinCellsAfterLargeRow_Neg") }
   @Test def test_fixedUnboundedMax() { runner.runOneTest("fixedUnboundedMax") }
   @Test def test_minMaxDoNotMatch() { runner.runOneTest("minMaxDoNotMatch") }
-  @Test def test_facet_pattern_01() { runner.runOneTest("facetPattern01") }
-  @Test def test_facet_pattern_02() { runner.runOneTest("facetPattern02") }
-  @Test def test_facet_pattern_03() { runner.runOneTest("facetPattern03") }
-  @Test def test_facet_pattern_04() { runner.runOneTest("facetPattern04") }
-  @Test def test_facet_pattern_05() { runner.runOneTest("facetPattern05") }
-  @Test def test_facet_pattern_06() { runner.runOneTest("facetPattern06") }
-  @Test def test_facet_pattern_07() { runner.runOneTest("facetPattern07") }
-  @Test def test_facet_pattern_08() { runner.runOneTest("facetPattern08") }
-  @Test def test_facet_pattern_09() { runner.runOneTest("facetPattern09") }
-
-  <!-- Awaiting completion of DFDL-414
-  @Test def test_maxLength01() { runner.runOneTest("maxLength01") }
-  @Test def test_maxLength02() { runner.runOneTest("maxLength02") }
-  @Test def test_maxLength03() { runner.runOneTest("maxLength03") }
--->
 
   @Test def test_maxOccursPass() { runner.runOneTest("checkMaxOccurs_Pass") }
-  //  @Test def test_maxOccursFail() { runner.runOneTest("checkMaxOccurs_Fail") }
+  @Test def test_maxOccursFail() { runner.runOneTest("checkMaxOccurs_Fail") }
   @Test def test_maxOccursUnboundedPass() { runner.runOneTest("checkMaxOccursUnbounded_Pass") }
 
 }
