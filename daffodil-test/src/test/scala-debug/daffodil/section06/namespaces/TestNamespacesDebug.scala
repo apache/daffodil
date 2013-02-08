2b56ee3c1e10776f5e94c29d0376d61988d24511
Mike Beckerle <mbeckerle.dfdl@gmail.com>
2013-02-08 17:44:47 -0500

    fixed TestNamespacesDebug.scala tests.

--- daffodil-test/src/test/scala-debug/daffodil/section06/namespaces/TestNamespacesDebug.scala
+++ daffodil-test/src/test/scala-debug/daffodil/section06/namespaces/TestNamespacesDebug.scala
@@ -11,12 +11,13 @@ import daffodil.util._
 import daffodil.tdml.DFDLTestSuite
 import java.io.File
 
-class TestNamespacesDebug extends JUnitSuite {
-  val testDir = "/daffodil/section06/namespaces/"
-  val aa = testDir + "namespaces.tdml"
-  lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa))
-
-  @Test def test_Lesson2_include_schema() { runner.runOneTest("Lesson2_include_schema") }
-  @Test def test_Lesson2_import_schema() { runner.runOneTest("Lesson2_import_schema") }
-
-}
+//class TestNamespacesDebug extends JUnitSuite {
+//  val testDir = "/daffodil/section06/namespaces/"
+//  val aa = testDir + "namespaces.tdml"
+//  lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa))
+//
+//  // these work. Moved to scala-new
+////  @Test def test_Lesson2_include_schema() { runner.runOneTest("Lesson2_include_schema") }
+////  @Test def test_Lesson2_import_schema() { runner.runOneTest("Lesson2_import_schema") }
+//
+//}
