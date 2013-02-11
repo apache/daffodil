af8d3a0e3bc2aa85765899a84e0ac9fc4e6b27eb
Steve Lawrence <slawrence@tresys.com>
2013-02-11 14:06:59 -0500

    Update copyrights
    DFDL-479

--- daffodil-core/src/main/scala/edu.illinois.ncsa.daffodil/api/Diagnostic.scala
+++ daffodil-core/src/main/scala/edu.illinois.ncsa.daffodil/api/Diagnostic.scala
@@ -1,25 +1,37 @@
 package edu.illinois.ncsa.daffodil.api
 
-/*
- * This file Copyright (c) 2012 Tresys
- * Technology LLC, Columbia, Maryland, USA
+/* Copyright (c) 2012-2013 Tresys Technology, LLC. All rights reserved.
  *
- * This software was developed by Tresys Technology LLC
- * with U.S. Government sponsorship.
- *
- * Tresys Technology LLC grants the U.S. Government and others
- * acting on its behalf, a paid-up non-exclusive, irrevocable
- * worldwide license in this computer software to reproduce,
- * prepare derivative works, and perform publicly and display
- * publicly, by or on behalf of the U.S. Government.
- *
- * All other rights are reserved by Tresys Technology LLC.
- *
- * The copyright notice above and this notice of U.S. Government
- * rights must be included with any such authorized reproductions,
- * derivative works, public displays, and public performances
- * of the computer software.
+ * Developed by: Tresys Technology, LLC
+ *               http://www.tresys.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal with
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimers.
+ * 
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimers in the
+ *     documentation and/or other materials provided with the distribution.
+ * 
+ *  3. Neither the names of Tresys Technology, nor the names of its contributors
+ *     may be used to endorse or promote products derived from this Software
+ *     without specific prior written permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE
+ * SOFTWARE.
  */
+
 
 /**
  * Base trait for all error, warning, info, and other sorts of objects
--- daffodil-lib/src/main/scala/edu.illinois.ncsa.daffodil/util/CopyrightNotices.scala
+++ daffodil-lib/src/main/scala/edu.illinois.ncsa.daffodil/util/CopyrightNotices.scala
@@ -1,5 +1,38 @@
 package edu.illinois.ncsa.daffodil.util
 
+/* Copyright (c) 2012-2013 Tresys Technology, LLC. All rights reserved.
+ *
+ * Developed by: Tresys Technology, LLC
+ *               http://www.tresys.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal with
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimers.
+ * 
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimers in the
+ *     documentation and/or other materials provided with the distribution.
+ * 
+ *  3. Neither the names of Tresys Technology, nor the names of its contributors
+ *     may be used to endorse or promote products derived from this Software
+ *     without specific prior written permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE
+ * SOFTWARE.
+ */
+
+
 /**
  * Put copies of any copyright notices in this class by appending them to the
  * noticesText variable.
@@ -62,7 +95,6 @@ Parts of this software are subject to the following copyright:
   //
   sys.addShutdownHook(useNotices)
 
-  //
   // Note to Tresys: Much of my work on Daffodil is copyright Tresys, as I began
   // working for Tresys on Daffodil on Feb 1, 2012. However, I have contributed 
   // quite a bit of code I created previous to that date to the project, (e.g., 
@@ -72,9 +104,38 @@ Parts of this software are subject to the following copyright:
   //           - Mike Beckerle
   // 
   add("""Copyright (C) 2011, 2012 by Michael J. Beckerle, All rights reserved.
-      Permission is granted to use this software for any purpose so long as 
-      this copyright is preserved in both the source and binary forms, and
-      in any documentation provided with the software. 
+Permission is granted to use this software for any purpose so long as 
+this copyright is preserved in both the source and binary forms, and
+in any documentation provided with the software. 
 """)
+
+
+ add("""Copyright (c) 2010 NCSA.  All rights reserved.
+Developed by: NCSA Cyberenvironments and Technologies
+              University of Illinois at Urbana-Champaign
+              http://cet.ncsa.uiuc.edu/
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal with the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+ 1. Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimers.
+ 2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimers in the
+    documentation and/or other materials provided with the distribution.
+ 3. Neither the names of NCSA, University of Illinois, nor the names of its
+    contributors may be used to endorse or promote products derived from this
+    Software without specific prior written permission.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+WITH THE SOFTWARE.""")
 
 }
