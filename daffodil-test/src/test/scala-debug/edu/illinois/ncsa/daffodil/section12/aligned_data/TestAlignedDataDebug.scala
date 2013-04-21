3d243a1183f456dcb671ae45d25f9a356750071d
Jessie Chab <jchab@tresys.com>
2013-04-21 08:36:06 -0700

    Moving alignment tests from scala-new to scala, and adding alignment tests, including tests with implicit alignment that currently fail (DFDL-362)

--- daffodil-test/src/test/scala-debug/edu/illinois/ncsa/daffodil/section12/aligned_data/TestAlignedDataDebug.scala
+++ daffodil-test/src/test/scala-debug/edu/illinois/ncsa/daffodil/section12/aligned_data/TestAlignedDataDebug.scala
@@ -45,42 +45,13 @@ import edu.illinois.ncsa.daffodil.tdml.DFDLTestSuite
 import java.io.File
 import edu.illinois.ncsa.daffodil.debugger.Debugger
 
-class TestAlignedData extends JUnitSuite {
+class TestAlignedDataDebug extends JUnitSuite {
   val testDir_01 = "/edu/illinois/ncsa/daffodil/section12/aligned_data/"
   val tdml1 = testDir_01 + "Aligned_Data.tdml"
   lazy val runner1 = new DFDLTestSuite(Misc.getRequiredResource(tdml1))
 
-  @Test def test_leadingSkip_1() = { runner1.runOneTest("leadingSkip1") }
-  @Test def test_leadingSkip_2() = { runner1.runOneTest("leadingSkip2") }
+  @Test def test_implicitAlignmentString1() = { runner1.runOneTest("implicitAlignmentString1") }
+  @Test def test_implicitAlignmentUInt() = { runner1.runOneTest("implicitAlignmentUInt") }
+  @Test def test_implicitAlignmentUShort() = { runner1.runOneTest("implicitAlignmentUShort") }
   
-  @Test def test_alignment01() = { runner1.runOneTest("alignment01") }
-  @Test def test_alignment02() = { runner1.runOneTest("alignment02") }
-  @Test def test_alignment03() = { runner1.runOneTest("alignment03") }
- 
- /*
-    2.95. Section 12.1. State that if representation is text or type is string, then alignment is
-          determined by character set encoding.
- */
-
-//  @Test def test_implicitAlignmentString1() = { runner1.runOneTest("implicitAlignmentString1") }
-  @Test def test_implicitAlignmentString2() = { runner1.runOneTest("implicitAlignmentString2") }
-
-//  @Test def test_implicitAlignmentUInt() = { runner1.runOneTest("implicitAlignmentUInt") }
-  @Test def test_implicitAlignmentUIntb() = { runner1.runOneTest("implicitAlignmentUIntb") }
-  
-//  @Test def test_implicitAlignmentUShort() = { runner1.runOneTest("implicitAlignmentUShort") }
-  @Test def test_implicitAlignmentUShortb() = { runner1.runOneTest("implicitAlignmentUShortb") }
-  
-  val tdml2 = testDir_01 + "BinaryInput_01.tdml"
-  lazy val runner2 = new DFDLTestSuite(Misc.getRequiredResource(tdml2))
-
-  @Test def test_leading_skip_byte(){ runner2.runOneTest("LeadingSkipBytes")}
-  @Test def test_leading_skip_bit(){ runner2.runOneTest("LeadingSkipBits")}
-  @Test def test_trailing_skip_byte(){ runner2.runOneTest("TrailingSkipBytes")}
-  @Test def test_trailing_skip_bit(){ runner2.runOneTest("TrailingSkipBits")}
-  @Test def test_AligningSkipBytes(){ runner2.runOneTest("AligningSkipBytes")}
-  @Test def test_AligningSkipBytes2(){ runner2.runOneTest("AligningSkipBytes2")}
-  @Test def test_AligningSkipBits(){ runner2.runOneTest("AligningSkipBits")}
-  @Test def test_AligningSkipBits2(){ runner2.runOneTest("AligningSkipBits2")}
-
 }
