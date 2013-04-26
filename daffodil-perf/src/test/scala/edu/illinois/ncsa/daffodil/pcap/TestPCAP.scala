a27e9ca83b4d1a1635704d532e1f9b4168f7b33d
Josh Adams <jadams@tresys.com>
2013-04-26 14:21:33 -0400

    Commenting out more tests
    Forgot that the data files for these tests were not in the repo as we
    are trying to keep some of the larger test files out of the git repo.
    These test files are located in an svn repo
    (repos:/repos/svn/ngf-dfdl/Input)

--- daffodil-perf/src/test/scala/edu/illinois/ncsa/daffodil/pcap/TestPCAP.scala
+++ daffodil-perf/src/test/scala/edu/illinois/ncsa/daffodil/pcap/TestPCAP.scala
@@ -50,9 +50,9 @@ class TestPCAP extends JUnitSuite {
   lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa))
 
   @Test def test_pcap_test() { runner.runPerfTest("pcap_test") }
-  @Test def test_pcap_1_6m() { runner.runPerfTest("pcap_1_6m") }
-  @Test def test_pcap_55m() { runner.runPerfTest("pcap_55m") }
-  @Test def test_pcap_13m() { runner.runPerfTest("pcap_13m") }
+  //@Test def test_pcap_1_6m() { runner.runPerfTest("pcap_1_6m") }
+  //@Test def test_pcap_55m() { runner.runPerfTest("pcap_55m") }
+  //@Test def test_pcap_13m() { runner.runPerfTest("pcap_13m") }
   //@Test def test_pcap_1m() { runner.runPerfTest("pcap_1m") }
   //@Test def test_pcap_50m() { runner.runPerfTest("pcap_50m") }
   //@Test def test_pcap_100m() { runner.runPerfTest("pcap_100m") }
