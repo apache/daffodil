ee4c1d257c7473ce77f4693d45649d61123c09f5
Jessie Chab <jchab@tresys.com>
2013-05-03 10:47:42 -0700

    Adding one more test for addition of bitPos, bytePos, and isAtEnd methods in Java API
    DFDL-679

--- daffodil-core/src/test/java/edu/illinois/ncsa/daffodil/example/TestJavaAPI.java
+++ daffodil-core/src/test/java/edu/illinois/ncsa/daffodil/example/TestJavaAPI.java
@@ -247,4 +247,36 @@ public class TestJavaAPI {
 		System.err.println("bitPos = " + res.location().bitPos());
 		System.err.println("bytePos = " + res.location().bytePos());
 	}
+
+  @Test
+  public void testJavaAPI5() throws IOException {
+		Compiler c = Daffodil.compiler();
+		String[] schemaFileNames = new String[1];
+		schemaFileNames[0] = getResource("/test/japi/mySchema3.dfdl.xsd");
+		c.setDistinguishedRootNode("e4", null);
+		ProcessorFactory pf = c.compile(schemaFileNames);
+		DataProcessor dp = pf.onPath("/");
+		java.io.File file = new java.io.File(getResource("/test/japi/myData3.dat"));
+		java.io.FileInputStream fis = new java.io.FileInputStream(file);
+		java.nio.channels.ReadableByteChannel rbc = java.nio.channels.Channels
+				.newChannel(fis);
+		ParseResult res = dp.parse(rbc, 4 << 3);
+		boolean err = res.isError();
+		org.jdom.output.XMLOutputter xo = new org.jdom.output.XMLOutputter();
+		xo.setFormat(Format.getPrettyFormat());
+		java.util.List<Diagnostic> diags = res.getDiagnostics();
+		for (Diagnostic d : diags) {
+			System.err.println(d.getMessage());
+		}
+		if (!err) {
+			org.jdom.Document doc = res.result();
+			xo.output(doc, System.out);
+		}
+		assertFalse(err);
+		assertTrue("Assertion failed: End of data not reached.", res.location().isAtEnd());
+		assertEquals(4, res.location().bytePos());
+		assertEquals(32, res.location().bitPos());
+		System.err.println("bitPos = " + res.location().bitPos());
+		System.err.println("bytePos = " + res.location().bytePos());
+	}
 }
