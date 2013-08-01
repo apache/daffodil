9feb2e97d05acf2c28b0a3ecdf7ed2f59713f3f0
Beth Finnegan <efinnegan@tresys.com>
2013-08-01 17:13:15 -0400

    Adding tests to check that failed assertions show the given value
    DFDL-752

--- daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section07/assertions/TestAssertions.scala
+++ daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section07/assertions/TestAssertions.scala
@@ -46,6 +46,9 @@ class TestAssertions {
   @Test def test_assertPass() { runner.runOneTest("assertPass") }
   @Test def test_assertFail1() { runner.runOneTest("assertFail1") }
   @Test def test_assertFail2() { runner.runOneTest("assertFail2") }
+  @Test def test_assertFailShowsValue() { runner.runOneTest("assertFailShowsValue") }
+  @Test def test_assertFailShowsValue2() { runner.runOneTest("assertFailShowsValue2") }
+  @Test def test_assertFailShowsDetails() { runner.runOneTest("assertFailShowsDetails") }
   @Test def test_assertWithWhitespace() { runner.runOneTest("assertWithWhitespace") }
   @Test def test_assertWithWhitespaceAndCdata() { runner.runOneTest("assertWithWhitespaceAndCdata") }
 
