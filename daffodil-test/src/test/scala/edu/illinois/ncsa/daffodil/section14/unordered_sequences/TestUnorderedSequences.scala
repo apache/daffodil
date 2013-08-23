55d025ce9359f6a0b684e2d34540eaea3d57a437
Josh Adams <jadams@tresys.com>
2013-08-23 15:49:58 -0400

    Added tests for unordered seq coverage
    Moved existing tests from scala-new.  Added tests for requirements that
    weren't covered by existing tests.
    
    DFDL-642

---
+++ daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section14/unordered_sequences/TestUnorderedSequences.scala
@@ -0,0 +1,136 @@
+package edu.illinois.ncsa.daffodil.section14.unordered_sequences
+
+/* Copyright (c) 2012-2013 Tresys Technology, LLC. All rights reserved.
+ *
+ * Developed by: Tresys Technology, LLC
+ *               http://www.tresys.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal with
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimers.
+ * 
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimers in the
+ *     documentation and/or other materials provided with the distribution.
+ * 
+ *  3. Neither the names of Tresys Technology, nor the names of its contributors
+ *     may be used to endorse or promote products derived from this Software
+ *     without specific prior written permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE
+ * SOFTWARE.
+ */
+
+import junit.framework.Assert._
+import org.junit.Test
+import edu.illinois.ncsa.daffodil.util.Misc
+import edu.illinois.ncsa.daffodil.tdml.DFDLTestSuite
+import edu.illinois.ncsa.daffodil.util.Logging
+import edu.illinois.ncsa.daffodil.util.LoggingDefaults
+import edu.illinois.ncsa.daffodil.util.LogLevel
+
+class TestUnorderedSequences {
+  val testDir = "/edu/illinois/ncsa/daffodil/section14/unordered_sequences/"
+  val uos = testDir + "UnorderedSequences.tdml"
+  val res = Misc.getRequiredResource(uos)
+  lazy val runner = new DFDLTestSuite(res)
+
+  @Test def test_simple = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_simple")
+  }
+
+  @Test def test_simple_fail_scalar = {
+    runner.runOneTest("test_simple_fail_scalar")
+  }
+
+  @Test def test_simple_min_max_occurs = {
+    runner.runOneTest("test_simple_min_max_occurs")
+  }
+
+  @Test def test_simple_min_max_occurs_fail = {
+    runner.runOneTest("test_simple_min_max_occurs_fail")
+  }
+
+  @Test def test_simple_delimited = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_simple_delimited")
+  }
+
+  @Test def test_simple_nil = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_simple_nil")
+  }
+
+  @Test def test_simple_invalid_path_to_branch = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_simple_invalid_path_to_branch")
+  }
+
+  @Test def test_simple_invalid_path_to_branch_does_not_exist = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_simple_invalid_path_to_branch_does_not_exist")
+  }
+
+  @Test def test_nested_valid_path_to_branch = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_nested_valid_path_to_branch")
+  }
+
+  @Test def test_nested_multiple_valid_paths_to_branch = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_nested_multiple_valid_paths_to_branch")
+  }
+
+  @Test def test_nested_multiple_invalid_paths_to_branch = {
+    // LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_nested_multiple_invalid_paths_to_branch")
+  }
+
+  @Test def test_nested_path_evaluates_to_nodelist = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_nested_path_evaluates_to_nodelist")
+  }
+
+  @Test def test_nested_invalid_path_to_branch = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_nested_invalid_path_to_branch")
+  }
+
+  @Test def test_nested_invalid_path_to_branch_2 = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_nested_invalid_path_to_branch_2")
+  }
+
+  @Test def test_nested_invalid_path_to_branch_3 = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_nested_invalid_path_to_branch_3")
+  }
+
+  @Test def test_sde_element_element_ref = {
+    //LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_sde_element_element_ref")
+  }
+
+  @Test def test_sde_optional_array_ock_parsed = {
+    // LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_sde_optional_array_ock_parsed")
+  }
+
+  @Test def test_sde_unique_names_in_ns = {
+    // LoggingDefaults.setLoggingLevel(LogLevel.Debug)
+    runner.runOneTest("test_sde_unique_names_in_ns")
+  }
+
+}
