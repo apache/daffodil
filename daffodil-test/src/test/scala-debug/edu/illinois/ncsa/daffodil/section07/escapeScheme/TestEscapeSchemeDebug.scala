dad088afc6351d0566c01a99f588f3a336efe81f
Beth Finnegan <efinnegan@treys.com>
2014-04-10 14:10:26 -0400

    Adding tests for escape scheme scenarios to debug

--- daffodil-test/src/test/scala-debug/edu/illinois/ncsa/daffodil/section07/escapeScheme/TestEscapeSchemeDebug.scala
+++ daffodil-test/src/test/scala-debug/edu/illinois/ncsa/daffodil/section07/escapeScheme/TestEscapeSchemeDebug.scala
@@ -50,4 +50,87 @@ class TestEscapeSchemeDebug {
   lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa), validateTDMLFile = false)
   
   @Test def test_escapeSchemeNonUnique() { runner.runOneTest("escapeSchemeNonUnique") }
+  
+  val bb = testDir + "escapeScenarios.tdml"
+  lazy val runner2 = new DFDLTestSuite(Misc.getRequiredResource(bb), validateTDMLFile = false)
+  
+  @Test def test_scenario1_1() { runner2.runOneTest("scenario1_1") }
+  //@Test def test_scenario1_2() { runner2.runOneTest("scenario1_2") }
+  @Test def test_scenario1_3() { runner2.runOneTest("scenario1_3") }
+  @Test def test_scenario1_4() { runner2.runOneTest("scenario1_4") }
+  //@Test def test_scenario1_5() { runner2.runOneTest("scenario1_5") }
+  @Test def test_scenario1_6() { runner2.runOneTest("scenario1_6") }
+  @Test def test_scenario1_7() { runner2.runOneTest("scenario1_7") }
+  @Test def test_scenario1_7_postfix() { runner2.runOneTest("scenario1_7_postfix") }
+  @Test def test_scenario1_8() { runner2.runOneTest("scenario1_8") }
+  @Test def test_scenario1_8_req_term() { runner2.runOneTest("scenario1_8_req_term") }
+  @Test def test_scenario1_9() { runner2.runOneTest("scenario1_9") }
+  @Test def test_scenario1_9_postfix() { runner2.runOneTest("scenario1_9_postfix") }
+  @Test def test_scenario1_10() { runner2.runOneTest("scenario1_10") }
+  @Test def test_scenario1_10_postfix() { runner2.runOneTest("scenario1_10_postfix") }
+  @Test def test_scenario1_11() { runner2.runOneTest("scenario1_11") }
+  @Test def test_scenario1_11_postfix() { runner2.runOneTest("scenario1_11_postfix") }
+  @Test def test_scenario1_12() { runner2.runOneTest("scenario1_12") }
+  @Test def test_scenario1_12_postfix() { runner2.runOneTest("scenario1_12_postfix") }
+  @Test def test_scenario1_13() { runner2.runOneTest("scenario1_13") }
+  @Test def test_scenario1_13_postfix() { runner2.runOneTest("scenario1_13_postfix") }
+  
+  @Test def test_scenario2_1() { runner2.runOneTest("scenario2_1") }
+  //@Test def test_scenario2_2() { runner2.runOneTest("scenario2_2") }
+  @Test def test_scenario2_3() { runner2.runOneTest("scenario2_3") }
+  @Test def test_scenario2_4() { runner2.runOneTest("scenario2_4") }
+  //@Test def test_scenario2_5() { runner2.runOneTest("scenario2_5") }
+  @Test def test_scenario2_6() { runner2.runOneTest("scenario2_6") }
+  @Test def test_scenario2_7() { runner2.runOneTest("scenario2_7") }
+  @Test def test_scenario2_8() { runner2.runOneTest("scenario2_8") }
+  @Test def test_scenario2_9() { runner2.runOneTest("scenario2_9") }
+  @Test def test_scenario2_10() { runner2.runOneTest("scenario2_10") }
+  @Test def test_scenario2_10_postfix() { runner2.runOneTest("scenario2_10_postfix") }
+  @Test def test_scenario2_11() { runner2.runOneTest("scenario2_11") }
+  @Test def test_scenario2_11_req_term() { runner2.runOneTest("scenario2_11_req_term") }
+  @Test def test_scenario2_12() { runner2.runOneTest("scenario2_12") }
+  @Test def test_scenario2_12_postfix() { runner2.runOneTest("scenario2_12_postfix") }
+  @Test def test_scenario2_13() { runner2.runOneTest("scenario2_13") }
+  @Test def test_scenario2_13_postfix() { runner2.runOneTest("scenario2_13_postfix") }
+  @Test def test_scenario2_14() { runner2.runOneTest("scenario2_14") }
+  @Test def test_scenario2_14_req_term() { runner2.runOneTest("scenario2_14_req_term") }
+  
+  @Test def test_scenario3_1() { runner2.runOneTest("scenario3_1") }
+  @Test def test_scenario3_2() { runner2.runOneTest("scenario3_2") }
+  //@Test def test_scenario3_3() { runner2.runOneTest("scenario3_3") }
+  @Test def test_scenario3_4() { runner2.runOneTest("scenario3_4") }
+  @Test def test_scenario3_5() { runner2.runOneTest("scenario3_5") }
+  @Test def test_scenario3_6() { runner2.runOneTest("scenario3_6") }
+  @Test def test_scenario3_7() { runner2.runOneTest("scenario3_7") }
+  @Test def test_scenario3_8() { runner2.runOneTest("scenario3_8") }
+  @Test def test_scenario3_9() { runner2.runOneTest("scenario3_9") }
+  @Test def test_scenario3_10() { runner2.runOneTest("scenario3_10") }
+  @Test def test_scenario3_10_postfix() { runner2.runOneTest("scenario3_10_postfix") }
+  @Test def test_scenario3_11() { runner2.runOneTest("scenario3_11") }
+  @Test def test_scenario3_11_postfix() { runner2.runOneTest("scenario3_11_postfix") }
+  //@Test def test_scenario3_12() { runner2.runOneTest("scenario3_12") }
+  //@Test def test_scenario3_12_req_term() { runner2.runOneTest("scenario3_12_req_term") }
+  @Test def test_scenario3_13() { runner2.runOneTest("scenario3_13") }
+  @Test def test_scenario3_13_postfix() { runner2.runOneTest("scenario3_13_postfix") }
+  //@Test def test_scenario3_14() { runner2.runOneTest("scenario3_14") }
+  //@Test def test_scenario3_14_req_term() { runner2.runOneTest("scenario3_14_req_term") }
+
+  @Test def test_scenario4_1() { runner2.runOneTest("scenario4_1") }
+  @Test def test_scenario4_2() { runner2.runOneTest("scenario4_2") }
+  @Test def test_scenario4_3() { runner2.runOneTest("scenario4_3") }
+  @Test def test_scenario4_4() { runner2.runOneTest("scenario4_4") }
+  @Test def test_scenario4_5() { runner2.runOneTest("scenario4_5") }
+  @Test def test_scenario4_6() { runner2.runOneTest("scenario4_6") }
+  @Test def test_scenario4_7() { runner2.runOneTest("scenario4_7") }
+  @Test def test_scenario4_7_req_term() { runner2.runOneTest("scenario4_7_req_term") }
+  @Test def test_scenario4_8() { runner2.runOneTest("scenario4_8") }
+  @Test def test_scenario4_8_postfix() { runner2.runOneTest("scenario4_8_postfix") }
+  @Test def test_scenario4_9() { runner2.runOneTest("scenario4_9") }
+  @Test def test_scenario4_9_req_term() { runner2.runOneTest("scenario4_9_req_term") }
+  @Test def test_scenario4_10() { runner2.runOneTest("scenario4_10") }
+  @Test def test_scenario4_10_req_term() { runner2.runOneTest("scenario4_10_req_term") }
+  @Test def test_scenario4_11() { runner2.runOneTest("scenario4_11") }
+  @Test def test_scenario4_11_postfix() { runner2.runOneTest("scenario4_11_postfix") }
+  @Test def test_scenario4_12() { runner2.runOneTest("scenario4_12") }
+  @Test def test_scenario4_12_req_term() { runner2.runOneTest("scenario4_12_req_term") }
 }
