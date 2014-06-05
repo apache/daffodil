0e5fdb3207a4cee4b88435cefc9c1b0f9f432c01
Mike Beckerle <mbeckerle@tresys.com>
2014-06-05 12:08:03 -0400

    Maybe class
    Removed ability to pattern match against it. Because this boxes the
    objects, and the whole point of this class is NOT to box objects. This
    is the most important change - because we may start using this class
    heavily, and I don't want us to be writing match-cases when we do. 
    
    Improved inlining slightly. Changed perf test (commented out) to use
    Long, not Int. (Makes no real difference in relative speed.).
    
    Fixed 'bug' where def empty was allocating multiple different Nope
    objects instead of their only being a single one. 
    
    Confirmed (on JVisualVM) that one can pass and return Maybe objects
    without allocation occuring.
    
    Added implicit conversion to an Iterable/List. (Convenience - scala
    Option has this.)
    
    Added implicit conversion from a Scala Option to a Maybe (but the other
    way must be explicit)

--- daffodil-lib/src/main/scala/edu/illinois/ncsa/daffodil/util/Maybe.scala
+++ daffodil-lib/src/main/scala/edu/illinois/ncsa/daffodil/util/Maybe.scala
@@ -6,61 +6,78 @@ package edu.illinois.ncsa.daffodil.util
  *
  *  To tell the difference these two items are called One(v) and Nope.
  */
-final class Maybe[+T] private (val v: Any) extends AnyVal {
+final class Maybe[+T] @inline private (val v: Any) extends AnyVal {
   import Maybe._
-  def get: T = if (isDefined) value.asInstanceOf[T] else throw new NoSuchElementException("Nope.get")
-  @inline def value: T = v.asInstanceOf[T]
-
+  @inline final def get: T = if (isDefined) value.asInstanceOf[T] else noneGet
+  @inline final def value: T = v.asInstanceOf[T]
+  final def noneGet = throw new NoSuchElementException("Nope.get")
   // ?? I don't know how isEmpty can work. It requires the value to be a reference
   // or a value, but there is no way to tell the difference without 
   // storing more than just the value. 
   // 
-  @inline def isEmpty: Boolean = NopeValue eq value.asInstanceOf[AnyRef]
-  @inline def isDefined: Boolean = !isEmpty
-  @inline def nonEmpty = isDefined
-  @inline def contains[U >: T](elem: U): Boolean = !isEmpty && value == elem
-  @inline def exists(p: T => Boolean): Boolean = !isEmpty && p(get)
-  @inline def forall(p: T => Boolean): Boolean = isEmpty || p(get)
-  @inline def orElse[U >: T](alternative: => Maybe[U]): Maybe[U] = if (isEmpty) alternative else this
-  @inline def collect[U](pf: PartialFunction[T, U]): Maybe[U] = if (!isEmpty && pf.isDefinedAt(get)) One(pf(get)) else Nope
-  @inline def iterator: Iterator[T] = if (isEmpty) collection.Iterator.empty else collection.Iterator.single(get)
-  @inline def toList: List[T] = if (isEmpty) List() else new ::(get, Nil)
-  def toRight[X](left: => X) = if (isEmpty) Left(left) else Right(value)
-  def toLeft[X](right: => X) = if (isEmpty) Right(right) else Left(value)
-  def getOrElse[U >: T](default: => U): U = if (isEmpty) default else get
-  def orNull[U >: T](implicit ev: Null <:< U): U = get
-  def filter(p: T => Boolean): Maybe[T] = if (isEmpty || p(get)) this else Nope
-  def filterNot(p: T => Boolean): Maybe[T] = if (isEmpty || !p(get)) this else Nope
-  def withFilter(f: T => Boolean): Maybe[T] = filter(f)
-  def map[U](f: T => U): Maybe[U] = if (isEmpty) Nope else One(f(get))
-  def flatMap[U](f: T => Maybe[U]): Maybe[U] = if (isEmpty) Nope else f(get)
-  def foreach[U](f: T => U): Unit = if (!isEmpty) f(get)
-  def fold[U](ifEmpty: => U)(f: T => U): U = if (isEmpty) ifEmpty else f(get)
-  def flatten[U](implicit ev: T <:< Maybe[U]): Maybe[U] = if (isEmpty) Nope else ev(get)
-  def toScalaOption: scala.Option[T] = if (isEmpty) scala.None else scala.Some(get)
-  override def toString = if (isEmpty) "Nope" else "One(" + get + ")"
+  @inline final def isEmpty: Boolean = NopeValue eq value.asInstanceOf[AnyRef]
+  @inline final def isDefined: Boolean = !isEmpty
+  @inline final def nonEmpty = isDefined
+  @inline final def contains[U >: T](elem: U): Boolean = !isEmpty && value == elem
+  @inline final def exists(p: T => Boolean): Boolean = !isEmpty && p(get)
+  @inline final def forall(p: T => Boolean): Boolean = isEmpty || p(get)
+  @inline final def orElse[U >: T](alternative: => Maybe[U]): Maybe[U] = if (isEmpty) alternative else this
+  @inline final def collect[U](pf: PartialFunction[T, U]): Maybe[U] = if (!isEmpty && pf.isDefinedAt(get)) One(pf(get)) else Nope
+  @inline final def iterator: Iterator[T] = if (isEmpty) collection.Iterator.empty else collection.Iterator.single(get)
+  @inline final def toList: List[T] = if (isEmpty) List() else new ::(get, Nil)
+  final def toRight[X](left: => X) = if (isEmpty) Left(left) else Right(value)
+  final def toLeft[X](right: => X) = if (isEmpty) Right(right) else Left(value)
+  final def getOrElse[U >: T](default: => U): U = if (isEmpty) default else get
+  final def orNull[U >: T](implicit ev: Null <:< U): U = get
+  final def filter(p: T => Boolean): Maybe[T] = if (isEmpty || p(get)) this else Nope
+  final def filterNot(p: T => Boolean): Maybe[T] = if (isEmpty || !p(get)) this else Nope
+  final def withFilter(f: T => Boolean): Maybe[T] = filter(f)
+  final def map[U](f: T => U): Maybe[U] = if (isEmpty) Nope else One(f(get))
+  final def flatMap[U](f: T => Maybe[U]): Maybe[U] = if (isEmpty) Nope else f(get)
+  final def foreach[U](f: T => U): Unit = if (!isEmpty) f(get)
+  final def fold[U](ifEmpty: => U)(f: T => U): U = if (isEmpty) ifEmpty else f(get)
+  final def flatten[U](implicit ev: T <:< Maybe[U]): Maybe[U] = if (isEmpty) Nope else ev(get)
+  final def toScalaOption: scala.Option[T] = if (isEmpty) scala.None else scala.Some(get)
+  override final def toString = if (isEmpty) "Nope" else "One(" + get + ")"
 }
 
 object Maybe {
 
-  @inline
-  def apply[T](value: T) = if (value == null) Nope else some(value)
+  import scala.language.implicitConversions
+
+  /**
+   *  implicitly treat as iterator/sequence/list (Scala's Option type has this)
+   */
+  implicit def toIterator[T](m: Maybe[T]) = m.toList
+
+  /**
+   * implicitly convert Option type to Maybe type.
+   *
+   * The conversion the other way must be explicit by calling toScalaOption
+   */
+  implicit def toMaybe[T](o: Option[T]) = o match {
+    case None => Nope
+    case Some(x) => One(x)
+  }
 
   @inline
-  def some[T](value: T) = new Maybe[T](value)
+  final def apply[T](value: T) = if (value == null) Nope else some(value)
 
-  def empty[T] = new Maybe[T](NopeValue)
+  @inline
+  final def some[T](value: T) = new Maybe[T](value)
+
+  def empty[T] = Nope
 
   val Nope = new Maybe[Nothing](NopeValue)
 
   object One {
     @inline
-    def apply[T](value: T) = Maybe(value)
+    final def apply[T](value: T) = Maybe(value)
 
     // If the pattern matching is going to box an object then this is hardly
     // worth using. 
     //
-    def unapply[T](value: Maybe[T]) = if (value.isDefined) scala.Some(value.get) else scala.None
+    // def unapply[T](value: Maybe[T]) = if (value.isDefined) scala.Some(value.get) else scala.None
   }
   private object NopeValue
 }
@@ -84,7 +101,7 @@ object Maybe {
 //  var callCount: Long = 0
 //
 //  // Original Tak function
-//  def tak1(x: Int, y: Int, z: Int): Int = {
+//  def tak1(x: Long, y: Long, z: Long): Long = {
 //    callCount += 1
 //    if (y < x)
 //      tak1(
@@ -96,7 +113,7 @@ object Maybe {
 //  }
 //
 //  // Tak, but passing Scala Option type Some objects.
-//  def taks(sx: Option[Int], sy: Option[Int], sz: Option[Int]): Option[Int] = {
+//  def taks(sx: Option[Long], sy: Option[Long], sz: Option[Long]): Option[Long] = {
 //    val x = sx.get
 //    val y = sy.get
 //    val z = sz.get
@@ -112,7 +129,7 @@ object Maybe {
 //
 //  // Tak, but passing scala 2.10 maybe One "value class" objects
 //  // which should be unboxed.
-//  def tak(sx: Maybe[Int], sy: Maybe[Int], sz: Maybe[Int]): Maybe[Int] = {
+//  def tak(sx: Maybe[Long], sy: Maybe[Long], sz: Maybe[Long]): Maybe[Long] = {
 //    val x = sx.value
 //    val y = sy.value
 //    val z = sz.value
@@ -129,9 +146,9 @@ object Maybe {
 //  def testTak() {
 //    println("Calibrating takeon units")
 //    callCount = 0
-//    val x = 21
-//    val y = 3
-//    val z = 21
+//    val x = 20L
+//    val y = 4L
+//    val z = 20L
 //    var nanos = Timer.getTimeNS { tak1(x, y, z) }
 //    println("tak call count = " + callCount + " in " + nanos + "ns")
 //    takeons = (1.0 * nanos) / callCount
@@ -152,4 +169,4 @@ object Maybe {
 //  }
 //
 //  val res = testTak()
-//}
+// }
