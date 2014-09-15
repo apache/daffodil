793bbb2a3fe0ee4826b022ba0f8bfcbc8dc31d2f
Jessie Chab <jchab@tresys.com>
2014-09-15 16:48:47 -0700

    Add test for empty textStringPadCharacter
    bugzid: 20469

--- daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section05/simple_types/TestSimpleTypes.scala
+++ daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section05/simple_types/TestSimpleTypes.scala
@@ -407,6 +407,7 @@ class TestSimpleTypes {
   @Test def test_padding_escape() { runner.runOneTest("padding_escape") }
   @Test def test_padding_nil() { runner.runOneTest("padding_nil") }
   @Test def test_padding_nil2() { runner.runOneTest("padding_nil2") }
+  @Test def test_padding_empty() { runner.runOneTest("padding_empty") }
   @Test def test_justification_1() { runner.runOneTest("justification_1") }
 
   // Verification that user's test works for DFDL-677
