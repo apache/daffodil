81e9654c7deaf6ad79779f9a03aa0a5e50409750
Jessie Chab <jchab@tresys.com>
2014-11-10 11:40:01 -0800

    Update test names to keep with naming convention

--- daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section05/simple_types/TestSimpleTypes.scala
+++ daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section05/simple_types/TestSimpleTypes.scala
@@ -494,9 +494,9 @@ class TestSimpleTypes {
   
   val st = testDir + "simple-type-bases.tdml"
   lazy val runnerST = new DFDLTestSuite(Misc.getRequiredResource(st))
-  @Test def test_simpleTypeDerivedFromPrimType() { runnerST.runOneTest("st-prim") }
-  @Test def test_simpleTypeChainedDerivations() { runnerST.runOneTest("st-derived") }
-  @Test def test_simpleTypeOverlapPrimError() { runnerST.runOneTest("st-prim-err1") }
-  @Test def test_simpleTypeOverlapSimpleTypeError() { runnerST.runOneTest("st-st-err1") }
+  @Test def test_simpleTypeDerivedFromPrimType() { runnerST.runOneTest("simpleTypeDerivedFromPrimType") }
+  @Test def test_simpleTypeChainedDerivations() { runnerST.runOneTest("simpleTypeChainedDerivations") }
+  @Test def test_simpleTypeOverlapPrimError() { runnerST.runOneTest("simpleTypeOverlapPrimError") }
+  @Test def test_simpleTypeOverlapSimpleTypeError() { runnerST.runOneTest("simpleTypeOverlapSimpleTypeError") }
 
 }
