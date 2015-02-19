e4a5aac2a33315e8ac608e40cea9ed89f74bfbc3
Jessie Chab <jchab@tresys.com>
2015-02-19 09:51:13 -0800

    Update fn:exactly-one tests to use the updated definition of the function based on the XPath definition
    DFDL-1189
    DFDL-1087

--- daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section23/dfdl_expressions/TestDFDLExpressions.scala
+++ daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section23/dfdl_expressions/TestDFDLExpressions.scala
@@ -436,10 +436,6 @@ class TestDFDLExpressions {
   //@Test def test_empty_02() { runner2.runOneTest("empty_02") }
   //@Test def test_exists_02() { runner2.runOneTest("exists_02") }
 
-  //DFDL-1087
-  //@Test def test_exactly_one_02() { runner2.runOneTest("exactly_one_02") }
-  //@Test def test_exactly_one_03() { runner2.runOneTest("exactly_one_03") }
-
   //DFDL-1091
   //@Test def test_count_05b() { runner2.runOneTest("count_05b") }
 
@@ -470,6 +466,8 @@ class TestDFDLExpressions {
 
   //DFDL-1189
   //@Test def test_exactly_one_01() { runner2.runOneTest("exactly_one_01") }
+  //@Test def test_exactly_one_02() { runner2.runOneTest("exactly_one_02") }
+  //@Test def test_exactly_one_03() { runner2.runOneTest("exactly_one_03") }
   //@Test def test_exactly_one_04() { runner2.runOneTest("exactly_one_04") }
   //@Test def test_exactly_one_05() { runner2.runOneTest("exactly_one_05") }
   //@Test def test_exactly_one_06() { runner2.runOneTest("exactly_one_06") }
