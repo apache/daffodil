7dc4965d6f20c833de92ae4a66148db6b1eb7f63
Jessie Chab <jchab@tresys.com>
2015-04-03 04:54:36 -0700

    Verify example unparser test added by dev; create additional unparser tests, including negative tests.
    DFDL-1289
    DFDL-1293
    DFDL-1294

---
+++ daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section00/general/TestUnparserGeneral.scala
@@ -0,0 +1,64 @@
+/* Copyright (c) 2015 Tresys Technology, LLC. All rights reserved.
+ *
+ * Developed by: Tresys Technology, LLC
+ *               http://www.tresys.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal with
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimers.
+ * 
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimers in the
+ *     documentation and/or other materials provided with the distribution.
+ * 
+ *  3. Neither the names of Tresys Technology, nor the names of its contributors
+ *     may be used to endorse or promote products derived from this Software
+ *     without specific prior written permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE
+ * SOFTWARE.
+ */
+
+package edu.illinois.ncsa.daffodil.section00.general
+
+import junit.framework.Assert._
+import org.junit.Test
+import scala.xml._
+import edu.illinois.ncsa.daffodil.xml.XMLUtils
+import edu.illinois.ncsa.daffodil.xml.XMLUtils._
+import edu.illinois.ncsa.daffodil.compiler.Compiler
+import edu.illinois.ncsa.daffodil.util._
+import edu.illinois.ncsa.daffodil.tdml.DFDLTestSuite
+import java.io.File
+import edu.illinois.ncsa.daffodil.debugger.Debugger
+
+class TestUnparserGeneral {
+
+  val testDir = "/edu/illinois/ncsa/daffodil/section00/general/"
+  val aa = testDir + "testUnparserGeneral.tdml"
+  lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa))
+
+  @Test def test_unparseFixedLengthString01() { runner.runOneTest("unparseFixedLengthString01") }
+  @Test def test_unparseFixedLengthString02() { runner.runOneTest("unparseFixedLengthString02") }
+
+  //DFDL-1293
+  //@Test def test_negativeUnparseTest01() { runner.runOneTest("negativeUnparseTest01") }
+  //@Test def test_negativeUnparseTest02() { runner.runOneTest("negativeUnparseTest02") }
+
+  //DFDL-1294
+  //@Test def test_negativeUnparseTest03() { runner.runOneTest("negativeUnparseTest03") }
+  //@Test def test_negativeUnparseTest04() { runner.runOneTest("negativeUnparseTest04") }
+
+}
+
