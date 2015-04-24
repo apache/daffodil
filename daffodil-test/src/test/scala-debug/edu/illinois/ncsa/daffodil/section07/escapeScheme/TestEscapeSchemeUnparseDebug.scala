e55ff1c2a4e55b83ecf58b0b40fa396c4b776d24
Jessie Chab <jchab@tresys.com>
2015-04-24 13:34:39 -0700

    Add more tests for verifying unparse of escape schemes Some tests highlight potential bugs with escape schemes.
    DFDL-1256
    DFDL-1314
    DFDL-1315
    DFDL-1316

--- daffodil-test/src/test/scala-debug/edu/illinois/ncsa/daffodil/section07/escapeScheme/TestEscapeSchemeUnparseDebug.scala
+++ daffodil-test/src/test/scala-debug/edu/illinois/ncsa/daffodil/section07/escapeScheme/TestEscapeSchemeUnparseDebug.scala
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015 Tresys Technology, LLC. All rights reserved.
+/* Copyright (c) 2012-2014 Tresys Technology, LLC. All rights reserved.
  *
  * Developed by: Tresys Technology, LLC
  *               http://www.tresys.com
@@ -30,11 +30,10 @@
  * SOFTWARE.
  */
 
-package edu.illinois.ncsa.daffodil.section00.general
+package edu.illinois.ncsa.daffodil.section07.escapeScheme
 
 import junit.framework.Assert._
 import org.junit.Test
-import org.junit.AfterClass
 import scala.xml._
 import edu.illinois.ncsa.daffodil.xml.XMLUtils
 import edu.illinois.ncsa.daffodil.xml.XMLUtils._
@@ -44,36 +43,19 @@ import edu.illinois.ncsa.daffodil.tdml.DFDLTestSuite
 import java.io.File
 import edu.illinois.ncsa.daffodil.debugger.Debugger
 
-object TestUnparserGeneral {
-  val testDir = "/edu/illinois/ncsa/daffodil/section00/general/"
-  val aa = testDir + "testUnparserGeneral.tdml"
-  var runner = new DFDLTestSuite(Misc.getRequiredResource(aa))
+class TestEscapeSchemeUnparseDebug {
+  val testDir = "/edu/illinois/ncsa/daffodil/section07/escapeScheme/"
+  val aa = testDir + "escapeSchemeUnparse.tdml"
+  lazy val runner = new DFDLTestSuite(Misc.getRequiredResource(aa), validateTDMLFile = false)
 
-  @AfterClass def shutDown {
-    runner = null
-  }
-}
-class TestUnparserGeneral {
-
-  import TestUnparserGeneral._
-
-  @Test def test_unparseFixedLengthString01() { runner.runOneTest("unparseFixedLengthString01") }
-  @Test def test_unparseFixedLengthString02() { runner.runOneTest("unparseFixedLengthString02") }
-  @Test def test_unparseFixedLengthString03() { runner.runOneTest("unparseFixedLengthString03") }
-
-  @Test def test_negativeUnparseTest01() { runner.runOneTest("negativeUnparseTest01") }
-  @Test def test_negativeUnparseTest02() { runner.runOneTest("negativeUnparseTest02") }
-  @Test def test_negativeUnparseTest05() { runner.runOneTest("negativeUnparseTest05") }
-
-  //DFDL-1294
-  @Test def test_negativeUnparseTest03() { runner.runOneTest("negativeUnparseTest03") }
-  @Test def test_negativeUnparseTest04() { runner.runOneTest("negativeUnparseTest04") }
-
-  @Test def test_unparseDelimitedString01() { runner.runOneTest("unparseDelimitedString01") }
-  @Test def test_unparseDelimitedPaddedString01() { runner.runOneTest("unparseDelimitedPaddedString01") }
-
-  @Test def test_parseDelimitedString01() { runner.runOneTest("parseDelimitedString01") }
-  @Test def test_parseDelimitedPaddedString01() { runner.runOneTest("parseDelimitedPaddedString01") }
+  //DFDL-1314
+  @Test def test_unparseDelimitedEscapedString03() { runner.runOneTest("unparseDelimitedEscapedString03") }
+  @Test def test_unparseDelimitedEscapedString04() { runner.runOneTest("unparseDelimitedEscapedString04") }
+  
+  //DFDL-1315 - uses too much memory 
+  //@Test def test_parseDelimitedEscapedString04() { runner.runOneTest("parseDelimitedEscapedString04") }
+  
+  //DFDL-1316
+  @Test def test_parseDelimitedEscapedString05() { runner.runOneTest("parseDelimitedEscapedString05") }
 
 }
-
--- daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section07/escapeScheme/TestEscapeSchemeUnparse.scala
+++ daffodil-test/src/test/scala/edu/illinois/ncsa/daffodil/section07/escapeScheme/TestEscapeSchemeUnparse.scala
@@ -30,7 +30,7 @@
  * SOFTWARE.
  */
 
-package edu.illinois.ncsa.daffodil.section00.general
+package edu.illinois.ncsa.daffodil.section07.escapeScheme
 
 import junit.framework.Assert._
 import org.junit.Test
@@ -44,36 +44,35 @@ import edu.illinois.ncsa.daffodil.tdml.DFDLTestSuite
 import java.io.File
 import edu.illinois.ncsa.daffodil.debugger.Debugger
 
-object TestUnparserGeneral {
-  val testDir = "/edu/illinois/ncsa/daffodil/section00/general/"
-  val aa = testDir + "testUnparserGeneral.tdml"
+object TestEscapeSchemeUnparse {
+  val testDir = "/edu/illinois/ncsa/daffodil/section07/escapeScheme/"
+  val aa = testDir + "escapeSchemeUnparse.tdml"
   var runner = new DFDLTestSuite(Misc.getRequiredResource(aa))
 
   @AfterClass def shutDown {
     runner = null
   }
 }
-class TestUnparserGeneral {
+class TestEscapeSchemeUnparse {
 
-  import TestUnparserGeneral._
+  import TestEscapeSchemeUnparse._
+  
+  //DFDL-1314
+  //@Test def test_unparseDelimitedEscapedString03() { runner.runOneTest("unparseDelimitedEscapedString03") }
+  //@Test def test_unparseDelimitedEscapedString04() { runner.runOneTest("unparseDelimitedEscapedString04") }
+  
+  @Test def test_unparseDelimitedEscapedString01() { runner.runOneTest("unparseDelimitedEscapedString01") }
+  @Test def test_unparseDelimitedEscapedString02() { runner.runOneTest("unparseDelimitedEscapedString02") }
+  @Test def test_unparseDelimitedEscapedString05() { runner.runOneTest("unparseDelimitedEscapedString05") }
 
-  @Test def test_unparseFixedLengthString01() { runner.runOneTest("unparseFixedLengthString01") }
-  @Test def test_unparseFixedLengthString02() { runner.runOneTest("unparseFixedLengthString02") }
-  @Test def test_unparseFixedLengthString03() { runner.runOneTest("unparseFixedLengthString03") }
+  //DFDL-1316
+  //@Test def test_parseDelimitedEscapedString05() { runner.runOneTest("parseDelimitedEscapedString05") }
 
-  @Test def test_negativeUnparseTest01() { runner.runOneTest("negativeUnparseTest01") }
-  @Test def test_negativeUnparseTest02() { runner.runOneTest("negativeUnparseTest02") }
-  @Test def test_negativeUnparseTest05() { runner.runOneTest("negativeUnparseTest05") }
+  @Test def test_parseDelimitedEscapedString01() { runner.runOneTest("parseDelimitedEscapedString01") }
+  @Test def test_parseDelimitedEscapedString03() { runner.runOneTest("parseDelimitedEscapedString03") }
 
-  //DFDL-1294
-  @Test def test_negativeUnparseTest03() { runner.runOneTest("negativeUnparseTest03") }
-  @Test def test_negativeUnparseTest04() { runner.runOneTest("negativeUnparseTest04") }
-
-  @Test def test_unparseDelimitedString01() { runner.runOneTest("unparseDelimitedString01") }
-  @Test def test_unparseDelimitedPaddedString01() { runner.runOneTest("unparseDelimitedPaddedString01") }
-
-  @Test def test_parseDelimitedString01() { runner.runOneTest("parseDelimitedString01") }
-  @Test def test_parseDelimitedPaddedString01() { runner.runOneTest("parseDelimitedPaddedString01") }
+  //DFDL-1315 
+  //@Test def test_parseDelimitedEscapedString04() { runner.runOneTest("parseDelimitedEscapedString04") }
 
 }
 
