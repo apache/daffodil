7572b4bea282d998b37f38e394e32f224ba2d0e5
Steve Lawrence <slawrence@tresys.com>
2015-08-13 12:11:43 -0400

    Replace NS thread cache with UniquenessCache from scala
    The existing cache using a mutable map was not thread safe. Replace that
    with the UniquenessCache from scala which contains all the logic to be
    thread safe and simplifies the NS code a bit. This also changes the
    cache to key on a URI rather than a string representation of a URI.
    
    DFDL-1408

--- daffodil-lib/src/main/scala/edu/illinois/ncsa/daffodil/xml/Namespaces.scala
+++ daffodil-lib/src/main/scala/edu/illinois/ncsa/daffodil/xml/Namespaces.scala
@@ -34,13 +34,14 @@ package edu.illinois.ncsa.daffodil.xml
 
 import java.net.URI
 import edu.illinois.ncsa.daffodil.exceptions.Assert
+import edu.illinois.ncsa.daffodil.util.UniquenessCache
 
 /**
  * Central factory for, and class to represent namespace URIs
  *
  * Import this object. I.e., import edu.illinois.ncsa.daffodil.xml.NS._
  */
-object NS {
+object NS extends UniquenessCache[URI, NS] {
 
   /**
    * Import these implicit conversions for convenience if you like
@@ -49,38 +50,25 @@ object NS {
   implicit def implicitNStoString(ns: NS): String = ns.toString
   implicit def implicitNStoURI(ns: NS): URI = ns.uri
 
-  private val nsCache = scala.collection.mutable.Map[String, NS]()
-
-  def apply(nsString: String): NS = {
-    // null means NoNamespace. So does ""
-    val s = if (nsString == null) "" else nsString
-    fromNSString(s, None)
+  override def apply(uri: URI): NS = {
+    Assert.usage(uri != null)
+    super.apply(uri)
   }
 
-  private def fromNSString(s: String, uriArg: Option[URI]): NS = {
-    Assert.usage(s != null)
-    Assert.usage(s != NoNamespace.toString)
-    Assert.usage(s != UnspecifiedNamespace.toString)
-    val haveIt = nsCache.get(s)
-    (haveIt, uriArg) match {
-      case (Some(ns), _) => ns
-      case (None, Some(uri)) => {
-        val newNS = new NS(uri)
-        nsCache.put(s, newNS)
-        newNS
-      }
-      case (None, None) => {
-        val newNS = new NS(URI.create(s))
-        nsCache.put(s, newNS)
-        newNS
-      }
+  def apply(nsString: String): NS = {
+    // NoNamespace and UnspecifiedNamespace do not have a URI, and so they are
+    // not retrieved from the uniqueness cache
+    if (nsString == null || nsString == "") {
+      NoNamespace
+    } else if (nsString == UnspecifiedNamespace.toString) {
+      UnspecifiedNamespace
+    } else {
+      apply(URI.create(nsString))
     }
   }
 
-  def apply(uri: URI): NS = fromNSString(uri.toString, Some(uri))
-
-  nsCache.put("", NoNamespace)
-  nsCache.put(UnspecifiedNamespace.toString, UnspecifiedNamespace)
+  protected def valueFromKey(uri: URI): NS = new NS(uri)
+  protected def keyFromValue(ns: NS): Option[URI] = Some(ns.uri)
 
   /**
    * Finds all prefixes for a given namespace. Used to suggest
@@ -105,9 +93,7 @@ object NoNamespace extends NS(null) {
   override def toString = "No_Namespace"
   override def uri = Assert.usageError("No-namespace has no URI.")
   override def toStringOrNullIfNoNS: String = null // most places in Java APIs, no namespace is represented by null.
-  override def equalsNS(other: NS) = this eq other
   override def explainForMsg = "in no namespace"
-
 }
 
 /**
@@ -120,9 +106,7 @@ object UnspecifiedNamespace extends NS(null) {
   override def toString = "Unspecified_Namespace"
   override def uri = Assert.usageError("UnspecifiedNamespace has no URI.")
   override def toStringOrNullIfNoNS: String = null // most places in Java APIs, no namespace is represented by null.
-  override def equalsNS(other: NS) = this eq other
   override def explainForMsg = "with unspecified namespace"
-
 }
 
 sealed class NS protected (uriArg: URI) extends Serializable { // protected constructor. Must use factory.
@@ -132,22 +116,26 @@ sealed class NS protected (uriArg: URI) extends Serializable { // protected cons
   def isNoNamespace = false
   def isUnspecified = false
   override def hashCode() = toString.hashCode()
-  
-  final protected def readResolve(): AnyRef = {
-    val s = if (this.toString == NoNamespace.toString) "" else this.toString
-    NS.nsCache.getOrElseUpdate(s, this)
+  def explainForMsg = "in namespace " + toString
+
+  /**
+   * The readResolve function is called when these objects are deserialized. We
+   * don't want deserialization to create new NS objects that aren't in the
+   * cache. Instead we want to use the NS objects already in the cache, or add
+   * a new one to the cache if it's not already cached and use that.
+   * Additionally, because NoNamespace and UnspecifiedNamespace are not stored
+   * in the cache, special case those to use those objects when necessary.
+   */
+  @throws(classOf[java.io.ObjectStreamException])
+  protected def readResolve(): Any = {
+    if (this.toString == NoNamespace.toString) NoNamespace
+    else if (this.toString == UnspecifiedNamespace.toString) UnspecifiedNamespace
+    else NS.apply(uri)
   }
-  
-  def equalsNS(other: NS): Boolean = {
-    if (this eq other) return true
+
+  override def equals(other: Any): Boolean = {
+    if (this eq other.asInstanceOf[AnyRef]) return true
     Assert.invariant(this.toString != other.toString) // this fails if the cache isn't being used
     false
   }
-  final override def equals(other: Any): Boolean = {
-    other match {
-      case otherNS: NS => equalsNS(otherNS)
-      case _ => false
-    }
-  }
-  def explainForMsg = "in namespace " + toString
 }
