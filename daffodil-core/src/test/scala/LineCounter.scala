2e1ce1098618eecd17129996732d3ffe68715fa8
Mike Beckerle <mbeckerle@tresys.com>
2015-12-22 15:06:23 -0500

    Update LineCounter to include more things, exclude more finely.
    Counts code, unit tests (scala code), tdml tests, DFDL schemas, and
    demo code.
    
    Excludes files authored by 3rd parties, TDML files that are mostly data.
    
    Here's the summary output:
    
    daffodil source total = 65134
    daffodil unit test total = 26331
    daffodil tests and examples total = 160113
    DFDLSchemas total = 1689
    daffodil-fouo/data-formats total = 27856
    calabash total = 2779
    all sbt files total = 489
    Grand total = 284391 in 1108 files.

--- daffodil-core/src/test/scala/LineCounter.scala
+++ daffodil-core/src/test/scala/LineCounter.scala
@@ -1,28 +1,27 @@
 
-
 /* Copyright (c) 2012-2015 Tresys Technology, LLC. All rights reserved.
  *
  * Developed by: Tresys Technology, LLC
  *               http://www.tresys.com
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal with
  * the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
  * of the Software, and to permit persons to whom the Software is furnished to do
  * so, subject to the following conditions:
- * 
+ *
  *  1. Redistributions of source code must retain the above copyright notice,
  *     this list of conditions and the following disclaimers.
- * 
+ *
  *  2. Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimers in the
  *     documentation and/or other materials provided with the distribution.
- * 
+ *
  *  3. Neither the names of Tresys Technology, nor the names of its contributors
  *     may be used to endorse or promote products derived from this Software
  *     without specific prior written permission.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,48 +35,160 @@ import java.io.File
 import scala.io.Source
 
 /**
- * A very sandbox-structure specific way to count the lines of code in a project.
+ * Counts Lines-of-Code in Daffodil, and associated DFDL schemas and demo code.
+ *
+ * Excludes TDML files that are mostly just big chunks of data.
+ * Excludes data files.
+ * Includes TDML and Scala and Java and XSD and SBT files.
  */
 object LineCounter extends App {
 
   /**
-   * exclude generated code from count (we count the generator, not the generated).
-   */
-  val filesToExclude = List("GeneratedCode.scala")
-
-  /**
-   * What directories within modules contain stuff we want to count.
-   */
-  val sourceDirectoriesToInclude = List("src/main", "src/test")
-
-  /**
-   * What to exclude (subversion artifacts, what else)
-   */
-  val directoriesToExclude = List(".svn", ".git", "lib_managed", "src_managed", "bin", ".cache", "target", "lib")
-
-  /**
    * What file extensions to count.
    */
-  val fileSuffixesToInclude = List(".scala") //, ".tdml") // , ".xml") Some test .xml files are Huge. Don't include them.
-
-  /**
-   * What modules within the sandbox to count
-   */
-  val modulesToInclude = List("daffodil-core", "daffodil-runtime1", "daffodil-lib", "daffodil-propgen", "daffodil-tdml", "daffodil-io", "daffodil-cli", "daffodil-japi")
+  val fileSuffixesToInclude = List(".scala", ".java", ".tdml", ".xsd") // not .xml as some of those are data files that are giant.
 
   /**
    * Where is this freakin' sandbox anyway....edit to point to yours.
    */
-  val root = "/home/mbeckerle/dataiti/git/daffodil"
+  val root = "/home/mbeckerle/dataiti/git/"
 
-  def countLines(fi: File): Int = {
-    val src = Source.fromFile(fi)
-    val cnt = src.getLines.foldLeft(0) { (i, line) => i + 1 }
-    System.out.println(fi.getAbsolutePath() + " line count = " + cnt)
+  /**
+   * What modules within the sandbox to count
+   */
+  val srcModulesToInclude = List("daffodil-core", "daffodil-runtime1", "daffodil-runtime1-unparser", "daffodil-lib", "daffodil-propgen", "daffodil-tdml", "daffodil-io", "daffodil-cli", "daffodil-japi", "daffodil-sapi")
+  val unitTestModulesToInclude = srcModulesToInclude
+
+  def top() = {
+
+    val counters = List(
+      new LineCounterCombiner("daffodil source", List(
+        new LineCounter("daffodil source", root + "daffodil", srcModulesToInclude, List("src/main"),
+          filesToExclude = List("GeneratedCode.scala",
+            "DFDL_part3_model.xsd", "DFDL_part2_attributes.xsd", "DFDL_part1_simpletypes.xsd" // IBM supplied. Only minor tweeks by us.
+            )),
+        new LineCounter("daffodil source sbt", root, List("daffodil"), List("project")))),
+      new LineCounter("daffodil unit test", root + "daffodil", srcModulesToInclude, List("src/test")),
+      new LineCounterCombiner("daffodil tests and examples", List(
+        new LineCounter("daffodil tests and examples", root + "daffodil", List("daffodil-test", "daffodil-examples"), List("src/main", "src/test")),
+        new LineCounter("daffodil 'ibm1' ibm-supplied tests", root + "daffodil", List("daffodil-test-ibm1"),
+          List("src/main",
+            "src/test/resources/test-suite/ibm-contributed",
+            "src/test/scala",
+            "src/test/scala-debug",
+            "src/test/scala-new"),
+          fileSuffixesToInclude = List(".scala", ".java", ".tdml", ".sbt"),
+          filesToExclude = List("TresysTests")),
+        new LineCounter("daffodil 'ibm1' tresys-supplied tests", root + "daffodil", List("daffodil-test-ibm1"),
+          List("src/main",
+            "src/test/resources/test-suite/tresys-contributed",
+            "src/test/scala",
+            "src/test/scala-debug",
+            "src/test/scala-new"),
+          filesToExclude = List("IBMTests")) // not .xsd as we didn't write the schemas here.
+          )),
+      new LineCounterCombiner("DFDLSchemas", List(
+        // NACHA is separate due to the 2013 subdir, which we don't have a way to deal with yet
+        new LineCounter("DFDLSchemas/NACHA", root + "DFDLSchemas/NACHA", List("2013"), List("src/test"),
+          filesToExclude = List("NACHA.tdml") // mostly contains test data
+          ), // only src/test as IBM wrote this schema. We just tweeked it.
+        new LineCounter("DFDLSchemas/others", root + "DFDLSchemas", List("mil-std-2045"), List("src/main", "src/test")) // Not PCAP since included in daffodil-examples
+        )),
+      new LineCounterCombiner("daffodil-fouo/data-formats", List(
+        new LineCounter("daffodil-fouo/data-formats", root + "daffodil-fouo/data-formats", Nil, List("src/main", "src/test"),
+          filesToExclude = List(
+            "army_drrs_lh.dfdl.tdml", // a giant tdml file that mostly just contains data
+            "sets.xsd", "fields.xsd", "messages.xsd", "composites.xsd" // usmtf generated schema
+            ) //
+            ),
+        new LineCounter("vmf", root + "daffodil-fouo/data-formats/vmf", List("generator", "schema"), List("src/main", "src/test")),
+        new LineCounterOneDir("vmf other", root + "daffodil-fouo/data-formats/vmf", List("build.sbt", "README.md") //
+        ))), //
+      new LineCounterCombiner("calabash", List(
+        new LineCounter("calabash proper", root + "daffodil-calabash-extension", List("calabash-server"), List("src/main", "src/test"),
+          fileSuffixesToInclude = List(".scala", ".java", ".tdml", ".xsd", ".xpl", ".sbt")),
+        new LineCounter("calabash fouo", root + "daffodil-fouo", List("calabash-test"), List("src/main", "src/test"),
+          fileSuffixesToInclude = List(".scala", ".java", ".tdml", ".xpl", ".sbt")))),
+      new LineCounter("all sbt files", root, List("daffodil", "daffodil-fouo", "DFDLSchemas", "daffodil-calabash-extension"), List(""), fileSuffixesToInclude = List(".sbt")))
+    val allpairs = counters.flatMap { _.pairs }
+    val nFiles = allpairs.length
+    allpairs.foreach { case (fn, cnt) => println(fn.stripPrefix(root), cnt) }
+    counters.foreach { cntr => println(cntr.title + " total = " + cntr.total) }
+    System.out.println("Grand total = " + counters.map { _.total }.sum + " in " + nFiles + " files.")
+  }
+
+  top()
+}
+
+trait LineCounterBase {
+  def title: String
+
+  def pairs: List[(String, Int)]
+
+  final def total = pairs.map { _._2 }.sum
+
+  protected final def linesWithoutCopyright(f: File): List[String] = {
+    val srcContents = Source.fromFile(f).getLines().mkString("\n")
+    val srcNoCopyright: String = srcContents.replaceFirst("""(?i)(?s)Copyright\s+\(c\)\s+[\w\s-]{0,10}?\s+Tresys\s+Technology.+?\sOTHER.+?DEALINGS.+?WITH.+?THE.+?SOFTWARE\.""", "")
+    if (srcContents == srcNoCopyright)
+      System.err.println("No banner in " + f.toString)
+    else
+      System.err.println("Banner found in " + f.toString)
+    val lines: List[String] = srcNoCopyright.split("""\n""").toList
+    lines
+  }
+}
+
+/**
+ * Use when you want to combine a bunch of counters that can't be expressed as uniform modules under a root,
+ * but you still want them reported as a single count.
+ */
+class LineCounterCombiner(val title: String, counters: List[LineCounterBase]) extends LineCounterBase {
+
+  lazy val pairs = counters.flatMap { _.pairs }
+}
+
+class LineCounterOneDir(val title: String, root: String, files: List[String]) extends LineCounterBase {
+
+  lazy val pairs =
+    files.map { fname =>
+      {
+        val fn = root + "/" + fname
+        val f = new File(fn)
+        val lines = linesWithoutCopyright(f)
+        (fn, lines.length)
+      }
+    }
+
+}
+
+/**
+ * Counts lines of a set of modules under a root.
+ */
+class LineCounter(
+  val title: String,
+  root: String,
+  modulesToInclude: List[String], // if Nil, uses all directories under the root as modules.
+  sourceDirectoriesToInclude: List[String],
+  fileSuffixesToInclude: List[String] = LineCounter.fileSuffixesToInclude,
+  filesToExclude: List[String] = List())
+  extends LineCounterBase {
+
+  /**
+   * What to exclude (subversion artifacts, anything generated, version control stuff, caches, settings, etc.)
+   */
+  private val directoriesToExclude = List(".svn", ".git", ".settings", "lib_managed", "src_managed", "resource_managed", "bin", ".cache", "target", "lib")
+
+  lazy val pairs: List[(String, Int)] = {
+    sourceDirectoriesToInclude.flatMap { srcDir => perSourceDir(srcDir) }
+  }
+
+  private def countLines(fi: File): Int = {
+    val cnt = linesWithoutCopyright(fi).length
     cnt
   }
 
-  def allSubdirsOfInterest(dir: File): List[File] = {
+  private def allSubdirsOfInterest(dir: File): List[File] = {
     //println("allSubdirsOfInterest")
     var res: List[File] = null
     if (!dir.isDirectory()) {
@@ -99,51 +210,38 @@ object LineCounter extends App {
     res
   }
 
-  def filesOfInterest(dir: File): List[File] = {
+  private def filesOfInterest(dir: File): List[File] = {
     val files =
       if (!dir.isDirectory()) Nil
       else dir.listFiles.toList.filterNot(_.isDirectory())
-    val filesWithoutExcluded = files.filterNot { f: File => filesToExclude.contains(f.getName()) }
+    val filesWithoutExcluded = files.filterNot {
+      f: File =>
+        filesToExclude.exists { ex => f.getName().contains(ex) }
+    }
     val filesWithExtensions = filesWithoutExcluded.filter { file =>
       {
         val res = fileSuffixesToInclude.exists { ext => file.getName().endsWith(ext) }
         res
       }
     }
-    //println(filesWithExtensions)
     filesWithExtensions
   }
 
-  /**
-   * This is the thing that uses all the very sandbox specific paths above.
-   */
-  def totalPerSourceDir(srcDir: String) = {
+  private def perSourceDir(srcDir: String): List[(String, Int)] = {
     val topDir = new File(root).listFiles.toList
-    val modules = topDir.filter { d: File => modulesToInclude.contains(d.getName()) }
-    //System.err.println(modules)
-    val modulesSrcDirs = modules.map { modDir => new File(modDir + "/" + srcDir) }
-    // println(modulesSrcDirs)
+    val modules =
+      if (modulesToInclude != Nil)
+        topDir.filter { d: File => modulesToInclude.contains(d.getName()) }
+      else {
+        // default to all modules
+        topDir
+      }
+    val moduleDirSuffix = if (srcDir != "") "/" + srcDir else ""
+    val modulesSrcDirs = modules.map { modDir => new File(modDir + moduleDirSuffix) }
     val modulesDirs = modulesSrcDirs.flatMap { allSubdirsOfInterest(_) }
-    // println(modulesDirs)
     val files = modulesDirs.flatMap { filesOfInterest(_) }
-    val counts = files.map { file => countLines(file) }
-    files zip counts foreach println
-    val count = counts.sum
-    System.out.println("total = " + count)
-    count
+    val pairs = files.map { file => (file.toString, countLines(file)) }
+    pairs
   }
 
-  def top() = {
-    val count = sourceDirectoriesToInclude.map { srcDir => totalPerSourceDir(srcDir) }.sum
-    System.out.println("grand total = " + count)
-
-    /*
-    val counts = sourceDirectoriesToInclude.map { srcDir => filesOfInterest(new File(srcDir)).map { countLines(_) }.sum }
-    val res = sourceDirectoriesToInclude zip counts
-    res
-    */
-  }
-
-  top()
 }
-
