9b97b0e3f6e968b9d58183936a23c88b1606cf83
Steve Lawrence <slawrence@tresys.com>
2017-05-09 12:51:26 -0400

    Improve performance of Scala and JDOM Infoset Inputters
    The current implementation of these two infoset inputters were just
    stubs. They convert the respective data (scala.xml.Node or
    org.jdom2.Document) to a string, convert that string to a StringReader,
    and then create an XMLTextInfosetInputter to generate the appropriate
    events. This conversion to a string has some amount of overhead and
    should be avoided. This patch modifies the Scala and JDOM infoset
    inputters to step through the data structures and return the appropriate
    events, avoiding the overhead related to toString conversion and parsing
    of the XML string.
    
    Some basic tests show that the performance improvements can vary pretty
    wildy, in some cases seeing only a 10% increase, but in others seeing a
    60% increase. Regardless, there is always an increase in performance.
    
    DFDL-1760, DFDL-1761

--- daffodil-runtime1/src/main/scala/edu/illinois/ncsa/daffodil/infoset/JDOMInfosetInputter.scala
+++ daffodil-runtime1/src/main/scala/edu/illinois/ncsa/daffodil/infoset/JDOMInfosetInputter.scala
@@ -32,30 +32,168 @@
 
 package edu.illinois.ncsa.daffodil.infoset
 
+import edu.illinois.ncsa.daffodil.util.MStackOf
+import edu.illinois.ncsa.daffodil.util.MaybeBoolean
+import edu.illinois.ncsa.daffodil.xml.XMLUtils
+
 import org.jdom2.Document
+import org.jdom2.Element
+import org.jdom2.Text
+import org.jdom2.Content
+import org.jdom2.Namespace
+import org.jdom2.ProcessingInstruction
+import org.jdom2.Comment
+
+import java.util.Iterator
+
+object JDOMInfosetInputter {
+  protected val JDOM_XSI_NAMESPACE = Namespace.getNamespace(XMLUtils.XSI_NAMESPACE)
+}
 
 class JDOMInfosetInputter(doc: Document)
   extends InfosetInputter {
 
-  // TODO: This is very inefficient and slow. This entire class should be
-  // rewritten, likely using the InfosetInputterCursor interface, similar to
-  // XML or Json, and use some sort of jdom pull cursor library
-  private val xmlInfosetInputter = {
-    val rawFormat = org.jdom2.output.Format.getRawFormat()
-    val xmlOutputter = new org.jdom2.output.XMLOutputter(rawFormat)
-    val string = xmlOutputter.outputString(doc)
-    val rdr = new java.io.StringReader(string)
-    new XMLTextInfosetInputter(rdr)
+  /**
+   * This stack represents the stack of elements that have been visited. Each
+   * item on the stack is a tuple of an element and an iterator of its
+   * children. The current element is the _1 of the item on the top of the
+   * stack. Each time we want to descend into a child element (via a call to
+   * next()) we get the next node from _2 on the top of the stack, and the push
+   * a tuple of that element and its children to the top of the stack. When a
+   * child iterator.hasNext is false, that means we have visited all the
+   * children and we should do a end event for the current element at the top
+   * of the stack. The doStartEvent variable defined below helps to keep track
+   * if we should do a start or end event for the element at the top of the
+   * stack. Note that null at the top of the stack is used to signify the
+   * Start/EndDocument events. All other items on the stack should be an
+   * Element.
+   */
+  private val stack = {
+    val s = new MStackOf[(Element,Iterator[Content])]
+    val docChildren = doc.getContent.iterator
+    if (!docChildren.hasNext) {
+      throw new InvalidInfosetException("Document does not contain a root element")
+    }
+    s.push((null, docChildren))
+    s
   }
 
-  override def getEventType() = xmlInfosetInputter.getEventType()
-  override def getLocalName() = xmlInfosetInputter.getLocalName()
-  override def getNamespaceURI() = xmlInfosetInputter.getNamespaceURI()
-  override def getSimpleText() = xmlInfosetInputter.getSimpleText()
-  override def hasNext() = xmlInfosetInputter.hasNext()
-  override def isNilled() = xmlInfosetInputter.isNilled()
-  override def next() = xmlInfosetInputter.next()
-  override val supportsNamespaces = xmlInfosetInputter.supportsNamespaces
+  private var doStartEvent = true
 
-  override def fini = xmlInfosetInputter.fini
+  override def getEventType(): InfosetInputterEventType = {
+    if (stack.top._1 == null) {
+      if (doStartEvent) StartDocument else EndDocument
+    } else {
+      if (doStartEvent) StartElement else EndElement
+    }
+  }
+
+  override def getLocalName(): String = stack.top._1.getName
+  
+  override val supportsNamespaces = true
+
+  override def getNamespaceURI(): String = stack.top._1.getNamespace.getURI
+  
+  override def getSimpleText(): String = {
+    val text =
+      if (stack.top._2.hasNext) {
+        val child = stack.top._2.next
+        if (stack.top._2.hasNext) {
+          throw new NonTextFoundInSimpleContentException(stack.top._1.getQualifiedName)
+        }
+
+        val text = child match {
+          case t: Text => t.getText()
+          case _ => throw new NonTextFoundInSimpleContentException(stack.top._1.getQualifiedName)
+        }
+        XMLUtils.remapPUAToXMLIllegalCharacters(text)
+      } else {
+        ""
+      }
+
+    // a call to getCurrentEventType will be the EndElement for this simple type
+    doStartEvent = false
+
+    text
+  }
+
+  override def isNilled(): MaybeBoolean = {
+    val elem = stack.top._1
+    val nilAttrValue = elem.getAttributeValue("nil", JDOMInfosetInputter.JDOM_XSI_NAMESPACE)
+    val res =
+      if (nilAttrValue == null) {
+        MaybeBoolean.Nope
+      } else if (nilAttrValue == "true" || nilAttrValue == "1") {
+        MaybeBoolean(true)
+      } else if (nilAttrValue == "false" || nilAttrValue == "0") {
+        MaybeBoolean(false)
+      } else {
+        throw new InvalidInfosetException("xsi:nil property is not a valid boolean: '" + nilAttrValue + "' for element " + elem.getQualifiedName)
+      }
+    res
+  }
+
+  override def hasNext(): Boolean = {
+    // there is a next event when we are not at the DocumentEnd event. A
+    // DocumentEnd event happens if doStartEvent is false and the element at
+    // the top of the stack is null.
+    val atDocumentEnd = stack.top._1 == null && doStartEvent == false
+    !atDocumentEnd
+  }
+
+  override def fini = {
+    stack.clear
+  }
+
+  /**
+   * This attemptes to descend into a child by looking for an Element in
+   * the child list. This ignores all empty text nodes, processing
+   * instructions, and comments. If it is all ignored, then we did not descened
+   */
+  private def tryDescend(): Boolean = {
+    var descended = false
+    while (stack.top._2.hasNext && !descended) {
+      stack.top._2.next match {
+        case e: Element => {
+          // found a child element, push it and its children to the stack
+          stack.push((e, e.getContent.iterator))
+          descended = true
+        }
+        case t: Text if t.getText().forall(_.isWhitespace) => // ignore empty text elements
+        case _: ProcessingInstruction => // ignore
+        case _: Comment => // ignore
+        case c => throw new IllegalContentWhereEventExpected("Found " + c.getCType.toString)
+      }
+    }
+    descended
+  }
+
+  override def next(): Unit = {
+    if (tryDescend()) {
+      // found a child element, make sure we do a start event for it next
+      doStartEvent = true
+    } else {
+      // we have finished parsing all the children of this thing
+      if (doStartEvent) {
+        // this was a simple type with no children to descened into. We already
+        // did a startEvent for it, so the next event needs to be the end event
+        doStartEvent = false
+      } else {
+        // we just did a end event for this element, so we're done with it.
+        // pop it off the stack, and see if it has any later siblings
+        stack.pop
+        if (tryDescend()) {
+          // found a sibling element, it was pushed to the top of the stack in
+          // tryDescend, do start event for it
+          doStartEvent = true
+        } else {
+          // the element we just popped has no later siblings. So we
+          // need an end event for the element now at the top of the stack.
+          // doStartEvent must be false right here, so its already correct
+          // and we don't have to do anything.
+        }
+      }
+    }
+  }
+
 }
--- daffodil-runtime1/src/main/scala/edu/illinois/ncsa/daffodil/infoset/ScalaXMLInfosetInputter.scala
+++ daffodil-runtime1/src/main/scala/edu/illinois/ncsa/daffodil/infoset/ScalaXMLInfosetInputter.scala
@@ -32,27 +32,166 @@
 
 package edu.illinois.ncsa.daffodil.infoset
 
-import scala.xml.Node
+import edu.illinois.ncsa.daffodil.util.MStackOf
+import edu.illinois.ncsa.daffodil.util.MaybeBoolean
+import edu.illinois.ncsa.daffodil.xml.XMLUtils
 
-class ScalaXMLInfosetInputter(node: Node)
+import scala.xml.Elem
+import scala.xml.Text
+import scala.xml.Node
+import scala.xml.ProcInstr
+import scala.xml.Comment
+
+class ScalaXMLInfosetInputter(rootNode: Node)
   extends InfosetInputter {
 
-  // TODO: This is very inefficient and slow. This entire class should be
-  // rewritten, likely using the InfosetInputterCursor interface, similar to
-  // XML or Json, and use some sort of node pull cursor library
-  private val xmlInfosetInputter = {
-    val rdr = new java.io.StringReader(node.toString())
-    new XMLTextInfosetInputter(rdr)
+  /**
+   * This stack represents the stack of elements that have been visited. Each
+   * item on the stack is a tuple of an element and an iterator of its
+   * children. The current element is the _1 of the item on the top of the
+   * stack. Each time we want to descend into a child element (via a call to
+   * next()) we get the next node from _2 on the top of the stack, and the push
+   * a tuple of that element and its children to the top of the stack. When a
+   * child iterator.hasNext is false, that means we have visited all the
+   * children and we should do a end event for the current element at the top
+   * of the stack. The doStartEvent variable defined below helps to keep track
+   * if we should do a start or end event for the element at the top of the
+   * stack. Note that a null _1 at the top of the stack is used to signify the
+   * Start/EndDocument events.
+   */
+  private val stack = {
+    val s = new MStackOf[(Elem,Iterator[Node])]
+
+    val iter = rootNode match {
+      case e: Elem => e.iterator
+      case _ => throw new InvalidInfosetException("Root node is not an element")
+    }
+    s.push((null, iter))
+    s
   }
 
-  override def getEventType() = xmlInfosetInputter.getEventType()
-  override def getLocalName() = xmlInfosetInputter.getLocalName()
-  override def getNamespaceURI() = xmlInfosetInputter.getNamespaceURI()
-  override def getSimpleText() = xmlInfosetInputter.getSimpleText()
-  override def hasNext() = xmlInfosetInputter.hasNext()
-  override def isNilled() = xmlInfosetInputter.isNilled()
-  override def next() = xmlInfosetInputter.next()
-  override val supportsNamespaces = xmlInfosetInputter.supportsNamespaces
+  private var doStartEvent = true
 
-  override def fini = xmlInfosetInputter.fini
+  override def getEventType(): InfosetInputterEventType = {
+    if (stack.top._1 == null) {
+      if (doStartEvent) StartDocument else EndDocument
+    } else {
+      if (doStartEvent) StartElement else EndElement
+    }
+  }
+
+  override def getLocalName(): String = stack.top._1.label
+  
+  override val supportsNamespaces = true
+
+  override def getNamespaceURI(): String = stack.top._1.namespace
+  
+  override def getSimpleText(): String = {
+    val text =
+      if (stack.top._2.hasNext) {
+        val child = stack.top._2.next
+        if (stack.top._2.hasNext) {
+          throw new NonTextFoundInSimpleContentException(stack.top._1.label)
+        }
+        val text = child match {
+          case t: Text => t.data
+          case _ => throw new NonTextFoundInSimpleContentException(stack.top._1.label)
+        }
+        XMLUtils.remapPUAToXMLIllegalCharacters(text)
+      } else {
+        ""
+      }
+
+    // a call to getCurrentEventType will be the EndElement for this simple type
+    doStartEvent = false
+
+    text
+  }
+
+  override def isNilled(): MaybeBoolean = {
+    val elem = stack.top._1
+    val nilAttrValueOpt = elem.attribute(XMLUtils.XSI_NAMESPACE, "nil")
+    val res =
+      if (nilAttrValueOpt.isEmpty) {
+        MaybeBoolean.Nope 
+      } else {
+        val nilAttrValueSeq = nilAttrValueOpt.get
+        if (nilAttrValueSeq.length > 1) {
+          throw new InvalidInfosetException("multiple xsi:nil properties for element " + elem.label)
+        }
+        val nilAttrValue = nilAttrValueSeq.head.toString
+        if (nilAttrValue == "true" || nilAttrValue == "1") {
+          MaybeBoolean(true)
+        } else if (nilAttrValue == "false" || nilAttrValue == "0") {
+          MaybeBoolean(false)
+        } else {
+          throw new InvalidInfosetException("xsi:nil property is not a valid boolean: '" + nilAttrValue + "' for element " + elem.label)
+        }
+      }
+    res
+  }
+
+  override def hasNext(): Boolean = {
+    // there is a next event when we are not at the DocumentEnd event. A
+    // DocumentEnd event happens if doStartEvent is false and the element at
+    // the top of the stack is a null
+    val atDocumentEnd = stack.top._1 == null && doStartEvent == false
+    !atDocumentEnd
+  }
+
+  override def fini = {
+    stack.clear
+  }
+
+  /**
+   * This attemptes to descend into a child by looking for an Element in the
+   * child list. This ignores all empty text nodes, processing instructions,
+   * and comments. If it is all ignored, then we did not descened
+   */
+  private def tryDescend(): Boolean = {
+    var descended = false
+    while (stack.top._2.hasNext && !descended) {
+      stack.top._2.next match {
+        case e: Elem => {
+          // found a child element, push it and its children to the stack
+          stack.push((e, e.child.iterator))
+          descended = true
+        }
+        case t: Text if t.data.forall(_.isWhitespace) => // ignore empty text elements
+        case _: ProcInstr => // ignore
+        case _: Comment => // ignore
+        case c => throw new IllegalContentWhereEventExpected("Found " + c.getClass.getName)
+      }
+    }
+    descended
+  }
+
+  override def next(): Unit = {
+    if (tryDescend()) {
+      // found a child element, make sure we do a start event for it next
+      doStartEvent = true
+    } else {
+      // we have finished parsing all the children of this thing
+      if (doStartEvent) {
+        // this was a simple type with no children to descened into. We already
+        // did a startEvent for it, so the next event needs to be the end event
+        doStartEvent = false
+      } else {
+        // we just did a end event for this element, so we're done with it.
+        // pop it off the stack, and see if it has any later siblings
+        stack.pop
+        if (tryDescend()) {
+          // found a sibling element, it was pushed to the top of the stack in
+          // tryDescend, do start event for it
+          doStartEvent = true
+        } else {
+          // the element we just popped has no later siblings. So we
+          // need an end event for the element now at the top of the stack.
+          // doStartEvent must be false right here, so its already correct
+          // and we don't have to do anything.
+        }
+      }
+    }
+  }
+
 }
