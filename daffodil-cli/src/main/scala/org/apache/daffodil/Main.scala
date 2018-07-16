apache/daffodil

commit 07ee2434bb207d7137dbe4ea70efb41921562334

Modifications to IO layer to support streaming input data
- Modify the ByteBufferDataInputStream to no longer depend on
  ByteBuffers. It is now an InputSourceDataInputStream, and one can
  implement a new InputSource interface to provide Bytes. This also
  includes various changes like how bitPos and bitLimit are stored to
  simplify code (e.g. no offsets) and no longer requires a bitLimit to
  be set, since not all inputs may know a bit limit. Moves TLState
  members into the PState--the InputSourceDataInputStream class is now
  created by the user and so isn't necessarily created in thread from
  which it will be used, breaking the ThreadLocal functionality.
- Create two InputSource implementations, one using a ByteBuffer as the
  data store and one using a bucketing algorithm to support files larger
  than Int.MaxValue and can free up data that can no longer be
  backtracked to.
- Remove the DataLimits, most of these values weren't actually used.
  Instead, create new tunables for those that were and use those where
  appropriate.
- When we decode data to characters, we need to know exactly how many
  bits were used to decode each character. The Java decoders do not
  provide this information, requiring a lot of complex code to keep
  track, and even then there were bugs. This creates our own Decoders
  that provides the exact information we need and allow for further
  modifications that may be needed for things like
  dfdl:errorEncodingPolicies and dfdl:utf16Width.
- Remove the reporting and replacing decoders. Instead, we now just have
  a single decoder and it handles replacing/reporting based on the
  format info. Another benefit of our custom Decoders.
- Modifies the parse Scala/Java API to expect an
  InputSourceDataInputStream, created by the API user. Other public API
  methods are deprecated and are modified to
  create an InputSourceDataInputStream behind the scenes. API functions
  are also simplified to not take in a bit starting position or bit

daffodil-cli/src/main/scala/org/apache/daffodil/Main.scala
@@ -69,8 +69,8 @@ import org.apache.daffodil.xml.RefQName
 import org.rogach.scallop.ArgType
 import org.rogach.scallop.ValueConverter
 import org.apache.daffodil.processors.DataProcessor
+import org.apache.daffodil.processors.DataLoc
 import org.apache.daffodil.processors.HasSetDebugger
-import org.apache.daffodil.processors.parsers.PState
 import org.apache.daffodil.exceptions.UnsuppressableException
 import org.apache.daffodil.util.InvalidJavaVersionException
 import org.apache.daffodil.infoset.XMLTextInfosetOutputter
@@ -93,6 +93,7 @@ import javax.xml.parsers.DocumentBuilderFactory
 import org.apache.commons.io.IOUtils
 import org.apache.daffodil.api.TunableLimitExceededError
 import org.apache.daffodil.api.DaffodilTunables
+import org.apache.daffodil.io.InputSourceDataInputStream
 
 class NullOutputStream extends OutputStream {
   override def close() {}
@@ -320,6 +321,7 @@ class CLIConf(arguments: Array[String]) extends scallop.ScallopConf(arguments)
               |                      [--validate [mode]]
               |                      [-D[{namespace}]<variable>=<value>...] [-o <output>]
               |                      [-I <infoset_type>]
+              |                      [--stream]
               |                      [-c <file>] [infile]
               |
               |Parse a file, using either a DFDL schema or a saved parser
@@ -339,6 +341,7 @@ class CLIConf(arguments: Array[String]) extends scallop.ScallopConf(arguments)
     val tunables = props[String]('T', keyName = "tunable", valueName = "value", descr = "daffodil tunable to be used when parsing.")
     val config = opt[String](short = 'c', argName = "file", descr = "path to file containing configuration items.")
     val infosetType = opt[String](short = 'I', argName = "infoset_type", descr = "infoset type to output. Must be one of 'xml', 'scala-xml', 'json', 'jdom', 'w3cdom', or 'null'.", default = Some("xml")).map { _.toLowerCase }
+    val stream = toggle(noshort = true, default = Some(false), descrYes = "when left over data exists, parse again with remaining data", descrNo = "stop after the first parse, even if left over data exists")
     val infile = trailArg[String](required = false, descr = "input file to parse. If not specified, or a value of -, reads from stdin.")
 
     validateOpt(debug, infile) {
@@ -824,14 +827,14 @@ object Main extends Logging {
 
         val rc = processor match {
           case Some(processor) if (!processor.isError) => {
-            val (input, optDataSize) = parseOpts.infile.toOption match {
-              case Some("-") | None => (System.in, None)
+            val input = parseOpts.infile.toOption match {
+              case Some("-") | None => System.in
               case Some(file) => {
                 val f = new File(parseOpts.infile())
-                (new FileInputStream(f), Some(f.length()))
+                new FileInputStream(f)
               }
             }
-            val inChannel = java.nio.channels.Channels.newChannel(input);
+            val inStream = InputSourceDataInputStream(input)
 
             processor.setValidationMode(validate)
             setupDebugOrTrace(processor.asInstanceOf[DataProcessor], conf)
@@ -843,66 +846,83 @@ object Main extends Logging {
             val writer = new BufferedWriter(new OutputStreamWriter(output))
             val outputter = getInfosetOutputter(parseOpts.infosetType.toOption.get, writer)
 
-            val parseResult = Timer.getResult("parsing",
-              optDataSize match {
-                case None => processor.parse(inChannel, outputter)
-                case Some(szInBytes) => processor.parse(inChannel, outputter, szInBytes * 8)
-              })
-            val loc = parseResult.resultState.currentLocation
-            displayDiagnostics(parseResult)
-            if (parseResult.isProcessingError) {
-              1
-            } else {
-              // only XMLTextInfosetOutputter and JsonInfosetOutputter write
-              // directly to the writer. Other InfosetOutputters must manually
-              // be converted to a string and written to the output
-              outputter match {
-                case sxml: ScalaXMLInfosetOutputter => writer.write(sxml.getResult.toString)
-                case jdom: JDOMInfosetOutputter => writer.write(
-                  new org.jdom2.output.XMLOutputter().outputString(jdom.getResult))
-                case w3cdom: W3CDOMInfosetOutputter => {
-                  val tf = TransformerFactory.newInstance()
-                  val transformer = tf.newTransformer()
-                  val result = new StreamResult(writer)
-                  val source = new DOMSource(w3cdom.getResult)
-                  transformer.transform(source, result)
-                }
-                case _ => // do nothing
-              }
+            var lastParseBitPosition = 0L
+            var keepParsing = true
+            var error = false
 
-              writer.flush()
+            while (keepParsing) {
 
-              // check for left over data (if we know the size up front, like for a file)
-              optDataSize match {
-                case Some(sz) => {
-                  if (!loc.isAtEnd) {
-                    log(LogLevel.Warning, "Left over data. Consumed %s bit(s) with %s bit(s) remaining.", loc.bitPos1b - 1, (sz * 8) - (loc.bitPos1b - 1))
-                    true
-                  } else false
+              outputter.reset() // reset in case we are streaming
+
+              val parseResult = Timer.getResult("parsing", processor.parse(inStream, outputter))
+              val loc = parseResult.resultState.currentLocation.asInstanceOf[DataLoc]
+              displayDiagnostics(parseResult)
+
+              if (parseResult.isProcessingError || parseResult.isValidationError) {
+                keepParsing = false
+                error = true
+              } else {
+                // only XMLTextInfosetOutputter and JsonInfosetOutputter write
+                // directly to the writer. Other InfosetOutputters must manually
+                // be converted to a string and written to the output
+                outputter match {
+                  case sxml: ScalaXMLInfosetOutputter => writer.write(sxml.getResult.toString)
+                  case jdom: JDOMInfosetOutputter => writer.write(
+                    new org.jdom2.output.XMLOutputter().outputString(jdom.getResult))
+                  case w3cdom: W3CDOMInfosetOutputter => {
+                    val tf = TransformerFactory.newInstance()
+                    val transformer = tf.newTransformer()
+                    val result = new StreamResult(writer)
+                    val source = new DOMSource(w3cdom.getResult)
+                    transformer.transform(source, result)
+                  }
+                  case _ => // do nothing
                 }
-                case None => {
-                  // we need to look at the internal state of the parser inStream to
-                  // see how big it is. We do this after execution so as
-                  // not to traverse the data twice.
-                  val ps = parseResult.resultState.asInstanceOf[PState]
-                  val dis = ps.dataInputStream
-                  val hasMoreData = dis.isDefinedForLength(1) // do we have even 1 more bit?
-                  if (hasMoreData) {
-                    val maybeString = dis.getSomeString(processor.getTunables.maxFieldContentLengthInBytes, ps)
-                    val lengthInBytes = if (maybeString.isEmpty) 0 else maybeString.get.length
-                    if (lengthInBytes > 0)
-                      log(LogLevel.Warning, "Left over data. Consumed %s bit(s) with %s bit(s) remaining.", loc.bitPos1b - 1, (lengthInBytes * 8))
-                    else {
-                      // less than 1 byte is available
-                      log(LogLevel.Warning, "Left over data. Consumed %s bit(s) with less than one byte remaining.", loc.bitPos1b - 1)
+
+                writer.flush()
+
+                if (loc.isAtEnd) {
+                  // do not try to keep parsing, nothing left to parse
+                  keepParsing = false
+                  error = false
+                } else {
+                  // remaining data exists
+
+                  if (parseOpts.stream.toOption.get) {
+                    if (lastParseBitPosition == loc.bitPos0b) {
+                      // this parse consumed no data, that means this would get
+                      // stuck in an infinite loop if we kept trying to stream,
+                      // so we need quit
+                      val remainingBits =
+                        if (loc.bitLimit0b.isDefined) {
+                          (loc.bitLimit0b.get - loc.bitPos0b).toString
+                        } else {
+                          "at least " + (inStream.inputSource.bytesAvailable * 8)
+                        }
+                      log(LogLevel.Warning, "Left over data after consuming 0 bits while streaming. Stopped after consuming %s bit(s) with %s bit(s) remaining.", loc.bitPos0b, remainingBits)
+                      keepParsing = false
+                      error = true
+                    } else {
+                      lastParseBitPosition = loc.bitPos0b
+                      keepParsing = true
+                      error = false
                     }
-                    true
-                  } else false
+                  } else {
+                    // not streaming, show left over data warning
+                    val remainingBits =
+                      if (loc.bitLimit0b.isDefined) {
+                        (loc.bitLimit0b.get - loc.bitPos0b).toString
+                      } else {
+                        "at least " + (inStream.inputSource.bytesAvailable * 8)
+                      }
+                    log(LogLevel.Warning, "Left over data. Consumed %s bit(s) with %s bit(s) remaining.", loc.bitPos0b, remainingBits)
+                    keepParsing = false
+                    error = true
+                  }
                 }
               }
-
-              if (parseResult.isValidationError) 1 else 0
             }
+            if (error) 1 else 0
           }
           case Some(processor) => 1
           case None => 1
@@ -957,23 +977,21 @@ object Main extends Logging {
                 case true => infosetDataToInputterData(infosetType, fileContent)
                 case false => fileContent
               }
-              (filePath, data, dataSize * 8)
+              data
             }
 
             val inputs = (0 until performanceOpts.number()).map { n =>
               val index = n % dataSeq.length
-              val (path, data, dataLen) = dataSeq(index)
+              val data = dataSeq(index)
               val inData = performanceOpts.unparse() match {
                 case true => {
                   Left(data)
                 }
                 case false => {
-                  val bais = new ByteArrayInputStream(data.asInstanceOf[Array[Byte]])
-                  val channel = java.nio.channels.Channels.newChannel(bais);
-                  Right(channel)
+                  Right(InputSourceDataInputStream(data.asInstanceOf[Array[Byte]]))
                 }
               }
-              (path, inData, dataLen)
+              inData
             }
             val inputsWithIndex = inputs.zipWithIndex
 
@@ -997,17 +1015,16 @@ object Main extends Logging {
             val NSConvert = 1000000000.0
             val (totalTime, results) = Timer.getTimeResult({
               val tasks = inputsWithIndex.map {
-                case (c, n) =>
+                case (inData, n) =>
                   val task: Future[(Int, Long, Boolean)] = Future {
-                    val (_ /* path */ , inData, len) = c
                     val (time, result) = inData match {
                       case Left(anyRef) => Timer.getTimeResult({
                         val inputterForUnparse = getInfosetInputter(infosetType, anyRef)
                         processor.unparse(inputterForUnparse, nullChannelForUnparse)
                       })
-                      case Right(channel) => Timer.getTimeResult({
+                      case Right(dis) => Timer.getTimeResult({
                         val outputterForParse = getInfosetOutputter(infosetType, nullWriterForParse)
-                        processor.parse(channel, outputterForParse, len)
+                        processor.parse(dis, outputterForParse)
                       })
                     }
 

daffodil-runtime1/src/main/scala/org/apache/daffodil/processors/Runtime.scala
@@ -33,7 +33,6 @@ import org.apache.daffodil.externalvars.ExternalVariablesLoader
 import java.io.File
 import org.apache.daffodil.externalvars.Binding
 import java.nio.channels.Channels
-import java.nio.channels.FileChannel
 import org.apache.daffodil.util.Maybe
 import org.apache.daffodil.util.Maybe._
 import java.io.ObjectOutputStream
@@ -46,6 +45,7 @@ import org.apache.daffodil.processors.unparsers.UnparseError
 import org.apache.daffodil.oolag.ErrorAlreadyHandled
 import org.apache.daffodil.events.MultipleEventHandler
 import org.apache.daffodil.io.DirectOrBufferedDataOutputStream
+import org.apache.daffodil.io.InputSourceDataInputStream
 import org.apache.daffodil.util.LogLevel
 import org.xml.sax.ErrorHandler
 import org.xml.sax.SAXException
@@ -162,31 +162,11 @@ class DataProcessor(val ssrd: SchemaSetRuntimeData)
    * Here begins the parser runtime. Compiler-oriented mechanisms (OOLAG etc.) aren't used in the
    * runtime. Instead we deal with success and failure statuses.
    */
-  def parse(input: DFDL.Input, output: InfosetOutputter, lengthLimitInBits: Long = -1): DFDL.ParseResult = {
+  def parse(input: InputSourceDataInputStream, output: InfosetOutputter): DFDL.ParseResult = {
     Assert.usage(!this.isError)
 
     val rootERD = ssrd.elementRuntimeData
-
-    val initialState =
-      PState.createInitialPState(rootERD,
-        input,
-        output,
-        this,
-        bitOffset = 0,
-        bitLengthLimit = lengthLimitInBits)
-    parse(initialState)
-  }
-
-  def parse(file: File, output: InfosetOutputter): DFDL.ParseResult = {
-    Assert.usage(!this.isError)
-
-    val initialState =
-      PState.createInitialPState(ssrd.elementRuntimeData,
-        FileChannel.open(file.toPath),
-        output,
-        this,
-        bitOffset = 0,
-        bitLengthLimit = file.length * 8)
+    val initialState = PState.createInitialPState(rootERD, input, output, this)
     parse(initialState)
   }
 
@@ -223,7 +203,6 @@ class DataProcessor(val ssrd: SchemaSetRuntimeData)
 
   private def doParse(p: Parser, state: PState) {
     try {
-      state.dataInputStream.setLimits(state.tunable)
       this.startElement(state, p)
       p.parse1(state)
       this.endElement(state, p)
@@ -274,10 +253,15 @@ class DataProcessor(val ssrd: SchemaSetRuntimeData)
         state.setFailed(e)
       }
       case us: UnsuppressableException => throw us
-      case x: Throwable =>
-        Assert.invariantFailed("Runtime.scala - Leaked exception: " + x)
+      case x: Throwable => {
+        val sw = new java.io.StringWriter()
+        val pw = new java.io.PrintWriter(sw)
+        x.printStackTrace(pw)
+        Assert.invariantFailed("Runtime.scala - Leaked exception: " + x + "\n" + sw.toString)
+      }
     }
 
+    state.dataInputStream.inputSource.compact
     state.dataInputStream.validateFinalStreamState
   }
 

commit cce3874dde5b0dc1ceda31a932dafdf4eb5091e9

Reduce memory usage regressions in commit 07ee2434bb
The modifications to the IO layer to support streaming made changes
that substantially increased memory usage. This makes the following
changes to minimize that:

- No longer save the char iterator state. Saving this state required
  duplication a LongBuffer and CharBuffer, which are two non-trivial
  allocations/copies for every point of uncertainty. This really adds up
  for some file types. Instead, never save the char iterator state. When
  the bit position changes due to resetting a mark, we will just clear
  the char iterator state and decode data again. This does mean some
  data might be decoded twice if we backtrack, but that should be
  relatively quick, and means we only take a hit when we backtrack
  instead of every time there is a point of uncertainty.
- The regexMatch buffers are intentionally large to match long patterns.
  Unfortunately, the PState was changed so that every PState allocated
  its own regex buffers, which resulted in a lot of large allocations.
  Instead, modify the DataProcessor to store ThreadLocal state for the
  regex buffers, and the PState access that state when necessary. So we
  will no only have large regex buffers for each Thread rather than each
  call to parse.
- For every file parsed in the CLI performance command, we allocated a
  new InputSourceDataInputStream before doing any performance testing.
  So if you wanted to do a performance test of 500,000 files, we would
  allocate 500,000 InputSourceDataInputStreams immediately. This class
  isn't huge, but it can add up pretty quick and use a lot of memory.
  Instead, just allocate the InputSourceDataInputStream right before the
  call to parse so that it can be garbage collected when the parse ends.

DAFFODIL-1966

daffodil-cli/src/main/scala/org/apache/daffodil/Main.scala
@@ -968,30 +968,21 @@ object Main extends Logging {
 
             val infosetType = performanceOpts.infosetType.toOption.get
 
-            val dataSeq = files.map { filePath =>
+            val dataSeq: Seq[Either[AnyRef, Array[Byte]]] = files.map { filePath =>
               val input = (new FileInputStream(filePath))
               val dataSize = filePath.length()
               val fileContent = new Array[Byte](dataSize.toInt)
               input.read(fileContent) // For performance testing, we want everything in memory so as to remove I/O from consideration.
               val data = performanceOpts.unparse() match {
-                case true => infosetDataToInputterData(infosetType, fileContent)
-                case false => fileContent
+                case true => Left(infosetDataToInputterData(infosetType, fileContent))
+                case false => Right(fileContent)
               }
               data
             }
 
             val inputs = (0 until performanceOpts.number()).map { n =>
               val index = n % dataSeq.length
-              val data = dataSeq(index)
-              val inData = performanceOpts.unparse() match {
-                case true => {
-                  Left(data)
-                }
-                case false => {
-                  Right(InputSourceDataInputStream(data.asInstanceOf[Array[Byte]]))
-                }
-              }
-              inData
+              dataSeq(index)
             }
             val inputsWithIndex = inputs.zipWithIndex
 
@@ -1022,9 +1013,10 @@ object Main extends Logging {
                         val inputterForUnparse = getInfosetInputter(infosetType, anyRef)
                         processor.unparse(inputterForUnparse, nullChannelForUnparse)
                       })
-                      case Right(dis) => Timer.getTimeResult({
+                      case Right(data) => Timer.getTimeResult({
+                        val input = InputSourceDataInputStream(data)
                         val outputterForParse = getInfosetOutputter(infosetType, nullWriterForParse)
-                        processor.parse(dis, outputterForParse)
+                        processor.parse(input, outputterForParse)
                       })
                     }
 

daffodil-io/src/main/scala/org/apache/daffodil/io/InputSourceDataInputStream.scala
@@ -701,15 +701,16 @@ class InputSourceDataInputStreamCharIteratorState  {
   }
 
   def assignFrom(other: InputSourceDataInputStreamCharIteratorState) {
-    // threadCheck()
-    // TODO: we want to avoid this duplication, we should really only duplicate
-    // when we creating a mark. When we reset, we just need to assign
-    // decodedChars = other.decodedChars. This API doesn't currently
-    // differentiate between saving a mark and resetting a mark
-    this.decodedChars = other.decodedChars.duplicate
-    this.bitPositions = other.bitPositions.duplicate
-    this.moreDataAvailable = other.moreDataAvailable
-    this.bitPositionAtLastFetch0b = other.bitPositionAtLastFetch0b
+    // We are intentionally not saving/restoring any state here. This is
+    // because saving state requires duplicating the long and charbuffers,
+    // which is fairly expensive and can use up alot of memory, especially when
+    // there are lots of points of uncertainties. Instead, the
+    // checkNeedsRefetch method will determine if something changed the
+    // bitPosition (e.g. resetting a mark), and just clear all the internal
+    // state, leading to a redecode of data. This does mean that if we reset
+    // back to a mark we will repeat work that has already been done. But that
+    // should be relatively fast, avoids memory usage, and only takes a penalty
+    // when backtracking rather than every time there is a PoU.
   }
 }
 
@@ -734,9 +735,11 @@ class InputSourceDataInputStreamCharIterator(dis: InputSourceDataInputStream) ex
     val cis = dis.cst.charIteratorState
     if (cis.bitPositionAtLastFetch0b != dis.bitPos0b) {
       // Something outside of the char iterator  moved the bit position since
-      // the last time we fetched data. In this case, all of our cached decoded
-      // data is wrong. So lets clear the iterator state and reset the last
-      // fetch state so that more data will be fetched
+      // the last time we fetched data (e.g. backtracking to a previous mark,
+      // in which we do not save/restore this iterator state data). In this
+      // case, all of our cached decoded data is wrong. So lets clear the
+      // iterator state and reset the last fetch state so that more data will
+      // be fetched
       dis.cst.charIteratorState.clear()
       dis.cst.charIteratorState.bitPositionAtLastFetch0b = dis.bitPos0b
     }

daffodil-runtime1/src/main/scala/org/apache/daffodil/processors/Runtime.scala
@@ -17,6 +17,17 @@
 
 package org.apache.daffodil.processors
 
+import java.io.File
+import java.io.ObjectOutputStream
+import java.nio.channels.Channels
+import java.nio.CharBuffer
+import java.nio.LongBuffer
+import java.util.zip.GZIPOutputStream
+
+import org.xml.sax.ErrorHandler
+import org.xml.sax.SAXException
+import org.xml.sax.SAXParseException
+
 import org.apache.daffodil.Implicits._; object INoWarn4 { ImplicitsSuppressUnusedImportWarning() }
 import org.apache.daffodil.equality._; object EqualityNoWarn3 { EqualitySuppressUnusedImportWarning() }
 import org.apache.daffodil.api.WithDiagnostics
@@ -27,18 +38,13 @@ import org.apache.daffodil.api.DFDL
 import org.apache.daffodil.api.WithDiagnostics
 import org.apache.daffodil.api.DFDL
 import org.apache.daffodil.util.Validator
-import org.xml.sax.SAXParseException
 import org.apache.daffodil.api.ValidationMode
 import org.apache.daffodil.externalvars.ExternalVariablesLoader
-import java.io.File
 import org.apache.daffodil.externalvars.Binding
-import java.nio.channels.Channels
 import org.apache.daffodil.util.Maybe
 import org.apache.daffodil.util.Maybe._
-import java.io.ObjectOutputStream
 import org.apache.daffodil.util.Logging
 import org.apache.daffodil.debugger.Debugger
-import java.util.zip.GZIPOutputStream
 import org.apache.daffodil.processors.unparsers.UState
 import org.apache.daffodil.infoset.InfosetInputter
 import org.apache.daffodil.processors.unparsers.UnparseError
@@ -47,8 +53,6 @@ import org.apache.daffodil.events.MultipleEventHandler
 import org.apache.daffodil.io.DirectOrBufferedDataOutputStream
 import org.apache.daffodil.io.InputSourceDataInputStream
 import org.apache.daffodil.util.LogLevel
-import org.xml.sax.ErrorHandler
-import org.xml.sax.SAXException
 import org.apache.daffodil.io.BitOrderChangeException
 import org.apache.daffodil.infoset._
 import org.apache.daffodil.processors.parsers.ParseError
@@ -95,6 +99,20 @@ class DataProcessor(val ssrd: SchemaSetRuntimeData)
   with MultipleEventHandler {
 
   protected var tunablesObj = ssrd.tunable // Compiler-set tunables
+
+  // This thread local state is used by the PState when it needs buffers for
+  // regex matching. This cannot be in PState because a PState does not last
+  // beyond a single parse, but we want to share this among different parses to
+  // avoid large memory allocations. The alternative is to use a ThreadLocal
+  // companion object, but that would have not access to tunables, so one could
+  // not configure the size of the regex match buffers.
+  @transient lazy val regexMatchState = new ThreadLocal[(CharBuffer, LongBuffer)] {
+    override def initialValue = {
+      val cb = CharBuffer.allocate(tunablesObj.maximumRegexMatchLengthInCharacters)
+      val lb = LongBuffer.allocate(tunablesObj.maximumRegexMatchLengthInCharacters)
+      (cb, lb)
+    }
+  }
 
   def setValidationMode(mode: ValidationMode.Type): Unit = { ssrd.validationMode = mode }
   def getValidationMode() = ssrd.validationMode

daffodil-runtime1/src/main/scala/org/apache/daffodil/processors/parsers/PState.scala
@@ -18,8 +18,6 @@
 package org.apache.daffodil.processors.parsers
 
 import java.nio.channels.Channels
-import java.nio.CharBuffer
-import java.nio.LongBuffer
 
 import scala.Right
 import scala.collection.mutable
@@ -322,9 +320,7 @@ final class PState private (
     }
   }
 
-  // TODO: this should come from a thread safe pool or something so that we do not allocate it for every parse
-  override lazy val regexMatchBuffer = CharBuffer.allocate(tunable.maximumRegexMatchLengthInCharacters)
-  override lazy val regexMatchBitPositionBuffer = LongBuffer.allocate(tunable.maximumRegexMatchLengthInCharacters)
+  override lazy val (regexMatchBuffer, regexMatchBitPositionBuffer) = dataProcArg.regexMatchState.get
 }
 
 object PState {