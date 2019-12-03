fbabd0a565dde0e111cf492afe74b4211c99d7bd
Michael Beckerle <mbeckerle@tresys.com>
2019-12-03 13:34:38 -0500

    Added TakTimer to Timer.scala
    Removed Tak-timing stuff from Maybe.scala
    
    DAFFODIL-2246

--- daffodil-lib/src/main/scala/org/apache/daffodil/util/Timer.scala
+++ daffodil-lib/src/main/scala/org/apache/daffodil/util/Timer.scala
@@ -79,3 +79,90 @@ object Timer extends Logging {
     (nanos, result)
   }
 }
+
+/**
+ * Utility for computing timings in "Takeon" units.
+ * A takeon is the time it takes to do a 3-argument Java/Scala
+ * method/function call, passing 3 Long values, returning 1 Long value.
+ *
+ * Takeon comes from the Tak function or Takeuchi's function. This
+ * calls itself recursively billions of times given only small integers
+ * as arguments. It never computes a very large integer, never recurses very
+ * deeply. It is a useful function for measuring raw
+ * best-possible function-call speed.
+ *
+ * The point of this is to have way to measure performance that is
+ * not in units of time, but units that are self-relative
+ * to the speed of the machine.
+ *
+ * In theory, widely different CPUs at different speeds *could*
+ * produce similar timings when timed in units of "takeons".
+ */
+object TakTimer {
+
+  /**
+   * Tak or Takeuchi's function
+   */
+  def tak(x: Long, y: Long, z: Long): Long = {
+    callCount += 1
+    if (y < x)
+      tak(
+        tak(x - 1, y, z),
+        tak(y - 1, z, x),
+        tak(z - 1, x, y))
+    else
+      z
+  }
+
+  /**
+   * Useful if you want to also compare variations on the Tak
+   * function to this basic tak function used to compute the takeon unit.
+   *
+   * That's handy if you want to compare overheads of things that can
+   * be expressed in terms of tak argument passing.
+   *
+   * Valid after TakTimer.takeon has been evaluated.
+   */
+  var callCount: Long = 0
+
+  /**
+   * These x,y,z values produce about 1.4 billion tak calls.
+   * Taking about 2 seconds to calibrate. Increasing these
+   * (or decreasing y), increases the call count, but doesn't
+   * improve performance (i.e., make a smaller takeon unit,
+   * perhaps due to further hotspot optimizing)
+   * on openjdk 1.8.0_222 running on Ubuntu Linux 18 with lscpu
+   * output of:
+   * <pre>
+   * Model name:          Intel(R) Core(TM) i7-6820HQ CPU @ 2.70GHz
+   * CPU MHz:             2017.538
+   * CPU max MHz:         3600.0000
+   * CPU min MHz:         800.0000
+   * BogoMIPS:            5424.00
+   * </pre>
+   */
+  val x = 20L
+  val y = 2L
+  val z = 20L
+
+  private val info = LogLevel.Info
+
+  /**
+   * The number of nanoseconds for 1 tak function call.
+   *
+   * Divide nanosecond timings by this to get timings in Takeon units.
+   */
+  lazy val takeon: Double = {
+    callCount = 0
+    val basenanos = Timer.getTimeNS("Calibrating Takeon Units", tak(x, y, z))
+    val takUnit = (1.0E0 * basenanos) / callCount
+    takUnit
+  }
+
+  def timeInTakeons(call: => Any, message: String = null) = {
+    takeon // force initialization
+    val nanos = Timer.getTimeNS(message, call)
+    nanos / takeon
+  }
+
+}
