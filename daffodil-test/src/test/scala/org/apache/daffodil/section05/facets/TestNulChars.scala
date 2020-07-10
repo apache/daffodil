e74162d7dba3c5d49fbe98f65b08854d78fcd170
Michael Beckerle <mbeckerle@tresys.com>
2020-07-09 21:05:00 -0400

    Add test_nulPattern1, test_nulPad1
    test_nulPattern1 shows that <xs:pattern value="&#xE000;*"/> doesn't work as a
    pattern to match the E000 substitution characters that Daffodil
    uses in XML to replace ASCII NUL characters.
    
    test_nulPad1 shows up a bug with separator suppression and an infinite loop where we ought to either be getting something that works, or at least a "no forward progress" kind of error.
    
    DAFFODIL-2363, DAFFODIL-2364

---
+++ daffodil-test/src/test/scala/org/apache/daffodil/section05/facets/TestNulChars.scala
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.daffodil.section05.facets
+
+import org.apache.daffodil.tdml.Runner
+import org.junit.AfterClass
+import org.junit.Test
+
+object TestNulChars {
+  lazy val runner = Runner("/org/apache/daffodil/section05/facets", "NulChars.tdml")
+
+  @AfterClass def shutDown = {
+    runner.reset
+  }
+}
+
+import TestNulChars._
+
+class TestNulChars {
+
+  // DAFFODIL-2363 &#xE000; (NUL replacement into XML) can't be used in pattern facet. With full validation.
+  // @Test def test_nulPattern1() = { runner.runOneTest("nulPattern1") }
+
+  // DAFFODIL-2364 - infinite loop
+  // @Test def test_nulPad1() = { runner.runOneTest("nulPad1") }
+
+  @Test def ignoreThisDummyTestToPreventIDEFromRemovingImports(): Unit = {
+    // TODO Delete this dummy test once at least one of the above tests is made to work.
+    runner
+  }
+}
