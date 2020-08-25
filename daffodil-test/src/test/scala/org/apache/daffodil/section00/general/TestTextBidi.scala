0adb436a4c1957d44b96daaba1d2aac9bc53a935
Steinberger <rsteinberger@tresys.com>
2020-08-25 11:26:56 -0400

    Addressed DAFFODIL-2322 dfdl:textBidi property errors
    According to the specification, only textBidi="no" is valid.
    Following the pattern for ignoreCase, the following changes were made.
    Modified: DFDL_part2_attributes.xsd to correct the property error.
    Inserted <xsd:attribute form="qualified" name="textBidi" type="dfdl:YesNoEnum" />
    
    Created: testTextBidi.tdml, TestTextBidi.scala to test
    correct parsing for "no", and correct SDE for "yes"
    
    DAFFODIL-2322

---
+++ daffodil-test/src/test/scala/org/apache/daffodil/section00/general/TestTextBidi.scala
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.daffodil.section00.general
+
+import org.apache.daffodil.tdml.Runner
+import org.junit.{ AfterClass, Test }
+
+object TestTextBidi {
+  val testDir = "/org/apache/daffodil/section00/general/"
+  val runner = Runner(testDir, "testTextBidi.tdml")
+
+  @AfterClass def shutDown: Unit = {
+    runner.reset
+  }
+}
+
+/*
+ * This test addresses reported issue DAFFODIL-2322
+ * dfdl:textBidi property errors when used in attribute form
+ */
+class TestTextBidi {
+
+  import TestTextBidi._
+
+  @Test def test_textBidiNo(): Unit = { runner.runOneTest("textBidiNo") }
+  @Test def test_textBidiYes(): Unit = { runner.runOneTest("textBidiYes") }
+
+}
