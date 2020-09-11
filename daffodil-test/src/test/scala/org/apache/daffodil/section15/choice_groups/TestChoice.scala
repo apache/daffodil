b1dfcec5f7b9d261845f4875e68336ae4a4abac2
Steinberger <rsteinberger@tresys.com>
2020-09-11 13:24:44 -0400

    Removed old test

--- daffodil-test/src/test/scala/org/apache/daffodil/section15/choice_groups/TestChoice.scala
+++ daffodil-test/src/test/scala/org/apache/daffodil/section15/choice_groups/TestChoice.scala
@@ -40,7 +40,6 @@ class TestChoice {
 
   @Test def test_choice_noBranch (): Unit = { runnerCH.runOneTest("choice_noBranch") }
 
-  @Test def test_optionalChoice00(): Unit = { runnerCH.runOneTest("optionalChoice00") }
   @Test def test_optionalChoice01(): Unit = { runnerCH.runOneTest("optionalChoice01") }
   @Test def test_optionalChoice02(): Unit = { runnerCH.runOneTest("optionalChoice02") }
   @Test def test_optionalChoice03(): Unit = { runnerCH.runOneTest("optionalChoice03") }
