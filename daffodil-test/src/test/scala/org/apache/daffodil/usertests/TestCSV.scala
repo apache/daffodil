b4a4d92c24848a441823d004ca6396aab3994b39
Steve Lawrence <slawrence@apache.org>
2020-11-16 08:18:05 -0500

    Correctly detect zero length status for "dead" DataOutputStreams
    When unparsing postfix separators, we create split regions to determine
    if a data region is zero length or not, which is used to help decided
    whether or not to output a postfix separator. If these regions are
    "finished" without anything ever writing to them, then we know the
    region is zero length. But if one of these regions is finished and
    eventually becomes "dead" (i.e. its following DOS becomes direct), it is
    possible for the zero length status to have never been set. This leaves
    the zero length status in an "unknown" state which can lead to locked
    suspensions.
    
    This change fixes this so if a DOS is either finished *or dead* and the
    zero length status has never been set, then we can assume that region
    had zero length, thus allowing suspensions to resolve.
    
    DAFFODIL-2431

---
+++ daffodil-test/src/test/scala/org/apache/daffodil/usertests/TestCSV.scala
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.daffodil.usertests
+
+import org.junit.Test
+import org.apache.daffodil.tdml.Runner
+import org.junit.AfterClass
+
+object TestCSV {
+  val testDir = "/org/apache/daffodil/usertests/"
+  val runner = Runner(testDir, "test-csv.tdml")
+
+  @AfterClass def shutDown: Unit = {
+    runner.reset
+  }
+
+}
+
+class TestCSV {
+
+  import TestCSV._
+
+  @Test def test_csv_01(): Unit = { runner.runOneTest("csv_01") }
+  @Test def test_csv_02(): Unit = { runner.runOneTest("csv_02") }
+  @Test def test_csv_03(): Unit = { runner.runOneTest("csv_03") }
+  @Test def test_csv_04(): Unit = { runner.runOneTest("csv_04") }
+
+}
