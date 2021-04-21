3d2a6e59c5e300bee25cb19b9d0cdcfc449982b6
Michael Beckerle <mbeckerle@tresys.com>
2021-04-21 13:26:28 -0400

    Add tests to illustrate SSP 'never' bugs and poor diagnostics
    These are intended to cross-test with IBM DFDL.
    The tests that don't require 'treatAsEmpty' behavior work on IBM DFDL.
    
    DAFFODIL-2499, DAFFODIL-2496

--- daffodil-test/src/test/scala/org/apache/daffodil/usertests/TestSepTests.scala
+++ daffodil-test/src/test/scala/org/apache/daffodil/usertests/TestSepTests.scala
@@ -44,4 +44,24 @@ class TestSepTests {
   // @Test def test_sep_trailingEmptyStrict_1(): Unit = { runner.runOneTest("test_sep_trailingEmptyStrict_1") }
   @Test def test_sep_trailingEmptyStrict_2(): Unit = { runner.runOneTest("test_sep_trailingEmptyStrict_2") }
 
+  // DAFFODIL-2499 - separatorSuppressionPolicy 'never'
+  // Note: this test isn't commented out, because it works for IBM DFDL in cross testing
+  // The TDML for this test just has it disabled for the daffodil implementation.
+  // Add daffodil to implementations to see the erroneous daffodil behavior.
+  @Test def test_sep_ssp_never_1(): Unit = { runner.runOneTest("test_sep_ssp_never_1") }
+
+  @Test def test_sep_ssp_never_2(): Unit = { runner.runOneTest("test_sep_ssp_never_2") }
+
+  // DAFFODIL-2499 - separatorSuppressionPolicy 'never'
+  // Note: this test isn't commented out, because it works for IBM DFDL in cross testing
+  // The TDML for this test just has it disabled for the daffodil implementation.
+  // Add daffodil to implementations to see the erroneous daffodil behavior.
+  @Test def test_sep_ssp_never_3(): Unit = { runner.runOneTest("test_sep_ssp_never_3") }
+
+  // DAFFODIL-2496 - implement DFDL official emptyElementParsePolicy property
+  // Note: this test isn't commented out, because it works for IBM DFDL in cross testing
+  // The TDML for this test just has it disabled for the daffodil implementation.
+  // Add daffodil to implementations to see the erroneous daffodil behavior.
+  @Test def test_sep_ssp_never_4(): Unit = { runner.runOneTest("test_sep_ssp_never_4") }
+
 }
