5feebf33d8a8a9ac397d7b8d587ee391ee46d0b2
Michael Beckerle <mbeckerle@tresys.com>
2021-04-21 13:26:28 -0400

    Add tests to illustrate SSP 'never' bugs and poor diagnostics
    DAFFODIL-2499, DAFFODIL-2496

--- daffodil-test/src/test/scala/org/apache/daffodil/usertests/TestSepTests.scala
+++ daffodil-test/src/test/scala/org/apache/daffodil/usertests/TestSepTests.scala
@@ -44,4 +44,12 @@ class TestSepTests {
   // @Test def test_sep_trailingEmptyStrict_1(): Unit = { runner.runOneTest("test_sep_trailingEmptyStrict_1") }
   @Test def test_sep_trailingEmptyStrict_2(): Unit = { runner.runOneTest("test_sep_trailingEmptyStrict_2") }
 
+  // DAFFODIL-2499 - separatorSuppressionPolicy 'never'
+  // @Test def test_sep_ssp_never_1(): Unit = { runner.runOneTest("test_sep_ssp_never_1") }
+  @Test def test_sep_ssp_never_2(): Unit = { runner.runOneTest("test_sep_ssp_never_2") }
+  // DAFFODIL-2499 - separatorSuppressionPolicy 'never'
+  // @Test def test_sep_ssp_never_3(): Unit = { runner.runOneTest("test_sep_ssp_never_3") }
+  // DAFFODIL-2496 - implement DFDL official emptyElementParsePolicy property
+  // @Test def test_sep_ssp_never_4(): Unit = { runner.runOneTest("test_sep_ssp_never_4") }
+
 }
