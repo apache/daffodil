7fa60f9f3c72731c8bd4f9d4765d34b33e036326
Scala Steward <me@scala-steward.org>
2021-05-14 04:09:43 +0200

    Update scala-xml to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
 
   lazy val core = Seq(
     "com.lihaoyi" %% "os-lib" % "0.7.6", // for writing/compiling C source files
-    "org.scala-lang.modules" %% "scala-xml" % "1.3.0",
+    "org.scala-lang.modules" %% "scala-xml" % "2.0.0",
     "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2",
     "com.ibm.icu" % "icu4j" % "69.1",
     "xerces" % "xercesImpl" % "2.12.1",
