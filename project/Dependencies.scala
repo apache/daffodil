0def910c7e46b2b5ff6727b5c8e9c3df7d4746bb
Scala Steward <me@scala-steward.org>
2021-07-20 02:05:00 +0200

    Update scallop to 4.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
   lazy val cli = Seq( 
     "org.fusesource.jansi" % "jansi" % "2.3.3",
     "org.jline" % "jline" % "3.20.0",
-    "org.rogach" %% "scallop" % "4.0.3",
+    "org.rogach" %% "scallop" % "4.0.4",
     "net.sf.expectit" % "expectit-core" % "0.9.0" % "it,test"
   )
 
