1f4fe64d12ed8131cbaa807cde35fc5b093536c1
Min Phyo Aung <maung@owlcyberdefense.com>
2021-07-28 15:30:42 -0400

    dfdl prefix properties on dfdl annotation elements should generate useful diagnostic
    - Updated filename from TestDefError.scala to
      TestDFDLFormatPrefixedAttributesSDE.scala and disabled
      validateTDML setting to allow the parser to go through
      the whole file and pickup the TDML error.
    - Updated filename from generalSchemaDefinitionError.tdmlto
      dfdlFormatPrefixedAttributesSDE.tdml
    - Made recommended identations
    
    DAFFODIL-1512

---
+++ daffodil-test/src/test/scala/org/apache/daffodil/section00/general/TestDFDLFormatPrefixedAttributesSDE.scala
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.daffodil.section00.general
+
+import org.apache.daffodil.tdml.Runner
+import org.junit.{AfterClass, Test}
+
+object TestDFDLFormatPrefixedAttributesSDE {
+
+  val testDir = "/org/apache/daffodil/section00/general/"
+  val runner = Runner(testDir, "dfdlFormatPrefixedAttributesSDE.tdml", validateTDMLFile = false)
+
+  @AfterClass def tearDown(): Unit = {
+    runner.reset
+  }
+}
+
+
+class TestDFDLFormatPrefixedAttributesSDE {
+
+  import TestDFDLFormatPrefixedAttributesSDE._
+
+  @Test def test_dfdl_format_prefixed_attributes_sde(): Unit = {
+    runner.runOneTest("dfdl_format_prefixed_attributes_sde")
+  }
+}
