e6ae38e329be139d7c76d787385d192d8f6e1566
Scala Steward <me@scala-steward.org>
2021-12-19 04:07:32 +0100

    Update jackson-core to 2.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
   lazy val infoset = Seq(
     "org.jdom" % "jdom2" % "2.0.6.1",
     "com.fasterxml.woodstox" % "woodstox-core" % "6.2.7",
-    "com.fasterxml.jackson.core" % "jackson-core" % "2.13.0"
+    "com.fasterxml.jackson.core" % "jackson-core" % "2.13.1"
   )
 
   lazy val cli = Seq(
