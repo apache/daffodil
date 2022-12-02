3e63abc2e735ba06367f6f4106ac5a64ddbf5997
Steve Lawrence <slawrence@apache.org>
2022-12-02 11:49:33 -0500

    Add infosetWalkerSkipMin/Max tunables
    Allows configuration of how often the InfosetWalker skips walk() calls.
    Setting to a value of zero disables skipping walk() calls. A value of
    zero is useful during testing to ensure the InfosetWalker isn't
    incorrectly removing infoset elements, which can lead to seemingly
    random null pointer exceptions.
    
    Add multiple tests to show that the InfosetWalker currently removes
    infoset elements that are still needed by separator logic, that this
    does not arise in a particular case with the default skip min/max
    values, and that disabling infoset removal works around the issue.
    
    DAFFODIL-2755

---
+++ daffodil-test/src/test/scala/org/apache/daffodil/section00/general/TestInfosetWalker.scala
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.daffodil.section00.general
+
+import org.apache.daffodil.tdml.Runner
+import org.junit.AfterClass
+import org.junit.Test
+
+object TestInfosetWalker {
+  val testDir = "/org/apache/daffodil/section00/general/"
+  val runner2 = Runner(testDir, "infosetWalker.tdml")
+
+  @AfterClass def shutDown(): Unit = {
+    runner2.reset
+  }
+}
+
+class TestInfosetWalker {
+  import TestInfosetWalker._
+
+  @Test def test_infosetWalker_01() = { runner2.runOneTest("infosetWalker_01") }
+  // DAFFODIL-2755
+  /*@Test*/ def test_infosetWalker_02() = { runner2.runOneTest("infosetWalker_02") }
+  @Test def test_infosetWalker_03() = { runner2.runOneTest("infosetWalker_03") }
+}
