c96b0012e397e008bd48e3835eebccff2a33f635
Scala Steward <scala_steward@virtuslab.com>
2023-01-15 13:28:37 +0000

    Update woodstox-core to 6.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,7 +37,7 @@ object Dependencies {
 
   lazy val infoset = Seq(
     "org.jdom" % "jdom2" % "2.0.6.1",
-    "com.fasterxml.woodstox" % "woodstox-core" % "6.4.0",
+    "com.fasterxml.woodstox" % "woodstox-core" % "6.5.0",
     "com.fasterxml.jackson.core" % "jackson-core" % "2.14.1"
   )
 
