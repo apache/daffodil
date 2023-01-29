ccfe4299db6cdbc41cc2130d3c0f1b33fcc14281
Scala Steward <scala_steward@virtuslab.com>
2023-01-29 13:38:12 +0000

    Update scala-parser-combinators to 2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   lazy val core = Seq(
     "com.lihaoyi" %% "os-lib" % "0.9.0", // for writing/compiling C source files
     "org.scala-lang.modules" %% "scala-xml" % "2.1.0",
-    "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.1",
+    "org.scala-lang.modules" %% "scala-parser-combinators" % "2.2.0",
     "com.ibm.icu" % "icu4j" % "72.1",
     "xerces" % "xercesImpl" % "2.12.2",
     "xml-resolver" % "xml-resolver" % "1.2",
