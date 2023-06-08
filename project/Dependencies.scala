5267a4478679c0e109a402ae988c3879e0e091c7
Scala Steward <scala_steward@virtuslab.com>
2023-06-08 13:43:44 +0000

    Update commons-io to 2.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
     "com.ibm.icu" % "icu4j" % "73.1",
     "xerces" % "xercesImpl" % "2.12.2",
     "xml-resolver" % "xml-resolver" % "1.2",
-    "commons-io" % "commons-io" % "2.12.0",
+    "commons-io" % "commons-io" % "2.13.0",
     "com.typesafe" % "config" % "1.4.2",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5",
   )
