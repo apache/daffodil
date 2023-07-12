d029a3c219ff22cf5b45cfeb2f5730e368695de4
200019465 <katlic@ge.com>
2023-07-12 15:02:37 -0400

    Fix NumberFormatException message in hexCharToValue
    Update exception message in Misc.hex2Bytes.hexCharToValue function to include correct range of allowed values.
    
    Misc.scala: Update exception message to include correct values.
    
    TestMisc.scala: Add tests for Misc.hex2Bytes function.
    
    DAFFODIL-2834

--- daffodil-lib/src/test/scala/org/apache/daffodil/lib/util/TestMisc.scala
+++ daffodil-lib/src/test/scala/org/apache/daffodil/lib/util/TestMisc.scala
@@ -17,10 +17,43 @@
 
 package org.apache.daffodil.lib.util
 
+import org.apache.daffodil.lib.Implicits.intercept
+
 import org.junit.Assert._
 import org.junit.Test
 
 class TestMisc {
+
+  @Test def testHex2BytesInvalidLower(): Unit = {
+    val e = intercept[NumberFormatException] {
+      Misc.hex2Bytes("0g")
+    }
+    assertTrue(e.getMessage().contains("Hex character must be 0-9, a-f, or A-F"))
+  }
+
+  @Test def testHex2BytesInvalidUpper(): Unit = {
+    val e = intercept[NumberFormatException] {
+      Misc.hex2Bytes("0G")
+    }
+    assertTrue(e.getMessage().contains("Hex character must be 0-9, a-f, or A-F"))
+  }
+
+  @Test def testHex2BytesOdd(): Unit = {
+    val e = intercept[NumberFormatException] {
+      Misc.hex2Bytes("0")
+    }
+    assertTrue(e.getMessage().contains("Hex string must have an even number of characters"))
+  }
+
+  @Test def testHex2BytesValid(): Unit = {
+    assertArrayEquals(Array(0).map(_.toByte), Misc.hex2Bytes("00"))
+    assertArrayEquals(Array(9).map(_.toByte), Misc.hex2Bytes("09"))
+    assertArrayEquals(Array(10).map(_.toByte), Misc.hex2Bytes("0a"))
+    assertArrayEquals(Array(15).map(_.toByte), Misc.hex2Bytes("0F"))
+    assertArrayEquals(Array(10).map(_.toByte), Misc.hex2Bytes("0a"))
+    assertArrayEquals(Array(15).map(_.toByte), Misc.hex2Bytes("0f"))
+    assertArrayEquals(Array(171).map(_.toByte), Misc.hex2Bytes("Ab"))
+  }
 
   @Test def testIsAllUpper(): Unit = {
     assertTrue(Misc.isAllUpper("A", 0))
