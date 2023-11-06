4ac668210192b8fac5006f13d479d7d4ebb00128
Scala Steward <scala_steward@virtuslab.com>
2023-11-06 10:48:31 +0000

    Update scallop to 5.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,7 +46,7 @@ object Dependencies {
   lazy val cli = Seq(
     "org.fusesource.jansi" % "jansi" % "2.4.1",
     "org.jline" % "jline" % "3.22.0",
-    "org.rogach" %% "scallop" % "5.0.0",
+    "org.rogach" %% "scallop" % "5.0.1",
     "net.sf.expectit" % "expectit-core" % "0.9.0" % "test",
   )
 
