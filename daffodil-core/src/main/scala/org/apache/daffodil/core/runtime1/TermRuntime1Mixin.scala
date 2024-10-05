a48d92f436fec186f8e8f12a784177b126d2a5e9
olabusayoT <50379531+olabusayoT@users.noreply.github.com>
2024-10-04 20:58:29 -0400

    Ensure hidden groups children excluded from siblings list
    - currently we don't exclude the children of hidden groups from the siblings list. This is because we do the check for when a groupRef is hidden after ChoiceTermBase. The issue is ChoiceGroupRef and SequenceGroupRef, which are GroupRefs, also extend ChoiceTermBase and SequenceTermBase, so it's imperative that we do the hidden check before the *TermBases check
    - add unit test to verify the hidden group's children are excluded from the sibling list
    
    DAFFODIL-2937

--- daffodil-core/src/main/scala/org/apache/daffodil/core/runtime1/TermRuntime1Mixin.scala
+++ daffodil-core/src/main/scala/org/apache/daffodil/core/runtime1/TermRuntime1Mixin.scala
@@ -286,6 +286,7 @@ trait TermRuntime1Mixin { self: Term =>
           Closed(Seq(PNE(eb, true)))
         case eb: ElementBase =>
           Open(Seq(PNE(eb, false)))
+        case gr: GroupRef if gr.isHidden => Open(Nil)
         case ctb: ChoiceTermBase => {
           val individualBranchPossibles = ctb.groupMembers.map {
             _.possibleSelfPlusNextLexicalSiblingStreamingUnparserElements
@@ -326,7 +327,6 @@ trait TermRuntime1Mixin { self: Term =>
             }
           res
         }
-        case gr: GroupRef if gr.isHidden => Open(Nil)
         case stb: SequenceTermBase => {
           //
           // This case only applies to when we are analyzing a sequence, but it is
