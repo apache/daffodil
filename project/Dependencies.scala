6ca3c08ff3c3530f8b076bdfdec2296de6197e4a
Scala Steward <scala_steward@virtuslab.com>
2025-09-07 13:57:08 +0000

    Update scalacheck to 1.19.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -55,7 +55,7 @@ object Dependencies {
   lazy val test = Seq(
     "junit" % "junit" % "4.13.2" % "test",
     "com.github.sbt" % "junit-interface" % "0.13.3" % "test",
-    "org.scalacheck" %% "scalacheck" % "1.18.1" % "test"
+    "org.scalacheck" %% "scalacheck" % "1.19.0" % "test"
   )
 
   lazy val schematron = Seq(
