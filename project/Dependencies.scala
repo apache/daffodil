a29822df4838abb91cee49127f7309bb3dc55900
Scala Steward <scala_steward@virtuslab.com>
2026-01-28 15:59:13 +0000

    Update os-lib to 0.11.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   lazy val common = core ++ infoset ++ test
 
   lazy val core = Seq(
-    "com.lihaoyi" %% "os-lib" % "0.11.6", // for writing/compiling C source files
+    "com.lihaoyi" %% "os-lib" % "0.11.7", // for writing/compiling C source files
     "org.scala-lang.modules" %% "scala-xml" % "2.4.0",
     "org.scala-lang.modules" %% "scala-parser-combinators" % "2.4.0",
     "com.ibm.icu" % "icu4j" % "78.2",
