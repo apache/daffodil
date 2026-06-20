1a3255a6f8fae2e6331716e19aea3f7e6094673f
Scala Steward <scala_steward@virtuslab.com>
2026-06-20 14:35:40 +0000

    Update jline to 4.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   )
 
   lazy val cli = Seq(
-    "org.jline" % "jline" % "3.30.13",
+    "org.jline" % "jline" % "4.2.1",
     "org.rogach" %% "scallop" % "5.3.0",
     "net.sf.expectit" % "expectit-core" % "0.9.0" % "test"
   )
